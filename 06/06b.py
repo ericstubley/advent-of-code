af9f666ea7bbbb9891e6f8296213cb281bae1ee8
Eric Stubley <eric@macbook.local>
2021-12-06 07:57:48 -0500

    Day 06 solution

--- 06/06b.py
+++ 06/06b.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+with open("input.txt", 'r') as f:
+	data = f.readlines()
+
+raw_fish = [int(x) for x in data[0].rstrip('\n').split(',')]
+
+fish = [raw_fish.count(i) for i in range(9)]
+
+# you can fix the pattern of having to reset new_fish twice by putting it at the start of the loop
+for t in range(256):
+	new_fish = [0]*9
+	new_fish[6] = fish[0]
+	new_fish[8] = fish[0]
+	for i in range(1, 9):
+		new_fish[i-1] += fish[i]
+	fish = new_fish
+
+total_fish = sum(fish)
+print(total_fish)
