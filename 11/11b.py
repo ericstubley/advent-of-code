9fcea287a9949793a9c0c3009ec4e8873dc9d340
Eric Stubley <eric@macbook.local>
2021-12-11 22:35:22 -0500

    Day 11 solution (late because train shenanigans)

--- 11/11b.py
+++ 11/11b.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+
+import numpy as np
+
+NEIGHBORS = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+
+# single line to read in as array from file
+octo_grid = np.genfromtxt("input.txt", 
+		dtype=int, delimiter=1)
+
+step = 0
+while octo_grid.any() != 0:
+	octo_grid += 1
+
+	while True in (octo_grid > 9):
+		flash_mask = octo_grid > 9
+
+		# increase everything non-zero and not already > 9 by number of adjacent flash_masks
+		# for each grid element, if in [1,...,8], increase by number of flash_mask neighbors
+		# for each flash_mask, add to each neighbour
+		zero_mask = (np.pad(octo_grid, 1) == 0)
+
+		adjacent = np.zeros(np.shape(zero_mask), dtype=int)
+		for x, y in np.ndindex(np.shape(octo_grid)):
+			if flash_mask[x][y] == True:
+				adjacent[x:x+3, y:y+3] += NEIGHBORS
+		adjacent *= ~zero_mask
+		octo_grid += adjacent[1:-1, 1:-1]
+
+		# set all the flash_mask things to 0
+		octo_grid *= ~flash_mask
+	step += 1
+
+
+print(step)
