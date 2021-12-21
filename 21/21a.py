41dbdcaca0bc066932385a59334190ead33cf6cc
Eric Stubley <ericdavidstubley@gmail.com>
2021-12-21 08:27:23 -0500

    Day 21 solution

--- 21/21a.py
+++ 21/21a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+p1_position, p2_position = 7, 2
+p1_score, p2_score = 0, 0
+num_rolls = 0
+turn = 1
+
+
+while max(p1_score, p2_score) < 1000:
+    to_add = 3*num_rolls + 6
+    num_rolls += 3
+
+    if turn == 1:
+        p1_position = (p1_position + to_add) % 10
+        p1_score += p1_position if p1_position != 0 else 10
+        turn = 2
+    else: # turn == 2
+        p2_position = (p2_position + to_add) % 10
+        p2_score += p2_position if p2_position != 0 else 10
+        turn = 1
+print(min(p1_score, p2_score)*num_rolls)
--- 21/21b.py
+++ 21/21b.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+
+from functools import cache
+
+WEIGHTS = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+
+@cache
+def simulate_universe(p1, p2, s1, s2, turn):
+    # pass positions, scores as 2 element lists for less repetition of code
+    if max(s1, s2) >= 21:
+        return (1, 0) if s1 > s2 else (0, 1)
+
+    w1, w2 = 0, 0
+    for roll, weight in WEIGHTS:
+        new_p1, new_p2 = p1, p2
+        new_s1, new_s2 = s1, s2
+        new_turn = None
+        if turn == 1:
+            new_p1 = (p1 + roll) % 10
+            new_s1 += new_p1 if new_p1 != 0 else 10
+            new_turn = 2
+        else: # turn == 2
+            new_p2 = (p2 + roll) % 10
+            new_s2 += new_p2 if new_p2 != 0 else 10
+            new_turn = 1
+
+        path_w1, path_w2 = simulate_universe(new_p1, new_p2, new_s1, new_s2, new_turn)
+
+        w1 += weight*path_w1
+        w2 += weight*path_w2
+
+    return (w1, w2)
+
+
+
+
+p1_position, p2_position = 7, 2
+p1_wins, p2_wins = simulate_universe(p1_position, p2_position, 0, 0, 1)
+print(p1_wins, p2_wins)
+print(max(p1_wins, p2_wins))
