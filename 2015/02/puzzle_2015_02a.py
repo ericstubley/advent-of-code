cf3c0e69a0317f826524c732a36da08f55017960
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-02 11:06:41 -0500

    Day 02 solution

--- 2015/02/puzzle_2015_02a.py
+++ 2015/02/puzzle_2015_02a.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+
+def paper_required(l, w, h):
+    lw, lh, wh = l*w, l*h, w*h
+    slack = min(lw, lh, wh)
+    return 2*lw + 2*lh + 2*wh + slack
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    dimensions = []
+    for s in data:
+        dimension = tuple
+        dimensions.append(tuple(int(d) for d in s.rstrip('\n').split('x')))
+
+    total_paper = 0
+    for l, w, h in dimensions:
+        total_paper += paper_required(l, w, h)
+
+    print(total_paper)
--- 2015/02/puzzle_2015_02b.py
+++ 2015/02/puzzle_2015_02b.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+
+def ribbon_required(l, w, h):
+    plw, plh, pwh = 2*(l+w), 2*(l+h), 2*(w+h)
+    wrapping = min(plw, plh, pwh)
+    bow = l*w*h
+    return wrapping + bow
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    dimensions = []
+    for s in data:
+        dimension = tuple
+        dimensions.append(tuple(int(d) for d in s.rstrip('\n').split('x')))
+
+    total_ribbon = 0
+    for l, w, h in dimensions:
+        total_ribbon += ribbon_required(l, w, h)
+
+    print(total_ribbon)
