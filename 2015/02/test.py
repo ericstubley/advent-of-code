a9941fd3d5a130c14d04e9caccc7bf28e8dc8bcb
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-02 11:06:41 -0500

    Day 02 solution

--- 2015/02/test.py
+++ 2015/02/test.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
 import unittest
+from puzzle_2015_02a import paper_required
+from puzzle_2015_02b import ribbon_required
+
+class WrappingPaperTest(unittest.TestCase):
+    def test_paper_required(self):
+        self.assertEqual(paper_required(2, 3, 4), 58)
+        self.assertEqual(paper_required(1, 1, 10), 43)
+
+    def test_ribbon_required(self):
+        self.assertEqual(ribbon_required(2, 3, 4), 34)
+        self.assertEqual(ribbon_required(1, 1, 10), 14)
+
+if __name__ == "__main__":
+    unittest.main()
