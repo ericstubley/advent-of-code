0d8f3c50573e313c999391d5eae026095dffa900
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-04 10:44:16 -0500

    Day 04 solution

--- 2015/04/puzzle_2015_04b.py
+++ 2015/04/puzzle_2015_04b.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+
+from hashlib import md5
+
+
+def get_md5_prefix(to_hash, length):
+    m = md5(to_hash.encode())
+    prefix = m.hexdigest()[0:length]
+    return prefix
+
+
+def find_lowest_for_prefix(key, prefix):
+    length = len(prefix)
+    number = 1
+
+    while True:
+        to_hash = key + str(number)
+        hashed_prefix = get_md5_prefix(to_hash, length)
+
+        if hashed_prefix == prefix:
+            break
+        number += 1
+
+    return number
+
+
+if __name__ == "__main__":
+    lowest = find_lowest_for_prefix("bgvyzdsv", "000000")
+    print(lowest)
--- 2015/04/test.py
+++ 2015/04/test.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
 import unittest
+from puzzle_2015_04a import get_md5_prefix, find_lowest_for_prefix
+
+class AdventCoinTest(unittest.TestCase):
+    def test_get_md5_prefix(self):
+        self.assertEqual(get_md5_prefix(to_hash="abcdef609043", length=5), "00000")
+        self.assertEqual(get_md5_prefix(to_hash="abcdef609143", length=5), "ba9c6")
+
+    def test_find_lowest_for_prefix(self):
+        self.assertEqual(find_lowest_for_prefix(key="abcdef", prefix="00000"), 609043)
+        self.assertEqual(find_lowest_for_prefix(key="pqrstuv", prefix="00000"), 1048970)
+
+if __name__ == "__main__":
+    unittest.main()
