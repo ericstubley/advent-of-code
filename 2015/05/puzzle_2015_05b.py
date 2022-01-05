d91b57fff3c9d1dafe4d0f59d60c4f95c8f0a19f
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-05 10:43:13 -0500

    Day 05 solution

--- 2015/05/puzzle_2015_05b.py
+++ 2015/05/puzzle_2015_05b.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+
+def pair_appears_twice(string):
+    ret = False
+    for i in range(len(string) - 1):
+        pair = string[i:i+2]
+        if pair in string[i+2:]:
+            ret = True
+            break
+    return ret
+
+
+def has_xyx_pattern(string):
+    ret = False
+    for i in range(len(string) - 2):
+        if string[i] == string[i+2]:
+            ret = True
+            break
+    return ret
+
+
+def is_nice_string(string):
+    cond_1 = pair_appears_twice(string)
+    cond_2 = has_xyx_pattern(string)
+    return (cond_1 and cond_2)
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    strings = [__.rstrip('\n') for __ in data]
+
+    nice_count = len([__ for __ in strings if is_nice_string(__)])
+    print(nice_count)
--- 2015/05/test.py
+++ 2015/05/test.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
 
 import unittest
+import puzzle_2015_05a as a
+import puzzle_2015_05b as b
+
+
+class NiceStringTest(unittest.TestCase):
+    def test_is_nice_string_a(self):
+        self.assertTrue(a.is_nice_string("ugknbfddgicrmopn"))
+        self.assertTrue(a.is_nice_string("aaa"))
+        self.assertFalse(a.is_nice_string("jchzalrnumimnmhp"))
+        self.assertFalse(a.is_nice_string("haegwjzuvuyypxyu"))
+        self.assertFalse(a.is_nice_string("dvszwmarrgswjxmb"))
+
+    def test_has_three_vowels(self):
+        self.assertTrue(a.has_three_vowels("aei"))
+        self.assertTrue(a.has_three_vowels("xazegov"))
+        self.assertTrue(a.has_three_vowels("aeiouaeiouaeiou"))
+        self.assertFalse(a.has_three_vowels("dvszwmarrgswjxmb"))
+
+    def test_has_double_letter(self):
+        self.assertTrue(a.has_double_letter("xx"))
+        self.assertTrue(a.has_double_letter("abcdde"))
+        self.assertTrue(a.has_double_letter("aabbccdd"))
+        self.assertFalse(a.has_double_letter("jchzalrnumimnmhp"))
+
+    def test_has_banned_substring(self):
+        self.assertFalse(a.has_banned_substring("ugknbfddgicrmopn"))
+        self.assertTrue(a.has_banned_substring("haegwjzuvuyypxyu"))
+
+    def test_is_nice_string_b(self):
+        self.assertTrue(b.is_nice_string("qjhvhtzxzqqjkmpb"))
+        self.assertTrue(b.is_nice_string("xxyxx"))
+        self.assertFalse(b.is_nice_string("uurcxstgmygtbstg"))
+        self.assertFalse(b.is_nice_string("ieodomkazucvgmuy"))
+
+    def test_pair_appears_twice(self):
+        self.assertTrue(b.pair_appears_twice("xyxy"))
+        self.assertTrue(b.pair_appears_twice("aabcdefgaa"))
+        self.assertFalse(b.pair_appears_twice("aaa"))
+
+    def test_has_xyx_pattern(self):
+        self.assertTrue(b.has_xyx_pattern("xyx"))
+        self.assertTrue(b.has_xyx_pattern("abcdefeghi"))
+        self.assertTrue(b.has_xyx_pattern("aaa"))
+        self.assertFalse(b.has_xyx_pattern("uurcxstgmygtbstg"))
+
+
+if __name__ == "__main__":
+    unittest.main()
