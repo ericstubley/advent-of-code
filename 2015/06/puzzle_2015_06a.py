57dbf31cd6e9cc205776fa9b754b1e552bdfca14
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-06 11:06:45 -0500

    Day 06 solution

--- 2015/06/puzzle_2015_06a.py
+++ 2015/06/puzzle_2015_06a.py
@@ -1,1 +1,54 @@
 #!/usr/bin/env python3
+
+import numpy as np
+
+
+def turn_off(lights):
+    return np.zeros(np.shape(lights), dtype=bool)
+
+
+def turn_on(lights):
+    return np.ones(np.shape(lights), dtype=bool)
+
+
+def toggle(lights):
+    return np.logical_not(lights)
+
+
+def get_range_from_text(string):
+    split_string = string.split(" ")
+    p1_string, p2_string = split_string[0], split_string[2]
+
+    x1, y1 = [int(__) for __ in p1_string.split(",")]
+    x2, y2 = [int(__)+1 for __ in p2_string.split(",")]
+
+    return x1, y1, x2, y2
+
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        instructions = f.readlines()
+
+    lights = np.zeros((1000, 1000), dtype=bool)
+
+    for i in instructions:
+        if i.startswith("turn off"):
+            s = i.lstrip("turn off ").rstrip('\n')
+            x1, y1, x2, y2 = get_range_from_text(s)
+
+            lights[x1:x2, y1:y2] = turn_off(lights[x1:x2, y1:y2])
+
+        elif i.startswith("turn on"):
+            s = i.lstrip("turn on ")    
+            x1, y1, x2, y2 = get_range_from_text(s)
+
+            lights[x1:x2, y1:y2] = turn_on(lights[x1:x2, y1:y2])
+
+        elif i.startswith("toggle"):
+            s = i.lstrip("toggle ")    
+            x1, y1, x2, y2 = get_range_from_text(s)
+
+            lights[x1:x2, y1:y2] = toggle(lights[x1:x2, y1:y2])
+
+    print(np.sum(lights))
--- 2015/06/test.py
+++ 2015/06/test.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 
 import unittest
+import numpy as np
+
+import puzzle_2015_06a as a
+import puzzle_2015_06b as b
+
+
+class LightInstructionsTest(unittest.TestCase):
+    def test_get_range_from_text(self):
+        self.assertEqual(a.get_range_from_text("887,9 through 959,629"), (887, 9, 960, 630))
+
+    def test_part_b_instructions(self):
+        test_array = np.ones((2, 2), dtype=int)
+        zero_array = np.zeros((2, 2), dtype=int)
+
+        self.assertIsNone(np.testing.assert_array_equal(b.turn_on(test_array), 2*test_array))
+        self.assertIsNone(np.testing.assert_array_equal(b.toggle(test_array), 3*test_array))
+        self.assertIsNone(np.testing.assert_array_equal(b.turn_off(test_array), zero_array))
+        self.assertIsNone(np.testing.assert_array_equal(b.turn_off(zero_array), zero_array))
+
+
+if __name__ == "__main__":
+    unittest.main()
