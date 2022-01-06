948670b63f87189f34590d34d808c3da3100b397
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-06 13:57:30 -0500

    Day 07 solution

--- 2015/07/puzzle_2015_07b.py
+++ 2015/07/puzzle_2015_07b.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+from circuit import *
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        instructions = f.readlines()
+
+    # for part b we override the input to wire b to be the answer from part a
+    # that answer is 3176
+    instructions[3] = "3176 -> b\n"
+
+    builder = CircuitBuilder()
+    builder.build_circuit_from_list(instructions)
+
+    circuit = builder.circuit
+    circuit.compute_outputs()
+
+    print(circuit.output_to_int('a'))
--- 2015/07/test.py
+++ 2015/07/test.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python3
 
 import unittest
+from circuit import *
+
+
+class CircuitTest(unittest.TestCase):
+    def setUp(self):
+        self.builder = CircuitBuilder()
+
+        with open("test_input.txt") as f:
+            self.instructions = f.readlines()
+
+        self.builder.build_circuit_from_list(self.instructions)
+        self.circuit = self.builder.circuit
+
+        self.circuit.compute_outputs()
+
+    def test_the_simple_circuit(self):
+        """ d: 72
+            e: 507
+            f: 492
+            g: 114
+            h: 65412
+            i: 65079
+            x: 123
+            y: 456"""
+        self.assertEqual(self.circuit.output_to_int('d'), 72)
+        self.assertEqual(self.circuit.output_to_int('e'), 507)
+        self.assertEqual(self.circuit.output_to_int('f'), 492)
+        self.assertEqual(self.circuit.output_to_int('g'), 114)
+        self.assertEqual(self.circuit.output_to_int('h'), 65412)
+        self.assertEqual(self.circuit.output_to_int('i'), 65079)
+        self.assertEqual(self.circuit.output_to_int('x'), 123)
+        self.assertEqual(self.circuit.output_to_int('y'), 456)
+
+
+if __name__ == "__main__":
+    unittest.main()
