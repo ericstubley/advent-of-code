9352e3c802156c8c335dbfe7f035e07cf1c24067
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-09 09:22:21 -0500

    Day 09 solution

--- 2015/09/puzzle_2015_09b.py
+++ 2015/09/puzzle_2015_09b.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3
+
+from itertools import permutations
+
+
+def parse_data(data):
+    places = set()
+    distances = dict()
+
+    for line in data:
+        s1, s2 = line.split(" = ")
+        p1, p2 = s1.split(" to ")
+
+        places.add(p1)
+        places.add(p2)
+
+        dist = int(s2)
+        distances[(p1, p2)] = dist
+        distances[(p2, p1)] = dist
+
+    return places, distances
+
+
+def longest_path(places, distances):
+    path_lengths = [path_sum(__, distances) for __ in permutations(places)]
+    return max(path_lengths)
+
+
+def path_sum(path, distances):
+    distance = 0
+    for i in range(len(path) - 1):
+        distance += distances[(path[i], path[i+1])]
+    return distance
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    data = [__.rstrip('\n') for __ in data]
+    places, distances = parse_data(data)
+
+    print(longest_path(places, distances))
--- 2015/09/test.py
+++ 2015/09/test.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
 import unittest
+from puzzle_2015_09a import parse_data, shortest_path
+from puzzle_2015_09b import longest_path
+
+
+class SantaPathFinder(unittest.TestCase):
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        strings = [__.rstrip('\n') for __ in data]
+        self.places, self.distances = parse_data(strings)
+
+    def test_shortest_path(self):
+        self.assertEqual(shortest_path(self.places, self.distances), 605)
+
+    def test_longest_path(self):
+        self.assertEqual(longest_path(self.places, self.distances), 982)
+
+
+if __name__ == "__main__":
+    unittest.main()
