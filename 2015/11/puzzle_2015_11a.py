95d02eaa675bbc69ee808cf1db7e0c15fc033a80
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-11 09:34:39 -0500

    Day 11 solution

--- 2015/11/puzzle_2015_11a.py
+++ 2015/11/puzzle_2015_11a.py
@@ -1,1 +1,49 @@
 #!/usr/bin/env python3
+
+def is_valid_password(pwd):
+    cond1 = has_run(pwd)
+    cond2 = no_banned_letters(pwd)
+    cond3 = has_doubles(pwd)
+    return (cond1 and cond2 and cond3)
+
+
+def has_run(pwd):
+    ret = False
+    for i in range(len(pwd) - 2):
+        if ord(pwd[i+1]) == ord(pwd[i]) + 1 and ord(pwd[i+2]) == ord(pwd[i]) + 2:
+            ret = True
+            break
+    return ret        
+
+
+def no_banned_letters(pwd):
+    return (('i' not in pwd) and ('o' not in pwd) and ('l' not in pwd))
+
+
+def has_doubles(pwd):
+    doubles = set()
+    for i in range(len(pwd) - 1):
+        if pwd[i+1] == pwd[i]:
+            doubles.add(pwd[i])
+    return len(doubles) >= 2
+
+
+def next_valid(pwd):
+    possible = next_string(pwd)
+    while not is_valid_password(possible):
+        possible = next_string(possible)
+    return possible
+
+
+def next_string(pwd):
+    if pwd[-1] != 'z':
+        head = pwd[:-1]
+        tail = chr(ord(pwd[-1])+1)
+    else:
+        head = next_string(pwd[:-1])
+        tail = 'a'
+    return head + tail
+
+
+if __name__ == "__main__":
+    print(next_valid("vzbxkghb"))
--- 2015/11/test.py
+++ 2015/11/test.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
 import unittest
+from puzzle_2015_11a import is_valid_password, next_valid, next_string
+
+class PasswordTester(unittest.TestCase):
+    def test_is_valid_password(self):
+        self.assertFalse(is_valid_password("hijklmmn"))
+        self.assertFalse(is_valid_password("abbceffg"))
+        self.assertFalse(is_valid_password("abbcegjk"))
+
+    def test_next_valid(self):
+        self.assertEqual(next_valid("abcdefgh"), "abcdffaa")
+        self.assertEqual(next_valid("ghijklmn"), "ghjaabcc")
+
+    def test_next_string(self):
+        self.assertEqual(next_string("xy"), "xz")
+        self.assertEqual(next_string("xz"), "ya")
+
+
+if __name__ == "__main__":
+    unittest.main()
