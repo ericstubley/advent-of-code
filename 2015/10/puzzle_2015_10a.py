e2fa92fbadff5dfc32e4f00be14aea536a47f477
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-10 19:11:39 -0500

    Day 10 solution

--- 2015/10/puzzle_2015_10a.py
+++ 2015/10/puzzle_2015_10a.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+def look_and_say(seq):
+    new_seq = []
+    i = 0
+    while i < len(seq):
+        digit = seq[i]
+        count = 0
+        while i < len(seq) and seq[i] == digit:
+            count += 1
+            i += 1
+
+        new_seq.append(count)
+        new_seq.append(digit)
+    return new_seq
+
+
+if __name__ == "__main__":
+    start = "1321131112"
+    seq = [int(__) for __ in start]
+
+    for i in range(40):
+        seq = look_and_say(seq)
+
+    print(len(seq))
--- 2015/10/test.py
+++ 2015/10/test.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
 import unittest
+from puzzle_2015_10a import look_and_say
+from puzzle_2015_10b import look_and_say as look_and_say_b
+
+class LookAndSayTest(unittest.TestCase):
+    def test_look_and_say(self):
+        self.assertEqual(look_and_say([1]), [1, 1])
+        self.assertEqual(look_and_say([1, 1]), [2, 1])
+        self.assertEqual(look_and_say([2, 1]), [1, 2, 1, 1])
+        self.assertEqual(look_and_say([1, 2, 1, 1]), [1, 1, 1, 2, 2, 1])
+        self.assertEqual(look_and_say([1, 1, 1, 2, 2, 1]), [3, 1, 2, 2, 1, 1])
+
+    def test_look_and_say_b(self):
+        self.assertEqual(look_and_say_b([1]), [1, 1])
+        self.assertEqual(look_and_say_b([1, 1]), [2, 1])
+        self.assertEqual(look_and_say_b([2, 1]), [1, 2, 1, 1])
+        self.assertEqual(look_and_say_b([1, 2, 1, 1]), [1, 1, 1, 2, 2, 1])
+        self.assertEqual(look_and_say_b([1, 1, 1, 2, 2, 1]), [3, 1, 2, 2, 1, 1])
+
+if __name__ == "__main__":
+    unittest.main()
