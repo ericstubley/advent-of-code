76ab0d5d7a044bf969033a901df7e877b30c4775
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-12 12:08:20 -0500

    Day 12 solution

--- 2015/12/puzzle_2015_12a.py
+++ 2015/12/puzzle_2015_12a.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+def number_sum(string):
+
+    total = 0
+    curr = []
+
+    for c in string:
+        if curr == [] and (c == '-' or c.isdigit()):
+            curr.append(c)
+        elif curr != [] and c.isdigit():
+            curr.append(c)
+        elif curr != [] and not c.isdigit():
+            number = int("".join(curr))
+            total += number
+            curr = []
+
+    return total
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        json_data = f.read()
+
+    print(number_sum(json_data))
