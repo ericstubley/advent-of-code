cd6d87ec31c90fe3731c83a1b7aa0f3961d47636
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-13 09:40:19 -0500

    Day 13 solution

--- 2015/13/puzzle_2015_13b.py
+++ 2015/13/puzzle_2015_13b.py
@@ -1,1 +1,59 @@
 #!/usr/bin/env python3
+
+from itertools import permutations
+
+
+def happiness(seating, preferences):
+    h = 0
+    for i in range(len(seating)):
+        left = seating[i]
+        right = seating[0] if i == len(seating) - 1 else seating[i+1]
+
+        h += preferences[left][right]
+        h += preferences[right][left]
+    return h
+
+
+def find_optimal(preferences):
+    names = list(preferences.keys())
+
+    optimal = 0  # not actually a good choice
+    for p in permutations(names):
+        h = happiness(p, preferences)
+        if h > optimal:
+            optimal = h
+    return optimal
+
+
+def process_input(lines):
+    preferences = {}
+
+    for line in lines:
+        split = line.rstrip('\n').split(' ')
+
+        name_1 = split[0]
+        name_2 = split[10][:-1]  # it ends in a period
+        sign = -1 if split[2] == "lose" else 1
+        amount = int(split[3])
+
+        if name_1 in preferences:
+            preferences[name_1][name_2] = sign*amount
+        else:
+            preferences[name_1] = {name_2: sign*amount}
+    return preferences
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    preferences = process_input(data)
+
+    # add yourself to this list
+    names = list(preferences.keys())
+    preferences['Yourself'] = dict()
+    for name in names:
+        preferences['Yourself'][name] = 0
+        preferences[name]['Yourself'] = 0
+
+    print(find_optimal(preferences))
--- 2015/13/test.py
+++ 2015/13/test.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
 import unittest
+import puzzle_2015_13a as a
+
+
+class SeatingTest(unittest.TestCase):
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        self.preferences = a.process_input(data)
+
+    def test_happiness(self):
+        self.assertEqual(a.happiness(list(self.preferences.keys()), self.preferences), 330)
+
+    def test_find_optimal(self):
+        self.assertEqual(a.find_optimal(self.preferences), 330)
+
+
+if __name__ == "__main__":
+    unittest.main()
