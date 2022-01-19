83845fa8a55ab2bb5c612d835d3d7ec0c3acbce6
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-19 13:10:25 -0500

    Day 19 solution

--- 2015/19/puzzle_2015_19a.py
+++ 2015/19/puzzle_2015_19a.py
@@ -1,1 +1,41 @@
 #!/usr/bin/env python3
+
+from collections import defaultdict
+
+
+def generate_molecules(string, rules):
+    molecules = set()
+
+    for i in range(len(string)):
+        if string[i] in rules:
+            for r in rules[string[i]]:
+                molecules.add(operation(string, i, string[i], r)) 
+        if i < len(string)-1 and string[i:i+2] in rules: 
+            for r in rules[string[i:i+2]]:
+                molecules.add(operation(string, i, string[i:i+2], r)) 
+
+    return molecules
+
+def operation(string, index, before, after):
+    ret = [string[0:index], after, string[index+len(before):]]
+    return "".join(ret)
+
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    rules = defaultdict(list)
+    for line in data:
+        if line == "\n":
+            break
+
+        split = line.rstrip('\n').split(" ")
+        rules[split[0]].append(split[2])
+
+
+    string = data[-1].rstrip('\n')
+
+
+    print(len(generate_molecules(string, rules)))
--- 2015/19/puzzle_2015_19b.py
+++ 2015/19/puzzle_2015_19b.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+from collections import defaultdict
+
+TOKENS = ['Al', 'Ar', 'B', 'C', 'Ca', 'F', 'H', 'Mg', 'N', 'O', 'P', 'Rn', 'Si', 'Th', 'Ti', 'Y']
+assert len(TOKENS) == 16
+
+if __name__ == "__main__":
+
+    with open("medicine.txt") as f:
+        medicine = f.read()
+
+    # this test example should give 3 steps
+    # medicine = "CRnFYFYFArCaF"
+
+    # find how many of each token there are
+    token_counts = defaultdict(int)
+    for t in TOKENS:
+        token_counts[t] += medicine.count(t)
+    token_counts['C'] = 1
+
+    # number of steps is number of tokens - all the Rn/Ar - twice the Y + 1
+    total_tokens = sum([token_counts[__] for __ in token_counts])
+    num_steps = total_tokens
+    num_steps -= token_counts['Rn']
+    num_steps -= token_counts['Ar']
+    num_steps -= 2*token_counts['Y']
+    num_steps -= 1
+
+    print(num_steps)
