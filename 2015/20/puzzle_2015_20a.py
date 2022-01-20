d6aa0be126488ea0f45ac18f09604cc27148b02b
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-20 10:04:23 -0500

    Day 20 solution

--- 2015/20/puzzle_2015_20a.py
+++ 2015/20/puzzle_2015_20a.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+import numpy as np
+
+
+def divisor_sum(n):
+    ds = 0
+    for i in range(1, n+1):
+        if n%i == 0:
+            ds += i
+    return ds
+
+
+if __name__ == "__main__":
+
+    # naive upper bound from experiments of 1021020 = 2 * 2 * 3 * 5 * 7 * 11 * 13 * 17
+    # 1000000 is the new upper bound
+
+    houses = np.zeros(1000000, dtype=int)
+    for elf in range(1000000):
+        houses[elf::elf+1] += 10*(elf+1)
+
+    print(min([i+1 for i in range(1000000) if houses[i] >= 36000000]))
--- 2015/20/puzzle_2015_20b.py
+++ 2015/20/puzzle_2015_20b.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+from collections import defaultdict
+
+if __name__ == "__main__":
+    houses = defaultdict(int)
+    elf = 1
+    for i in range(1, 51):
+        houses[elf*i] += 11*elf
+
+    while houses[elf] < 36000000:
+        del houses[elf] 
+
+        elf += 1
+        print(f"\r{elf}", end="")
+        for i in range(1, 51): 
+            houses[elf*i] += 11*elf
+     
+    print(" ")
+    print(elf, houses[elf])
