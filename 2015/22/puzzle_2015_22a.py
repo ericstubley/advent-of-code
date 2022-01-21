b1adb3ecae0266adb7b013dc20384aa0c6a2b8d4
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-21 11:59:01 -0500

    Day 22 solution

--- 2015/22/puzzle_2015_22a.py
+++ 2015/22/puzzle_2015_22a.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+
+from functools import cache
+from spell import *
+
+@cache
+def min_mana_to_win(gs):
+    if gs.player_hp <= 0 or gs.boss_hp <= 0:
+        return (gs.boss_hp <= 0, 0)
+
+    if gs.turn == "boss":
+        return min_mana_to_win(advance_turn(gs, None))
+
+    else:
+        winning_outcomes = []
+        for s in SPELLS:
+            if s.is_castable(gs):
+                outcome, spent = min_mana_to_win(advance_turn(gs, s))
+                if outcome == True:
+                    winning_outcomes.append((outcome, spent + s.cost))
+
+        if winning_outcomes != []:
+            ret = (True, min([x[1] for x in winning_outcomes]))
+        else:
+            ret = (False, 0)
+        return ret
+
+
+def advance_turn(gs, action):
+    # do the ongoing effects
+    for s in SPELLS:
+        gs = s.effect(gs)
+
+    if gs.turn == "player":
+        assert action.is_castable(gs)
+        post_cast = action.cast(gs)
+        return post_cast._replace(turn="boss")
+
+    else:  # gs.turn == "boss" 
+        new_player_hp = gs.player_hp - gs.damage
+        return gs._replace(player_hp=new_player_hp, turn="player")
+
+
+if __name__ == "__main__":
+    print(min_mana_to_win(INITIAL_GAME_STATE))
--- 2015/22/test.py
+++ 2015/22/test.py
@@ -1,3 +1,58 @@
 #!/usr/bin/env python3
 
 import unittest
+from spell import *
+import puzzle_2015_22a as a
+import puzzle_2015_22b as b
+
+
+class RPGTest(unittest.TestCase):
+    def setUp(self):
+        INITIAL_GAME_STATE = GameState(10, 14, 250, 8, 0, 0, 0, "player")
+        self.gs = INITIAL_GAME_STATE
+
+
+    def test_run_game(self):
+        turn_0 = GameState(10, 14, 250, 8, 0, 0, 0, "player")
+        self.assertEqual(self.gs, turn_0)
+
+        self.gs = a.advance_turn(self.gs, Recharge())
+        turn_1 = GameState(10, 14, 21, 8, 0, 0, 5, "boss")
+        self.assertEqual(self.gs, turn_1)
+
+        self.gs = a.advance_turn(self.gs, None)
+        turn_2 = GameState(2, 14, 122, 8, 0, 0, 4, "player")
+        self.assertEqual(self.gs, turn_2)
+
+        self.gs = a.advance_turn(self.gs, Shield())
+        turn_3 = GameState(2, 14, 110, 8, 6, 0, 3, "boss")
+        self.assertEqual(self.gs, turn_3)
+
+        self.gs = a.advance_turn(self.gs, None)
+        turn_4 = GameState(1, 14, 211, 1, 5, 0, 2, "player")
+        self.assertEqual(self.gs, turn_4)
+
+        self.gs = a.advance_turn(self.gs, Drain())
+        turn_5 = GameState(3, 12, 239, 1, 4, 0, 1, "boss")
+        self.assertEqual(self.gs, turn_5)
+
+        self.gs = a.advance_turn(self.gs, None)
+        turn_6 = GameState(2, 12, 340, 1, 3, 0, 0, "player")
+        self.assertEqual(self.gs, turn_6)
+
+        self.gs = a.advance_turn(self.gs, Poison())
+        turn_7 = GameState(2, 12, 167, 1, 2, 6, 0, "boss")
+        self.assertEqual(self.gs, turn_7)
+
+        self.gs = a.advance_turn(self.gs, None)
+        turn_8 = GameState(1, 9, 167, 1, 1, 5, 0, "player")
+        self.assertEqual(self.gs, turn_8)
+
+        self.gs = a.advance_turn(self.gs, MagicMissile())
+        turn_9 = GameState(1, 2, 114, 1, 0, 4, 0, "boss")
+        self.assertEqual(self.gs, turn_9)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
