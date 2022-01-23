8b06107d81910b35c64f34468965cb66e7ba125e
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-23 10:03:44 -0500

    Day 23 solution

--- 2015/23/test.py
+++ 2015/23/test.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
 import unittest
+import puzzle_2015_23a as a
+import puzzle_2015_23b as b
+
+class TuringLockTest(unittest.TestCase):
+    def setUp(self):
+        with open("test_input.txt") as f:
+            test_instructions = f.readlines()
+        self.instructions = a.parse_instructions(test_instructions)
+
+    def test_program(self):
+        self.assertTupleEqual(a.run_program(self.instructions), (2, 0))
+
+
+if __name__ == "__main__":
+    unittest.main()
