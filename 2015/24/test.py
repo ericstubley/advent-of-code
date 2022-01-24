b2513662b2b3f13513da7ca5fb241a7a45ee1257
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-24 11:18:18 -0500

    Day 24 solution

--- 2015/24/test.py
+++ 2015/24/test.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
 import unittest
+import puzzle_2015_24a as a
+import puzzle_2015_24b as b
+
+
+class SleighPackerTest(unittest.TestCase):
+    def setUp(self):
+        self.test_weights = (1, 2, 3, 4, 5, 7, 8, 9, 10, 11)
+
+    def test_ideal_first_group(self):
+        self.assertTupleEqual(a.ideal_first_group(self.test_weights), (9, 11))
+
+
+if __name__ == "__main__":
+    unittest.main()
