211d844dba6a21c2e2a26eaf720b6b12225f2bd9
Eric Stubley <ericdavidstubley@gmail.com>
2022-01-25 08:52:56 -0500

    Day 25 solution

--- 2015/25/puzzle_2015_25a.py
+++ 2015/25/puzzle_2015_25a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+C0 = 20151125
+MOD = 33554393
+MULT = 252533
+
+def code_by_index(n):
+    return (C0 * pow(MULT, n-1, MOD)) % MOD
+
+
+def code_by_grid(row, col):
+    diag_end = row + col - 1
+    triangle = (diag_end * (diag_end + 1)) // 2
+    n = triangle - row + 1
+
+    return code_by_index(n)
+
+
+if __name__ == "__main__":
+    print(code_by_grid(2947, 3029))
