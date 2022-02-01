26fd859b51e2822edabb1dd45717e7e976fd37bd
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-01 09:30:57 -0500

    Day 01 solution

--- 2016/01/puzzle_2016_01.py
+++ 2016/01/puzzle_2016_01.py
@@ -1,10 +1,75 @@
 #! /usr/bin/env python3
 
+LEFT_TURN = {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}
+RIGHT_TURN = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
+DIR_TO_VECTOR = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
+
+
+def parse_instructions(string):
+    ls = string.rstrip("\n").split(", ")
+    ls = [(s[0], int(s[1:])) for s in ls]
+    return ls
+
+
+def total_distance(instructions):
+    x, y, d = 0, 0, 'N'
+    for turn, dist in instructions:
+        if turn == 'L':
+            d = LEFT_TURN[d]
+        else:
+            d = RIGHT_TURN[d]
+
+        if d == 'N':
+            y += dist
+        elif d == 'E':
+            x += dist
+        elif d == 'S':
+            y -= dist
+        elif d == 'W':
+            x -= dist
+
+    return abs(x) + abs(y)
+
+
+def first_visit_twice(instructions):
+    x, y, d = 0, 0, 'N'
+    visited = set()
+    visited.add((0, 0))
+    for turn, dist in instructions:
+        if turn == 'L':
+            d = LEFT_TURN[d]
+        else:
+            d = RIGHT_TURN[d]
+
+        dir_x, dir_y = DIR_TO_VECTOR[d]
+
+        revisit = False
+        for __ in range(1, dist+1):
+            x, y = x + dir_x, y + dir_y
+
+            if (x, y) in visited:
+                revisit = True
+                break
+            else:
+                visited.add((x, y))
+        if revisit:
+            break
+
+    return abs(x) + abs(y)
+
 def main_a():
-    pass
+    with open("input.txt") as f:
+        data = f.read()
+
+    instructions = parse_instructions(data)
+    print(total_distance(instructions))
 
 def main_b():
-    pass
+    with open("input.txt") as f:
+        data = f.read()
+
+    instructions = parse_instructions(data)
+    print(first_visit_twice(instructions))
 
 if __name__ == "__main__":
     main_a()
--- 2016/01/test_2016_01.py
+++ 2016/01/test_2016_01.py
@@ -5,7 +5,19 @@ import puzzle_2016_01 as puz
 
 
 class Test_2016_01(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.instructions_1 = puz.parse_instructions("R2, L3")
+        self.instructions_2 = puz.parse_instructions("R2, R2, R2")
+        self.instructions_3 = puz.parse_instructions("R5, L5, R5, R3")
+        self.instructions_4 = puz.parse_instructions("R8, R4, R4, R8")
+
+    def test_total_distance(self):
+        self.assertEqual(puz.total_distance(self.instructions_1), 5)
+        self.assertEqual(puz.total_distance(self.instructions_2), 2)
+        self.assertEqual(puz.total_distance(self.instructions_3), 12)
+
+    def test_first_visit_twice(self):
+        self.assertEqual(puz.first_visit_twice(self.instructions_4), 4)
 
 
 if __name__ == "__main__":
