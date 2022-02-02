c0c4b2ec303c2d9af1c21ecc54c834d38a9c1d93
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-02 09:03:03 -0500

    Day 02 solution

--- 2016/02/puzzle_2016_02.py
+++ 2016/02/puzzle_2016_02.py
@@ -1,11 +1,57 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+DIR_TO_VEC = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
+KEYPAD_A = {(-1, 1): '1',
+    (0, 1): '2',
+    (1, 1): '3',
+    (-1, 0): '4',
+    (0, 0): '5',
+    (1, 0): '6',
+    (-1, -1): '7',
+    (0, -1): '8',
+    (1, -1): '9'}
+KEYPAD_B = {(0, 0): '5',
+    (1, 0): '6',
+    (2, 0): '7',
+    (3, 0): '8',
+    (4, 0): '9',
+    (1, 1): '2',
+    (2, 1): '3',
+    (3, 1): '4',
+    (2, 2): '1',
+    (1, -1): 'A',
+    (2, -1): 'B',
+    (3, -1): 'C',
+    (2, -2): 'D'}
 
-def main_b():
-    pass
+
+def parse(data):
+    return [__.rstrip('\n') for __ in data]
+
+
+def get_code_from_keypad(instructions, keypad):
+    x, y = 0, 0
+    ret = []
+    for i in instructions:
+        for c in i:
+            dx, dy = DIR_TO_VEC[c]
+            if (x + dx, y + dy) in keypad:
+                x, y = x + dx, y + dy
+        ret.append(keypad[(x, y)])
+    return "".join(ret)
+
+
+def main_a(instructions):
+    print(get_code_from_keypad(instructions, KEYPAD_A))
+
+
+def main_b(instructions):
+    print(get_code_from_keypad(instructions, KEYPAD_B))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    instructions = parse(data)
+    main_a(instructions)
+    main_b(instructions)
--- 2016/02/test_2016_02.py
+++ 2016/02/test_2016_02.py
@@ -5,7 +5,17 @@ import puzzle_2016_02 as puz
 
 
 class Test_2016_02(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        self.instructions = puz.parse(data)
+
+    def test_get_code_a(self):
+        self.assertEqual(puz.get_code_from_keypad(self.instructions, puz.KEYPAD_A), "1985")
+
+    def test_get_code_b(self):
+        self.assertEqual(puz.get_code_from_keypad(self.instructions, puz.KEYPAD_B), "5DB3")
 
 
 if __name__ == "__main__":
