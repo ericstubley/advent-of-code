86aee9754658d8d24501d3d264518755481ddac8
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-03 08:59:09 -0500

    Day 03 solution

--- 2016/03/puzzle_2016_03.py
+++ 2016/03/puzzle_2016_03.py
@@ -1,11 +1,44 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def parse(data):
+    ret = []
+    for x in data:
+        ls = x.strip().split()
+        ret.append((int(ls[0]), int(ls[1]), int(ls[2])))
+    return ret
 
-def main_b():
-    pass
+
+def column_parse(data):
+    ret = []
+    for i in range(0, len(data), 3):
+        r1, r2, r3 = data[i], data[i+1], data[i+2]
+        s1, s2, s3 = r1.split(), r2.split(), r3.split()
+
+        ret.append((int(s1[0]), int(s2[0]), int(s3[0])))
+        ret.append((int(s1[1]), int(s2[1]), int(s3[1])))
+        ret.append((int(s1[2]), int(s2[2]), int(s3[2])))
+    return ret
+
+
+def is_valid_triangle(t):
+    m = max(t)
+    others = sum(t) - m
+    return others > m
+
+
+def main_a(triangles):
+    print(len([t for t in triangles if is_valid_triangle(t)]))
+
+
+def main_b(triangles):
+    print(len([t for t in triangles if is_valid_triangle(t)]))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    triangles_a = parse(data)
+    triangles_b = column_parse(data)
+
+    main_a(triangles_a)
+    main_b(triangles_b)
--- 2016/03/test_2016_03.py
+++ 2016/03/test_2016_03.py
@@ -5,7 +5,22 @@ import puzzle_2016_03 as puz
 
 
 class Test_2016_03(unittest.TestCase):
-    pass
+    def test_parse(self):
+        with open("input.txt") as f:
+            data = f.readlines()
+        triangles = puz.parse(data)
+        self.assertTupleEqual(triangles[0], (541, 588, 421))
+
+    def test_is_valid_triangle(self):
+        self.assertEqual(puz.is_valid_triangle((5, 10, 25)), False)
+        self.assertEqual(puz.is_valid_triangle((3, 4, 5)), True)
+
+
+    def test_column_parse(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+        triangles = puz.column_parse(data)
+        self.assertEqual(len([t for t in triangles if puz.is_valid_triangle(t)]), 6)
 
 
 if __name__ == "__main__":
