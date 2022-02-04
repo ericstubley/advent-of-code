d53cd6402dedbdaccc8fc4e4a680036817a97af2
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-04 08:57:32 -0500

    Day 04 solution

--- 2016/04/puzzle_2016_04.py
+++ 2016/04/puzzle_2016_04.py
@@ -1,11 +1,86 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from collections import namedtuple
 
-def main_b():
-    pass
+
+EncryptedRoom = namedtuple("EncryptedRoom", ["name", "sector", "checksum"])
+LETTERS = [chr(ord('a')+i) for i in range(26)]
+
+
+def parse(data):
+    ret = []
+    for line in data:
+        line = line.rstrip('\n')
+
+        # last characters are -XYZ[abcde]
+        # name is everything through -11
+        # sector is -10 through -7
+        # checksum is -6 through -1
+        name  = line[:-11]
+        sector = int(line[-10:-7])
+        checksum = line[-6:-1]
+
+        ret.append(EncryptedRoom(name=name, sector=sector, checksum=checksum))
+    return ret
+
+
+def chr_to_num(c):
+    return ord(c) - ord('a')
+
+def num_to_chr(n):
+    return chr(ord('a') + n)
+
+
+def letter_counts(name):
+    lc = [0]*26
+    for c in name:
+        num = chr_to_num(c)
+        if 0 <= num and num < 26:
+            lc[num] += 1
+    return lc
+
+
+def is_valid(room):
+    lc = letter_counts(room.name)
+    freqs = sorted(zip(lc, LETTERS), key=lambda x: (-1*x[0],x[1]))
+    checksum = "".join(__[1] for __ in freqs[0:5])
+
+    return checksum == room.checksum
+
+
+def decrypt_name(room):
+    decrypt_ls = []
+    shift = room.sector % 26
+    for c in room.name:
+        if c == '-':
+            decrypt_ls.append(' ')
+        else:
+            shifted = num_to_chr((chr_to_num(c) + shift) % 26)
+            decrypt_ls.append(shifted)
+    return "".join(decrypt_ls)
+
+
+def valid_sector_id_sum(rooms):
+    return sum(r.sector for r in rooms if is_valid(r))
+
+
+def main_a(rooms):
+    print(valid_sector_id_sum(rooms))
+
+
+def main_b(rooms):
+    for r in rooms:
+        if is_valid(r):
+            actual_name = decrypt_name(r)
+            if "north" in actual_name or "pole" in actual_name or "object" in actual_name or "stor" in actual_name:
+                print(actual_name, r.name, r.sector)
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    rooms = parse(data)
+
+    main_a(rooms)
+    main_b(rooms)
--- 2016/04/test_2016_04.py
+++ 2016/04/test_2016_04.py
@@ -5,7 +5,34 @@ import puzzle_2016_04 as puz
 
 
 class Test_2016_04(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        self.rooms = puz.parse(data)
+
+
+    def test_parse(self):
+        test_room = puz.EncryptedRoom(name="aaaaa-bbb-z-y-x", sector=123, checksum="abxyz")
+        self.assertEqual(self.rooms[0].name, test_room.name)
+        self.assertEqual(self.rooms[0].sector, test_room.sector)
+        self.assertEqual(self.rooms[0].checksum, test_room.checksum)
+
+
+    def test_is_valid(self):
+        self.assertTrue(puz.is_valid(self.rooms[0]))
+        self.assertTrue(puz.is_valid(self.rooms[1]))
+        self.assertTrue(puz.is_valid(self.rooms[2]))
+        self.assertFalse(puz.is_valid(self.rooms[3]))
+
+
+    def test_valid_sector_id_sum(self):
+        self.assertEqual(puz.valid_sector_id_sum(self.rooms), 1514)
+
+
+    def test_decrypt_name(self):
+        test_room = puz.EncryptedRoom(name="qzmt-zixmtkozy-ivhz", sector=343, checksum="aaaaa")
+        self.assertEqual(puz.decrypt_name(test_room), "very encrypted name")
 
 
 if __name__ == "__main__":
