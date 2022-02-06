ea3fe85f429760fb48ff66cf7153a62d23d003c0
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-06 08:38:53 -0500

    Day 06 solution

--- 2016/06/puzzle_2016_06.py
+++ 2016/06/puzzle_2016_06.py
@@ -1,11 +1,58 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from collections import defaultdict
 
-def main_b():
-    pass
+
+LETTERS = [chr(ord('a') + i) for i in range(26)]
+
+
+def parse(data):
+    return [__.rstrip('\n') for __ in data]
+
+
+def extract_message(messages, extractor):
+    counts = defaultdict(int)
+    for m in messages:
+        for i, l in enumerate(m):
+            counts[(i, l)] += 1
+    ret = []
+    for i in range(len(messages[0])):
+
+        index = extractor([counts[(i, l)] for l in LETTERS])
+        ret.append(chr(ord('a') + index))
+
+    return "".join(ret)
+
+
+def max_extractor(ls):
+    # given a list of counts for each letter, return index of max
+    max_count = max(ls)
+    index = ls.index(max_count)
+    return index
+
+
+def min_extractor(ls):
+    # return index of letter which has min non-zero count
+    min_index, min_count = 0, max(ls)
+    for i, x in enumerate(ls):
+        if x > 0 and x < min_count:
+            min_index = i
+            min_count = x
+    return min_index
+
+
+def main_a(messages):
+    print(extract_message(messages, max_extractor))
+
+
+def main_b(messages):
+    print(extract_message(messages, min_extractor))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    messages = parse(data)
+
+    main_a(messages)
+    main_b(messages)
--- 2016/06/test_2016_06.py
+++ 2016/06/test_2016_06.py
@@ -5,7 +5,16 @@ import puzzle_2016_06 as puz
 
 
 class Test_2016_06(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+        self.messages = puz.parse(data)
+
+    def test_extract_message_a(self):
+        self.assertEqual(puz.extract_message(self.messages, puz.max_extractor), "easter")
+
+    def test_extract_message_b(self):
+        self.assertEqual(puz.extract_message(self.messages, puz.min_extractor), "advent")
 
 
 if __name__ == "__main__":
