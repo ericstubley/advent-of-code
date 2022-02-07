cc782ee06de68dd127236b8e5d827e38123ae8b1
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-07 08:41:51 -0500

    Day 07 solution

--- 2016/07/puzzle_2016_07.py
+++ 2016/07/puzzle_2016_07.py
@@ -1,11 +1,66 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def parse(data):
+    return [__.rstrip("\n") for __ in data]
 
-def main_b():
-    pass
+
+def tls_support(ip):
+    ret = False
+    paren_stack = []
+    for i, c in enumerate(ip[:-3]):
+        if c == '[':
+            paren_stack.append(c)
+        elif c == ']':
+            paren_stack.pop()
+        elif ip[i:i+4].isalpha() and ip[i] != ip[i+1] and ip[i] == ip[i+3] and ip[i+1] == ip[i+2]:
+            if len(paren_stack) > 0:
+                ret = False
+                break
+            else:
+                ret = True
+    return ret
+
+
+def ssl_support(ip):
+    ret = False
+    paren_stack = []
+    super_patterns, hyper_patterns = set(), set() 
+    for i, c in enumerate(ip[:-2]):
+        if c == '[':
+            paren_stack.append(c)
+        elif c == ']':
+            paren_stack.pop()
+        else:
+            extract = ip[i:i+3]
+            if extract.isalpha() and extract[0] != extract[1] and extract[0] == extract[2]:
+                if len(paren_stack) > 0:
+                    if invert_pattern(extract) in super_patterns:
+                        ret = True
+                        break
+                    hyper_patterns.add(extract)
+                else:
+                    if invert_pattern(extract) in hyper_patterns:
+                        ret = True
+                        break
+                    super_patterns.add(extract)
+    return ret
+
+
+def invert_pattern(s):
+    # s is of the form ABA, return BAB
+    return "".join([s[1], s[0], s[1]])
+
+
+def main_a(ips):
+    print(len([x for x in ips if tls_support(x)]))
+
+def main_b(ips):
+    print(len([x for x in ips if ssl_support(x)]))
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    ips = parse(data)
+
+    main_a(ips)
+    main_b(ips)
--- 2016/07/test_2016_07.py
+++ 2016/07/test_2016_07.py
@@ -5,7 +5,32 @@ import puzzle_2016_07 as puz
 
 
 class Test_2016_07(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.ip_1 = "abba[mnop]qrst"
+        self.ip_2 = "abcd[bddb]xyyx"
+        self.ip_3 = "aaaa[qwer]tyui"
+        self.ip_4 = "ioxxoj[asdfgh]zxcvbn"
+
+        self.ip_5 = "aba[bab]xyz"
+        self.ip_6 = "xyx[xyx]xyx"
+        self.ip_7 = "aaa[kek]eke"
+        self.ip_8 = "zazbz[bzb]cdb"
+
+    def test_tls_support(self):
+        self.assertTrue(puz.tls_support(self.ip_1))
+        self.assertFalse(puz.tls_support(self.ip_2))
+        self.assertFalse(puz.tls_support(self.ip_3))
+        self.assertTrue(puz.tls_support(self.ip_4))
+
+    def test_ssl_support(self):
+        self.assertTrue(puz.ssl_support(self.ip_5))
+        self.assertFalse(puz.ssl_support(self.ip_6))
+        self.assertTrue(puz.ssl_support(self.ip_7))
+        self.assertTrue(puz.ssl_support(self.ip_8))
+
+
+    def test_invert_pattern(self):
+        self.assertEqual(puz.invert_pattern("aba"), "bab")
 
 
 if __name__ == "__main__":
