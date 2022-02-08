82230b98c6aa0b833800cac2742599b913afd696
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-08 09:10:19 -0500

    Day 08 solution

--- 2016/08/puzzle_2016_08.py
+++ 2016/08/puzzle_2016_08.py
@@ -1,11 +1,69 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+import numpy as np
 
-def main_b():
-    pass
+
+DISPLAY_SIZE = (6, 50)
+
+
+def parse(data):
+    instructions = []
+    for line in data:
+        split = line.rstrip("\n").split(" ")
+        if split[0] == "rect":
+            temp = split[1].split("x")
+            height, width = int(temp[1]), int(temp[0]) 
+            instructions.append(("rect", height, width))
+        elif split[0] == "rotate":
+            r_or_c = split[1]
+            index = int(split[2][2:])
+            distance = int(split[4])
+            instructions.append(("rotate", r_or_c, index, distance))
+    return instructions
+
+
+def operation(instruction, display):
+    if instruction[0] == "rect":
+        rect(instruction[1], instruction[2], display)
+    elif instruction[0] == "rotate" and instruction[1] == "row":
+        rotate_row(instruction[2], instruction[3], display)
+    elif instruction[0] == "rotate" and instruction[1] == "column":
+        rotate_col(instruction[2], instruction[3], display)
+
+
+def rect(height, width, display):
+    display[0:height, 0:width] = True
+
+
+def rotate_row(index, distance, display):
+    display[index, :] = np.roll(display[index, :], distance)
+
+
+def rotate_col(index, distance, display):
+    display[:, index] = np.roll(display[:, index], distance)
+
+
+def main_a(instructions):
+    display = np.zeros(DISPLAY_SIZE, dtype=bool)
+    for i in instructions:
+        operation(i, display)
+
+    print(display.sum()) 
+
+
+def main_b(instructions):
+    display = np.zeros(DISPLAY_SIZE, dtype=bool)
+    for i in instructions:
+        operation(i, display)
+
+    for row in display:
+        print("".join([" " if x == False else "#" for x in row]))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    instructions = parse(data)
+
+    main_a(instructions)
+    main_b(instructions)
--- 2016/08/test_2016_08.py
+++ 2016/08/test_2016_08.py
@@ -1,11 +1,49 @@
 #! /usr/bin/env python3
 
 import unittest
+import numpy as np
 import puzzle_2016_08 as puz
 
 
 class Test_2016_08(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines() 
+
+        self.instructions = puz.parse(data)
+        self.dimensions = (3, 7)
+        self.display = np.zeros(self.dimensions, dtype=bool)
+
+    def test_operations(self):
+        puz.operation(self.instructions[0], self.display)
+        result_0 = np.array(
+            [[True, True, True, False, False, False, False],
+            [True, True, True, False, False, False, False],
+            [False, False, False, False, False, False, False]], dtype=bool)
+        self.assertTrue(np.array_equal(self.display, result_0))
+
+        puz.operation(self.instructions[1], self.display)
+        result_1 = np.array(
+            [[True, False, True, False, False, False, False],
+            [True, True, True, False, False, False, False],
+            [False, True, False, False, False, False, False]], dtype=bool)
+        self.assertTrue(np.array_equal(self.display, result_1))
+
+        puz.operation(self.instructions[2], self.display)
+        result_2 = np.array(
+            [[False, False, False, False, True, False, True],
+            [True, True, True, False, False, False, False],
+            [False, True, False, False, False, False, False]], dtype=bool)
+        self.assertTrue(np.array_equal(self.display, result_2))
+
+        puz.operation(self.instructions[3], self.display)
+        result_3 = np.array(
+            [[False, True, False, False, True, False, True],
+            [True, False, True, False, False, False, False],
+            [False, True, False, False, False, False, False]], dtype=bool)
+        self.assertTrue(np.array_equal(self.display, result_3))
+         
+
 
 
 if __name__ == "__main__":
