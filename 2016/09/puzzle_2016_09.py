1dfa73566f4b2395ef7042f37f372af8314cc8dd
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-09 09:12:21 -0500

    Day 09 solution

--- 2016/09/puzzle_2016_09.py
+++ 2016/09/puzzle_2016_09.py
@@ -1,11 +1,49 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def parse_marker(data, i):
+    assert data[i] == '('
+    parse = []
+    i += 1
+    while data[i] != ')':
+        parse.append(data[i])
+        i += 1
+    i += 1
+    marker = "".join(parse).rstrip(')').lstrip('(')
+    split = marker.split('x')
+    chunk, repeat = int(split[0]), int(split[1])
+    return chunk, repeat, i
 
-def main_b():
-    pass
+
+def decompressed_length(data, recurse=False):
+    i = 0
+    length = 0
+    while i < len(data):
+        if data[i] == '(':
+            # parse off the two numbers
+            chunk, repeat, i = parse_marker(data, i)
+            # add decompressed length
+            if recurse:
+                length += repeat * decompressed_length(data[i:i+chunk], True)
+            else:
+                length += repeat * chunk
+            # advance pointer by appropriate amount
+            i += chunk
+        else:
+            length += 1
+            i += 1
+    return length
+
+
+def main_a(data):
+    print(decompressed_length(data, False))
+
+
+def main_b(data):
+    print(decompressed_length(data, True))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.read()
+    main_a(data)
+    main_b(data)
--- 2016/09/test_2016_09.py
+++ 2016/09/test_2016_09.py
@@ -5,7 +5,19 @@ import puzzle_2016_09 as puz
 
 
 class Test_2016_09(unittest.TestCase):
-    pass
+    def test_decompressed_length_v1(self):
+        self.assertEqual(puz.decompressed_length("ADVENT", False), 6)
+        self.assertEqual(puz.decompressed_length("A(1x5)BC", False), 7)
+        self.assertEqual(puz.decompressed_length("(3x3)XYZ", False), 9)
+        self.assertEqual(puz.decompressed_length("A(2x2)BCD(2x2)EFG", False), 11)
+        self.assertEqual(puz.decompressed_length("(6x1)(1x3)A", False), 6)
+        self.assertEqual(puz.decompressed_length("X(8x2)(3x3)ABCY", False), 18)
+
+    def test_decompressed_length_v2(self):
+        self.assertEqual(puz.decompressed_length("(3x3)XYZ", True), 9)
+        self.assertEqual(puz.decompressed_length("X(8x2)(3x3)ABCY", True), len("XABCABCABCABCABCABCY"))
+        self.assertEqual(puz.decompressed_length("(27x12)(20x12)(13x14)(7x10)(1x12)A", True), 241920)
+        self.assertEqual(puz.decompressed_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", True), 445)
 
 
 if __name__ == "__main__":
