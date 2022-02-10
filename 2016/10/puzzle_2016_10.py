47491d215e2223de1bd5f86c990d227fdda89095
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-10 09:09:22 -0500

    Day 10 solution

--- 2016/10/puzzle_2016_10.py
+++ 2016/10/puzzle_2016_10.py
@@ -1,11 +1,58 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from bot import *
 
-def main_b():
-    pass
+
+def parse(data):
+    bots, outputs, inputs = dict(), dict(), list()
+    for line in data:
+        split = line.rstrip("\n").split(" ")
+        if split[0] == "value":
+            inputs.append((int(split[1]), int(split[5])))
+        elif split[0] == "bot":
+            # handle low and high
+            handoffs = []
+            for i in [5, 10]:
+                num = int(split[i+1])
+                if split[i] == "output":
+                    if num not in outputs:
+                        outputs[num] = Output(num)
+                    handoffs.append(outputs[num])
+                else:
+                    if num not in bots:
+                        bots[num] = Bot(num) 
+                    handoffs.append(bots[num])
+
+            # handle actor 
+            actor_num = int(split[1])
+            if actor_num not in bots:
+                bots[actor_num] = Bot(actor_num)
+            bots[actor_num].low_output = handoffs[0]
+            bots[actor_num].high_output = handoffs[1]
+
+        else:
+            assert False
+    return bots, outputs, inputs
+
+
+def take_inputs(inputs, bots, outputs):
+    for val, bot_num in inputs:
+        bots[bot_num].add_value(val)
+
+
+def main_a(bots, outputs, inputs):
+    # the bot will print itself when it compares 17 and 61
+    take_inputs(inputs, bots, outputs)
+
+
+def main_b(bots, outputs, inputs):
+    print(outputs[0].value * outputs[1].value * outputs[2].value)
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+
+    bots, outputs, inputs = parse(data)
+    main_a(bots, outputs, inputs)
+    main_b(bots, outputs, inputs)
--- 2016/10/test_2016_10.py
+++ 2016/10/test_2016_10.py
@@ -5,7 +5,24 @@ import puzzle_2016_10 as puz
 
 
 class Test_2016_10(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        self.bots, self.outputs, self.inputs = puz.parse(data)
+
+    def test_parse(self):
+        self.assertEqual(len(self.bots), 3)
+        self.assertEqual(len(self.outputs), 3)
+        self.assertEqual(len(self.inputs), 3)
+
+
+    def test_run_actions(self):
+        puz.take_inputs(self.inputs, self.bots, self.outputs)
+        self.assertEqual(self.outputs[0].value, 5)
+        self.assertEqual(self.outputs[1].value, 2)
+        self.assertEqual(self.outputs[2].value, 3)
+
 
 
 if __name__ == "__main__":
