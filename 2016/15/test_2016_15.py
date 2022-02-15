34ccb7b68fac0fe6f9adb10c6e25e615334a6e61
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-15 08:53:56 -0500

    Day 15 solution

--- 2016/15/test_2016_15.py
+++ 2016/15/test_2016_15.py
@@ -5,7 +5,18 @@ import puzzle_2016_15 as puz
 
 
 class Test_2016_15(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        self.congruences = puz.parse_congruences(data)
+
+    def test_parse_congruences(self):
+        self.assertEqual(self.congruences[0], (5, 5))
+        self.assertEqual(self.congruences[1], (2, 3))
+
+    def test_crt(self):
+        self.assertEqual(puz.crt(self.congruences), 5)
 
 
 if __name__ == "__main__":
