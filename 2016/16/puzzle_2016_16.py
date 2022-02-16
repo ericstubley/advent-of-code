7798ff4e179226c8d13cced3868e4f1097b0e9ae
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-16 09:04:14 -0500

    Day 16 solution

--- 2016/16/puzzle_2016_16.py
+++ 2016/16/puzzle_2016_16.py
@@ -1,11 +1,41 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def checksum(bitstring):
+    cs = list(bitstring)
+    while len(cs) % 2 == 0:
+        ls = []
+        for i in range(len(cs)//2):
+            segment = cs[2*i:2*i+2]
+            if segment == ["0", "0"] or segment == ["1", "1"]:
+                ls.append("1")
+            else: # 10 or 01
+                ls.append("0")
+        cs = ls
+    return "".join(cs)
 
-def main_b():
-    pass
+
+def fill_to_length(length, initial):
+    cs = list(initial)
+    while len(cs) < length:
+        n = len(cs)
+        cs.append("0")
+        for i in range(n-1, -1, -1):
+            if cs[i] == "0":
+                cs.append("1")
+            else: 
+                cs.append("0")
+    return "".join(cs[:length])
+
+
+def main_a(length, initial):
+    print(checksum(fill_to_length(length, initial)))
+
+def main_b(length, initial):
+    print(checksum(fill_to_length(length, initial)))
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    disk_length_a = 272
+    disk_length_b = 35651584
+    initial = "10001110011110000"
+    main_a(disk_length_a, initial)
+    main_b(disk_length_b, initial)
--- 2016/16/test_2016_16.py
+++ 2016/16/test_2016_16.py
@@ -5,7 +5,15 @@ import puzzle_2016_16 as puz
 
 
 class Test_2016_16(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.initial = "10000"
+
+    def test_checksum(self):
+        self.assertEqual(puz.checksum("110010110100"), "100")
+        self.assertEqual(puz.checksum("10000011110010000111"), "01100")
+
+    def test_fill_to_length(self):
+        self.assertEqual(puz.fill_to_length(20, "10000"), "10000011110010000111")
 
 
 if __name__ == "__main__":
