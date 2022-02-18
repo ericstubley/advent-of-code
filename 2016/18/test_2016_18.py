7c45963560717dd124d5bdea6da40192fead599d
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-18 09:11:26 -0500

    Day 18 solution

--- 2016/18/test_2016_18.py
+++ 2016/18/test_2016_18.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python3
 
 import unittest
+import bitarray as ba
 import puzzle_2016_18 as puz
 
 
 class Test_2016_18(unittest.TestCase):
-    pass
+    def test_count_safe_tiles(self):
+        ba_1 = ba.bitarray([False, False, True, True, False])
+        ba_2 = ba.bitarray(".^^.^.^^^^".replace('^', '1').replace('.', '0'))
+        self.assertEqual(puz.count_safe_tiles(ba_1, 3), 6)
+        self.assertEqual(puz.count_safe_tiles(ba_2, 10), 38)
 
 
 if __name__ == "__main__":
