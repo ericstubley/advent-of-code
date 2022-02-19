9d83b60be988a92198e331a21eeeb902c8c875e0
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-19 10:37:20 -0500

    Day 19 solution

--- 2016/19/test_2016_19.py
+++ 2016/19/test_2016_19.py
@@ -5,7 +5,15 @@ import puzzle_2016_19 as puz
 
 
 class Test_2016_19(unittest.TestCase):
-    pass
+    def test_last_one_standing(self):
+        self.assertEqual(puz.last_one_standing_next(5), 3)
+        self.assertEqual(puz.last_one_standing_next(4), 1)
+        self.assertEqual(puz.last_one_standing_next(8), 1)
+        self.assertEqual(puz.last_one_standing_next(16), 1)
+
+    def test_last_one_standing_across(self):
+        self.assertEqual(puz.last_one_standing_across(5), 2)
+        self.assertEqual(puz.last_one_standing_across(6), 3)
 
 
 if __name__ == "__main__":
