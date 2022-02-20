d122173b6d5c019559c12f524eac3d9114c862eb
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-20 08:39:09 -0500

    Day 20 solution

--- 2016/20/puzzle_2016_20.py
+++ 2016/20/puzzle_2016_20.py
@@ -1,11 +1,57 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def parse(data):
+    ret = []
+    for line in data:
+        split = line.rstrip('\n').split('-')
+        lower, upper = int(split[0]), int(split[1])
+        assert lower <= upper
+        ret.append((lower, upper))
+    return ret
 
-def main_b():
-    pass
+
+def first_available(intervals):
+    ordered = sorted(intervals)
+    lower, upper = 0, 0
+    for l, u in ordered:
+        assert lower <= l
+        if l <= upper+1:
+            upper = max(upper, u)
+        else:
+            break
+
+    return upper + 1
+
+
+def num_allowed(intervals, maximum):
+    ordered = sorted(intervals)
+    count = 0
+    lower, upper = 0, 0
+    for l, u in ordered:
+        assert lower <= l
+        if l <= upper + 1:
+            upper = max(upper, u)
+        else:
+            count += l - upper - 1
+            lower, upper = l, u
+
+    count += maximum - upper
+
+    return count
+
+
+def main_a(intervals):
+    print(first_available(intervals))
+
+
+def main_b(intervals):
+    print(num_allowed(intervals, (1 << 32) - 1))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    intervals = parse(data)
+
+    main_a(intervals)
+    main_b(intervals)
--- 2016/20/test_2016_20.py
+++ 2016/20/test_2016_20.py
@@ -5,7 +5,19 @@ import puzzle_2016_20 as puz
 
 
 class Test_2016_20(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+        self.intervals = puz.parse(data)
+
+    def test_parse(self):
+        self.assertListEqual(self.intervals, [(5, 8), (0, 2), (4, 7)])
+
+    def test_first_available(self):
+        self.assertEqual(puz.first_available(self.intervals), 3)
+
+    def test_num_allowed(self):
+        self.assertEqual(puz.num_allowed(self.intervals, 9), 2)
 
 
 if __name__ == "__main__":
