41127c24dc0ea234352c4c8ea655890079fd90b1
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-22 08:26:19 -0500

    Day 22 solution

--- 2016/22/test_2016_22.py
+++ 2016/22/test_2016_22.py
@@ -5,7 +5,15 @@ import puzzle_2016_22 as puz
 
 
 class Test_2016_22(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("input.txt") as f:
+            data = f.readlines()
+        self.used, self.available = puz.parse(data, 30)
+
+
+    def test_parse(self):
+        self.assertEqual(self.used[18][24], 67)
+        self.assertEqual(self.available[18][24], 25)
 
 
 if __name__ == "__main__":
