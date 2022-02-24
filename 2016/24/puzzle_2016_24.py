a92270d5f8f7a8923f3d626100e868888294652f
Eric Stubley <ericdavidstubley@gmail.com>
2022-02-24 09:27:46 -0500

    Day 24 solution

--- 2016/24/puzzle_2016_24.py
+++ 2016/24/puzzle_2016_24.py
@@ -1,11 +1,96 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
 
-def main_b():
-    pass
+from itertools import permutations
+
+
+def neighbors(i, j):
+    # inputs are totally surrounded by walls, so you will never walk over
+    # the edges of the map; so this just returns the 4 possible moves without
+    # any boundary checking
+    return [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+
+
+def parse(data):
+    nodes = dict()
+    maze = []
+    for i, line in enumerate(data):
+        maze.append(line.rstrip('\n'))
+
+        for j, c in enumerate(line):
+            if c.isdigit():
+                nodes[int(c)] = (i, j)
+    return nodes, maze
+
+
+def shortest_roads(nodes, maze):
+    # given the node locations and maze, find the shortest maze path length
+    # (called the "road") from node x to node y for each pair (x, y)
+    roads = dict()
+
+    for n in nodes:
+        print(f"computing roads out of {n}")
+        curr_level = [nodes[n]]
+        curr_dist = 0
+        seen = set()
+        seen.add(nodes[n])
+
+
+        while len(curr_level) > 0:
+            next_level = []
+            for i, j in curr_level:
+                if maze[i][j].isdigit() and maze[i][j] != str(n):
+                    roads[(n, int(maze[i][j]))] = curr_dist
+
+                for di, dj in neighbors(i, j):
+                    if maze[di][dj] != '#' and (di, dj) not in seen:
+                        next_level.append((di, dj))
+                        seen.add((di, dj))
+
+            curr_level = next_level
+            curr_dist += 1
+    return roads
+
+
+def path_length(path, roads):
+    # compute the length of the given path along the roads
+    length = 0
+    for i in range(1, len(path)):
+        length += roads[(path[i-1], path[i])]
+    return length
+
+
+def shortest_path(num_nodes, roads, loop=False):
+    # given the weighted graph structure from roads, brute force all possible
+    # path permutations and find the shortest possible
+    min_length = 1 << 64  # just something bigger than an actual path
+    min_path = None
+    node_ls = list(range(1, num_nodes))
+    for sigma in permutations(node_ls):
+        path = [0] + list(sigma)
+        if loop:
+            path += [0]
+        length = path_length(path, roads)
+
+        if length < min_length:
+            min_length = length
+            min_path = path
+    return min_length, min_path
+
+
+def main_a(num_nodes, roads):
+    print(shortest_path(num_nodes, roads, loop=False))
+
+
+def main_b(num_nodes, roads):
+    print(shortest_path(num_nodes, roads, loop=True))
+
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    nodes, maze = parse(data)
+    roads = shortest_roads(nodes, maze)
+
+    main_a(len(nodes), roads)
+    main_b(len(nodes), roads)
--- 2016/24/test_2016_24.py
+++ 2016/24/test_2016_24.py
@@ -5,7 +5,30 @@ import puzzle_2016_24 as puz
 
 
 class Test_2016_24(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            data = f.readlines()
+
+        self.nodes, self.maze = puz.parse(data)
+        self.roads = puz.shortest_roads(self.nodes, self.maze)
+
+
+    def test_shortest_roads(self):
+        self.assertEqual(self.roads[(0, 1)], 2)
+        self.assertEqual(self.roads[(0, 2)], 8)
+        self.assertEqual(self.roads[(0, 3)], 10)
+        self.assertEqual(self.roads[(0, 4)], 2)
+        self.assertEqual(self.roads[(1, 2)], 6)
+        self.assertEqual(self.roads[(1, 3)], 8)
+        self.assertEqual(self.roads[(1, 4)], 4)
+        self.assertEqual(self.roads[(2, 3)], 2)
+        self.assertEqual(self.roads[(2, 4)], 10)
+        self.assertEqual(self.roads[(3, 4)], 8)
+
+
+    def test_shortest_path(self):
+        self.assertEqual(puz.shortest_path(len(self.nodes), self.roads)[0], 14)
+        print(puz.shortest_path(len(self.nodes), self.roads))
 
 
 if __name__ == "__main__":
