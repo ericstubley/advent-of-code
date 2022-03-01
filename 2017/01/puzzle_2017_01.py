6df9c8b9d075778ef745c4f46dc051fb4aab99dd
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-01 08:48:07 -0500

    Day 01 solution

--- 2017/01/puzzle_2017_01.py
+++ 2017/01/puzzle_2017_01.py
@@ -1,13 +1,32 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def next_match_sum(digits):
+    s = digits[0] if digits[0] == digits[-1] else 0
+    for i in range(0, len(digits) - 1):
+        if digits[i] == digits[i+1]:
+            s += digits[i]
+    return s
 
 
-def main_b():
-    pass
+def half_match_sum(digits):
+    # rather than summing over the whole list, twice the sum of the first half
+    assert len(digits) % 2 == 0
+    half = len(digits) // 2
+    return sum(2*digits[i] for i in range(0, half) if digits[i] == digits[i+half] )
+
+
+def main_a(digits):
+    print(next_match_sum(digits))
+
+
+def main_b(digits):
+    print(half_match_sum(digits))
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.read()
+
+    digits = list(map(int, data.rstrip('\n')))
+    main_a(digits)
+    main_b(digits)
