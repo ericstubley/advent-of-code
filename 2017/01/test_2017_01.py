0377c50b8788528bc7557f77b782a99f426acec5
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-01 08:48:07 -0500

    Day 01 solution

--- 2017/01/test_2017_01.py
+++ 2017/01/test_2017_01.py
@@ -5,7 +5,18 @@ import puzzle_2017_01 as puz
 
 
 class Test_2017_01(unittest.TestCase):
-    pass
+    def test_next_match_sum(self):
+        self.assertEqual(puz.next_match_sum([1, 1, 2, 2]), 3)
+        self.assertEqual(puz.next_match_sum([1, 1, 1, 1]), 4)
+        self.assertEqual(puz.next_match_sum([1, 2, 3, 4]), 0)
+        self.assertEqual(puz.next_match_sum([9, 1, 2, 1, 2, 1, 9]), 9)
+
+    def test_half_match_sum(self):
+        self.assertEqual(puz.half_match_sum([1, 2, 1, 2]), 6)
+        self.assertEqual(puz.half_match_sum([1, 2, 2, 1]), 0)
+        self.assertEqual(puz.half_match_sum([1, 2, 3, 4, 2, 5]), 4)
+        self.assertEqual(puz.half_match_sum([1, 2, 3, 1, 2, 3]), 12)
+        self.assertEqual(puz.half_match_sum([1, 2, 1, 3, 1, 4, 1, 5]), 4)
 
 
 if __name__ == "__main__":
