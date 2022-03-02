b33e7cc1448effecac749e37ee361d7c6bb6cc51
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-02 08:57:56 -0500

    Day 02 solution

--- 2017/02/puzzle_2017_02.py
+++ 2017/02/puzzle_2017_02.py
@@ -1,13 +1,45 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from itertools import combinations
 
 
-def main_b():
-    pass
+def parse(data):
+    ret = []
+    for line in data:
+        row = list(map(int, line.split()))
+        ret.append(row)
+    return ret
+
+
+def checksum(rows):
+    return sum(max(r) - min(r) for r in rows)
+
+
+def quotient_checksum(rows):
+    return sum(extract_quotient(r) for r in rows)
+
+    
+def extract_quotient(row):
+    for a, b in combinations(row, 2):
+        q1, r1 = divmod(b, a)
+        if r1 == 0:
+            return q1
+        q2, r2 = divmod(a, b)
+        if r2 == 0:
+            return q2
+
+
+def main_a(rows):
+    print(checksum(rows))
+
+
+def main_b(rows):
+    print(quotient_checksum(rows))
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    rows = parse(data)
+    main_a(rows)
+    main_b(rows)
--- 2017/02/test_2017_02.py
+++ 2017/02/test_2017_02.py
@@ -5,7 +5,19 @@ import puzzle_2017_02 as puz
 
 
 class Test_2017_02(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input_diff.txt") as f:
+            data = f.readlines()
+        self.rows_diff = puz.parse(data)
+        with open("test_input_quot.txt") as f:
+            data = f.readlines()
+        self.rows_quot = puz.parse(data)
+
+    def test_checksum(self):
+        self.assertEqual(puz.checksum(self.rows_diff), 18)
+
+    def test_quotient_checksum(self):
+        self.assertEqual(puz.quotient_checksum(self.rows_quot), 9)
 
 
 if __name__ == "__main__":
