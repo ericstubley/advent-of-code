9d4bb70862e1ebcc646eb80cfb2faf53de1b640c
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-03 10:09:28 -0500

    Day 03 solution

--- 2017/03/test_2017_03.py
+++ 2017/03/test_2017_03.py
@@ -5,7 +5,29 @@ import puzzle_2017_03 as puz
 
 
 class Test_2017_03(unittest.TestCase):
-    pass
+    def test_spiral(self):
+        for i in range(1, 26):
+            print(puz.ulam_coordinates(i))
+
+
+    def test_ulam_coordinates(self):
+        self.assertEqual(puz.ulam_coordinates(1), (0, 0))
+        self.assertEqual(puz.ulam_coordinates(12), (2, 1))
+        self.assertEqual(puz.ulam_coordinates(23), (0, -2))
+        self.assertEqual(puz.ulam_coordinates(1024), (-15, 16))
+
+
+    def test_manhattan_distance(self):
+        self.assertEqual(puz.ulam_in_ny(1), 0)
+        self.assertEqual(puz.ulam_in_ny(12), 3)
+        self.assertEqual(puz.ulam_in_ny(23), 2)
+        self.assertEqual(puz.ulam_in_ny(1024), 31)
+
+
+    def test_ulam_lies(self):
+        self.assertEqual(puz.ulam_lies(12), 23)
+        self.assertEqual(puz.ulam_lies(747), 747)
+        self.assertEqual(puz.ulam_lies(748), 806)
 
 
 if __name__ == "__main__":
