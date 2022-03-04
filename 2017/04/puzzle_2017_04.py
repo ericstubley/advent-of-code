252ce5979d94beceae4319a08fdc7dca5b6bbd6b
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-04 08:46:41 -0500

    Day 04 solution

--- 2017/04/puzzle_2017_04.py
+++ 2017/04/puzzle_2017_04.py
@@ -1,13 +1,47 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def is_valid_passphrase(p):
+    ret, words = True, set()
+    for word in p:
+        if word in words:
+            ret = False
+            break
+        else:
+            words.add(word)
+    return ret
 
 
-def main_b():
-    pass
+def word_to_letter_count(word):
+    counts = [0]*26
+    for c in word:
+        counts[c_to_index(c)] += 1
+    return tuple(counts)
+
+
+def c_to_index(c):
+    return ord(c) - ord('a')
+
+
+def main_a(passphrases):
+    count = 0
+    for p in passphrases:
+        if is_valid_passphrase(p):
+            count += 1
+    print(count)
+
+
+def main_b(passphrases):
+    count = 0
+    for p in passphrases:
+        ap = map(word_to_letter_count, p)
+        if is_valid_passphrase(ap):
+            count += 1
+    print(count)
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        data = f.readlines()
+    passphrases = [line.split() for line in data]
+    main_a(passphrases)
+    main_b(passphrases)
