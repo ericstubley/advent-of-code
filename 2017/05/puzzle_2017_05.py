23b9bd088c97f89dcf80970e28fd551232de8966
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-05 07:48:38 -0500

    Day 05 solution

--- 2017/05/puzzle_2017_05.py
+++ 2017/05/puzzle_2017_05.py
@@ -1,13 +1,29 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def count_jumps(rows, strange=False):
+    i, count = 0, 0
+    while 0 <= i and i < len(rows):
+        new_i = i + rows[i]
+        if strange and rows[i] >= 3:
+            rows[i] -= 1
+        else:
+            rows[i] += 1
+        count += 1
+        i = new_i
+    return count
 
 
-def main_b():
-    pass
+def main_a(rows):
+    print(count_jumps(rows))
+
+
+def main_b(rows):
+    print(count_jumps(rows, strange=True))
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    with open("input.txt") as f:
+        rows = [int(line.rstrip()) for line in f.readlines()]
+
+    main_a([__ for __ in rows])
+    main_b([__ for __ in rows])
--- 2017/05/test_2017_05.py
+++ 2017/05/test_2017_05.py
@@ -5,8 +5,17 @@ import puzzle_2017_05 as puz
 
 
 class Test_2017_05(unittest.TestCase):
-    pass
+    def setUp(self):
+        with open("test_input.txt") as f:
+            self.rows = [int(line.rstrip()) for line in f.readlines()]
+        self.rows_strange = [__ for __ in self.rows]
 
+    def test_count_jumps(self):
+        self.assertEqual(puz.count_jumps(self.rows), 5)
+        self.assertListEqual(self.rows, [2, 5, 0, 1, -2])
+
+        self.assertEqual(puz.count_jumps(self.rows_strange, strange=True), 10)
+        self.assertListEqual(self.rows_strange, [2, 3, 2, 3, -1])
 
 if __name__ == "__main__":
     unittest.main()
