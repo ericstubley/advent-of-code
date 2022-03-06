999f19155dc472d4d60b4f355dee25f74ec5c734
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-06 08:41:10 -0500

    Day 06 solution

--- 2017/06/puzzle_2017_06.py
+++ 2017/06/puzzle_2017_06.py
@@ -1,13 +1,40 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+def redistribute_blocks(banks):
+    to_distribute = max(banks)
+    idx = banks.index(to_distribute)
+    banks[idx] = 0
+    idx = (idx + 1) % len(banks)
+
+    while to_distribute > 0:
+        banks[idx] += 1
+        to_distribute -= 1
+        idx += 1
+        if not idx < len(banks):
+            idx = idx % len(banks)
+    return banks
 
 
-def main_b():
-    pass
+def time_to_repeated_state(banks):
+    observed_states = dict()
+    steps = 0
+    while tuple(banks) not in observed_states:
+        observed_states[tuple(banks)] = steps
+        banks = redistribute_blocks(banks)
+        steps += 1
+    return steps, observed_states[tuple(banks)]
+
+
+def main_a(banks):
+    print(time_to_repeated_state(banks))
+
+
+def main_b(banks):
+    total, first_seen = time_to_repeated_state(banks)
+    print(total - first_seen)
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    banks = list(map(int, "0 5 10 0 11 14 13 4 11 8 8 7 1 4 12 11".split()))
+    main_a([__ for __ in banks])
+    main_b([__ for __ in banks])
