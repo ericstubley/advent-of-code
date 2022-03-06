5eb2cba21bdb559ae418440a3b27e1c4a1439707
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-06 08:41:10 -0500

    Day 06 solution

--- 2017/06/test_2017_06.py
+++ 2017/06/test_2017_06.py
@@ -5,7 +5,16 @@ import puzzle_2017_06 as puz
 
 
 class Test_2017_06(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.banks = list(map(int, "0 2 7 0".split()))
+
+
+    def test_redistribute_blocks(self):
+        self.assertEqual(puz.redistribute_blocks([__ for __ in self.banks]), [2, 4, 1, 2])
+
+
+    def test_time_to_repeated_state(self):
+        self.assertEqual(puz.time_to_repeated_state(self.banks), (5, 1))
 
 
 if __name__ == "__main__":
