0cfa9920e6ab7060afdcfe35b4a1ddbf7cd2391b
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-08 10:05:48 -0500

    Day 08 solution

--- 2017/08/puzzle_2017_08.py
+++ 2017/08/puzzle_2017_08.py
@@ -1,13 +1,75 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from collections import defaultdict, namedtuple
 
 
-def main_b():
-    pass
+Instruction = namedtuple(typename="Instruction", 
+        field_names=["register", "value", "comp_register", "comp_function"])
+
+
+def parse_instructions(filename):
+    instructions = []
+    with open(filename) as f:
+        for line in f:
+            split = line.split()
+            register = split[0]
+            value = int(split[2]) if split[1] == "inc" else -1 * int(split[2])
+            comp_register = split[4]
+            comp_value = int(split[6]) 
+            comp_function = make_comparison_function(split[5], comp_value)
+            instructions.append(Instruction(register, value, comp_register, comp_function))
+    return instructions
+
+
+def make_comparison_function(operator, value):
+    if operator == '<':
+        return lambda x: x < value
+    elif operator == '>':
+        return lambda x: x > value
+    elif operator == '<=':
+        return lambda x: x <= value
+    elif operator == '>=':
+        return lambda x: x >= value
+    elif operator == '==':
+        return lambda x: x == value
+    elif operator == '!=':
+        return lambda x: x != value
+    else:
+        assert False
+
+
+def run_instructions(instructions, report=False):
+    registers = defaultdict(int)
+    highest = 0
+    for i in instructions:
+        if i.comp_function(registers[i.comp_register]):
+            registers[i.register] += i.value
+            if report:
+                highest = max(highest, registers[i.register])
+    if report:
+        return registers, highest
+    else:
+        return registers
+
+
+def max_register(instructions):
+    # be careful if the returned value is negative!
+    # there may or may not be registers which didn't end up in the dict but
+    # still have a value of 0
+    registers = run_instructions(instructions)
+    return max(registers.values())
+
+
+def main_a(instructions):
+    print(max_register(instructions))
+
+
+def main_b(instructions):
+    __, highest = run_instructions(instructions, report=True)
+    print(highest)
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    instructions = parse_instructions("input.txt")
+    main_a(instructions)
+    main_b(instructions)
--- 2017/08/test_2017_08.py
+++ 2017/08/test_2017_08.py
@@ -5,7 +5,21 @@ import puzzle_2017_08 as puz
 
 
 class Test_2017_08(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.instructions = puz.parse_instructions("test_input.txt")
+
+
+    def test_max_register(self):
+        self.assertEqual(puz.max_register(self.instructions), 1)
+
+
+    def test_run_instructions(self):
+        self.assertEqual(puz.run_instructions(self.instructions), {'a': 1, 'b': 0, 'c': -10})
+
+
+    def test_max_ever(self):
+        registers, highest = puz.run_instructions(self.instructions, report=True)
+        self.assertEqual(highest, 10)
 
 
 if __name__ == "__main__":
