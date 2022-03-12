2d101b3c8f6b8526063d64cf32dc276e4d801113
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-12 10:39:14 -0500

    Day 12 solution

--- 2017/12/test_2017_12.py
+++ 2017/12/test_2017_12.py
@@ -5,7 +5,14 @@ import puzzle_2017_12 as puz
 
 
 class Test_2017_12(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.graph = puz.parse_pipe_graph("test_input.txt")
+
+    def test_component_size(self):
+        self.assertEqual(puz.component_size(self.graph, 0), 6)
+
+    def test_count_components(self):
+        self.assertEqual(puz.count_components(self.graph), 2)
 
 
 if __name__ == "__main__":
