c583752e6bb67d44f18dbc92e3daee416c1b666d
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-13 09:45:40 -0400

    Day 13 solution

--- 2017/13/puzzle_2017_13.py
+++ 2017/13/puzzle_2017_13.py
@@ -1,13 +1,74 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from math import lcm
+from automation.automation import submit_answer
 
 
-def main_b():
-    pass
+def parse_input_to_ranges(filename):
+    with open(filename) as f:
+        data = f.readlines()
+
+    ranges = []
+    for line in data:
+        split = line.split()
+        level = int(split[0].rstrip(':'))
+        scan_range = int(split[1])
+        assert len(ranges) <= level
+        while len(ranges) < level:
+            ranges.append(0)
+        ranges.append(scan_range)
+    return ranges
+
+
+def caught(ranges, time, delay=0):
+    if time+delay == 0:
+        return True
+    elif ranges[time] != 0:
+        return ((time + delay) % (2*ranges[time] - 2) == 0)
+    else:
+        return False
+
+
+def trip_severity(ranges):
+    severity = 0
+    for t in range(len(ranges)):
+        if caught(ranges, t):
+            print(f"Caught at {t}")
+            severity += t * ranges[t]
+    return severity
+
+
+def optimal_delay(ranges):
+    delay = 0
+    while True:
+        caught_flag = False
+        for t in range(len(ranges)):
+            if caught(ranges, t, delay=delay):
+                caught_flag = True
+                break
+        if not caught_flag:
+            break
+        delay += 1
+    return delay
+
+
+def main_a(ranges):
+    answer = trip_severity(ranges)
+    print(answer)
+    # result = submit_answer(2017, 13, 1, answer)
+    # print(result)
+
+
+def main_b(ranges):
+    answer = optimal_delay(ranges)
+    print(answer)
+    # result = submit_answer(2017, 13, 2, answer)
+    # print(result)
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    ranges = parse_input_to_ranges("input.txt")
+    upper_bound = lcm(*(2*x-2 for x in ranges if x != 0))
+    main_a(ranges)
+    print(f"Upper bound on possible delay is {upper_bound}")
+    main_b(ranges)
--- 2017/13/test_2017_13.py
+++ 2017/13/test_2017_13.py
@@ -5,7 +5,23 @@ import puzzle_2017_13 as puz
 
 
 class Test_2017_13(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.ranges = puz.parse_input_to_ranges("test_input.txt")
+
+    def test_trip_severity(self):
+        self.assertEqual(puz.trip_severity(self.ranges), 24)
+
+    def test_caught(self):
+        self.assertEqual(puz.caught(self.ranges, time=0), True)
+        self.assertEqual(puz.caught(self.ranges, time=1), False)
+        self.assertEqual(puz.caught(self.ranges, time=2), False)
+        self.assertEqual(puz.caught(self.ranges, time=3), False)
+        self.assertEqual(puz.caught(self.ranges, time=4), False)
+        self.assertEqual(puz.caught(self.ranges, time=5), False)
+        self.assertEqual(puz.caught(self.ranges, time=6), True)
+
+    def test_optimal_delay(self):
+        self.assertEqual(puz.optimal_delay(self.ranges), 10)
 
 
 if __name__ == "__main__":
