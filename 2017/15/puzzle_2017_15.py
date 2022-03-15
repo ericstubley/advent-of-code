455eedc09821c7b47f6cf0465350efeb9faba906
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-15 10:13:39 -0400

    Day 15 solution

--- 2017/15/puzzle_2017_15.py
+++ 2017/15/puzzle_2017_15.py
@@ -1,13 +1,73 @@
 #! /usr/bin/env python3
 
+# from automation.automation import submit_answer
+import time
+
+FULL_16 = (1<<16) - 1
+FULL_31 = (1<<31) - 1
+SEED_A = 16807
+SEED_B = 48271
+START_A = 783
+START_B = 325
+
+
+class Generator:
+    def __init__(self, seed, start, multiple=1):
+        self.seed = seed
+        self.start = start
+        self.value = start
+        self.multiple = multiple
+
+    def __iter__(self):
+        self.value = self.start
+        return self
+
+    def __next__(self):
+        self.value = (self.value * self.seed) % FULL_31
+        while (self.value % self.multiple) != 0:
+            self.value = (self.value * self.seed) % FULL_31
+        return self.value
+
+
+def match_16_bits(a, b):
+    return (a & FULL_16) == (b & FULL_16)
+
+
+def judge(limit, gen_a, gen_b):
+    count = 0
+    for t in range(limit):
+        if t % 100000 == 0:
+            print(f"\r{t//100000} of {limit//100000}", end="")
+        a, b = next(gen_a), next(gen_b)
+        if match_16_bits(a, b):
+            count += 1
+    print("\r")
+    return count
+
+
 def main_a():
-    pass
+    gen_a = Generator(SEED_A, START_A)
+    gen_b = Generator(SEED_B, START_B)
+    time_1 = time.time()
+    answer = judge(40000000, gen_a, gen_b)
+    time_2 = time.time()
+    print(answer, time_2 - time_1)
+    # result = submit_answer(2017, 15, 1, answer)
+    # print(result)
 
 
 def main_b():
-    pass
+    gen_a = Generator(SEED_A, START_A, multiple=4)
+    gen_b = Generator(SEED_B, START_B, multiple=8)
+    time_1 = time.time()
+    answer = judge(5000000, gen_a, gen_b)
+    time_2 = time.time()
+    print(answer, time_2 - time_1)
+    # result = submit_answer(2017, 15, 2, answer)
+    # print(result)
 
 
 if __name__ == "__main__":
+
     main_a()
     main_b()
--- 2017/15/test_2017_15.py
+++ 2017/15/test_2017_15.py
@@ -5,7 +5,41 @@ import puzzle_2017_15 as puz
 
 
 class Test_2017_15(unittest.TestCase):
-    pass
+    def test_generator(self):
+        gen_a = puz.Generator(seed=16807, start=65)
+        self.assertEqual(next(gen_a), 1092455)
+        self.assertEqual(next(gen_a), 1181022009)
+        self.assertEqual(next(gen_a), 245556042)
+        self.assertEqual(next(gen_a), 1744312007)
+        self.assertEqual(next(gen_a), 1352636452)
+
+    def test_picky_generator(self):
+        gen_a = puz.Generator(seed=16807, start=65, multiple=4)
+        self.assertEqual(next(gen_a), 1352636452)
+        self.assertEqual(next(gen_a), 1992081072)
+        self.assertEqual(next(gen_a), 530830436)
+        self.assertEqual(next(gen_a), 1980017072)
+        self.assertEqual(next(gen_a), 740335192)
+
+
+    def test_match_16_bits(self):
+        self.assertEqual(puz.match_16_bits(1092455, 430625591), False)
+        self.assertEqual(puz.match_16_bits(1181022009, 1233683848), False)
+        self.assertEqual(puz.match_16_bits(245556042, 1431495498), True)
+        self.assertEqual(puz.match_16_bits(1744312007, 137874439), False)
+        self.assertEqual(puz.match_16_bits(1352636452, 285222916), False)
+
+
+    def test_judge(self):
+        gen_a = puz.Generator(seed=16807, start=65)
+        gen_b = puz.Generator(seed=48271, start=8921)
+        self.assertEqual(puz.judge(40000000, gen_a, gen_b), 588)
+
+
+    def test_picky_judge(self):
+        gen_a = puz.Generator(seed=16807, start=65, multiple=4)
+        gen_b = puz.Generator(seed=48271, start=8921, multiple=8)
+        self.assertEqual(puz.judge(5000000, gen_a, gen_b), 309)
 
 
 if __name__ == "__main__":
