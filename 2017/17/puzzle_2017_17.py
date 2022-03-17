b02297912f3cad354903ff638091f19dd307949d
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-17 10:01:25 -0400

    Day 17 solution

--- 2017/17/puzzle_2017_17.py
+++ 2017/17/puzzle_2017_17.py
@@ -1,11 +1,51 @@
 #! /usr/bin/env python3
 
+from collections import deque
+# from automation.automation import submit_answer
+
+
+def virtual_hurricane(time_limit, step):
+    after_zero, curr = 0, 0
+    for t in range(1, time_limit+1):
+        if curr + step < t:
+            curr = curr + step + 1
+        else:
+            curr = ((curr + step) % t) + 1
+
+        if curr == 1:
+            after_zero = t
+    return after_zero
+
+
+def hurricane(time_limit, step):
+    ram = deque([0])
+
+    curr = 0
+    for t in range(1, time_limit+1):
+        curr = ((curr + step) % len(ram)) + 1
+        ram.insert(curr, t) 
+    return list(ram)
+
+
+def element_after(ls, element):
+    i_element = ls.index(element)
+    i_after = (i_element + 1) % len(ls)
+    return ls[i_after]
+
+
+
 def main_a():
-    pass
+    answer = element_after(hurricane(2017, 329), 2017)
+    print(answer)
+    # result = submit_answer(2017, 17, 1, answer)
+    # print(result)
 
 
 def main_b():
-    pass
+    answer = virtual_hurricane(50000000, 329)
+    print(answer)
+    # result = submit_answer(2017, 17, 2, answer)
+    # print(result)
 
 
 if __name__ == "__main__":
--- 2017/17/test_2017_17.py
+++ 2017/17/test_2017_17.py
@@ -5,7 +5,27 @@ import puzzle_2017_17 as puz
 
 
 class Test_2017_17(unittest.TestCase):
-    pass
+    def test_hurricane(self):
+        self.assertEqual(puz.hurricane(1, 3), [0, 1])
+        self.assertEqual(puz.hurricane(2, 3), [0, 2, 1])
+        self.assertEqual(puz.hurricane(3, 3), [0, 2, 3, 1])
+        self.assertEqual(puz.hurricane(4, 3), [0, 2, 4, 3, 1])
+        self.assertEqual(puz.hurricane(5, 3), [0, 5, 2, 4, 3, 1])
+        self.assertEqual(puz.hurricane(6, 3), [0, 5, 2, 4, 3, 6, 1])
+
+
+    def test_after(self):
+        final_list = puz.hurricane(time_limit=2017, step=3)
+        self.assertEqual(puz.element_after(final_list, 2017), 638)
+
+
+    def test_virtual_hurricane(self):
+        self.assertEqual(puz.virtual_hurricane(1, 3), 1)
+        self.assertEqual(puz.virtual_hurricane(2, 3), 2)
+        self.assertEqual(puz.virtual_hurricane(3, 3), 2)
+        self.assertEqual(puz.virtual_hurricane(4, 3), 2)
+        self.assertEqual(puz.virtual_hurricane(5, 3), 5)
+        self.assertEqual(puz.virtual_hurricane(6, 3), 5)
 
 
 if __name__ == "__main__":
