14bb60eaf56ce6d1b50f7456eebfc80c685f50b9
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-19 09:13:16 -0400

    Day 19 solution

--- 2017/19/puzzle_2017_19.py
+++ 2017/19/puzzle_2017_19.py
@@ -1,13 +1,99 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from enum import Enum
+from automation.automation import submit_answer
 
 
-def main_b():
-    pass
+class Direction(Enum):
+    U = (-1, 0)
+    D = (1, 0)
+    L = (0, -1)
+    R = (0, 1)
+    C = (0, 0)
+    END = None
+
+SYMBOLS = {Direction.U: '|', 
+    Direction.D: '|',
+    Direction.L: '-',
+    Direction.R: '-',
+    Direction.C: '+',
+    Direction.END: ' '}
+
+REVERSE = {Direction.U: Direction.D,
+    Direction.D: Direction.U,
+    Direction.L: Direction.R,
+    Direction.R: Direction.L}
+
+
+def parse_path(filename):
+    with open(filename) as f:
+        path = []
+        for line in f:
+            path.append(line.rstrip('\n'))
+    return path
+
+
+def find_start(path):
+    return 0, path[0].index(SYMBOLS[Direction.D])
+
+
+def move(path, x, y, direction):
+    assert direction != Direction.C
+    assert direction != Direction.END
+
+    dx, dy = direction.value
+    nx, ny = x + dx, y + dy
+
+    if path[nx][ny] == "+":
+        # compute new direction
+        options = set([Direction.U, Direction.D, Direction.L, Direction.R])
+        options.remove(REVERSE[direction])
+
+        for d in options:
+            dx, dy = d.value
+            tx, ty = nx + dx, ny + dy
+            if path[tx][ty].isalpha() or path[tx][ty] == SYMBOLS[d]:
+                ndirection = d
+                break
+
+    elif path[nx][ny].isalpha() or path[nx][ny] == "-" or path[nx][ny] == "|":
+        ndirection = direction
+    else:
+        ndirection = Direction.END
+
+    return nx, ny, ndirection
+
+
+def walk_path(path):
+    x, y = find_start(path)
+    direction = Direction.D
+
+    letters = []
+    steps = 0
+    while direction != Direction.END:
+        x, y, direction = move(path, x, y, direction)
+        if path[x][y].isalpha():
+            letters.append(path[x][y])
+        steps += 1
+
+    return "".join(letters), steps
+
+
+def main_a(path):
+    answer, __ = walk_path(path)
+    print(answer)
+    # result = submit_answer(2017, 19, 1, answer)
+    # print(result)
+
+
+def main_b(path):
+    __, answer = walk_path(path)
+    print(answer)
+    # result = submit_answer(2017, 19, 2, answer)
+    # print(result)
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    path = parse_path("input.txt")
+    main_a(path)
+    main_b(path)
--- 2017/19/test_2017_19.py
+++ 2017/19/test_2017_19.py
@@ -5,7 +5,19 @@ import puzzle_2017_19 as puz
 
 
 class Test_2017_19(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.path = puz.parse_path("test_input.txt")
+
+    def test_walk_path(self):
+        self.assertEqual(puz.walk_path(self.path), ("ABCDEF", 38))
+
+    def test_find_start(self):
+        self.assertEqual(puz.find_start(self.path), (0, 5))
+
+    def test_move(self):
+        self.assertEqual(puz.move(self.path, 0, 5, puz.Direction.D), (1, 5, puz.Direction.D))
+        self.assertEqual(puz.move(self.path, 4, 5, puz.Direction.D), (5, 5, puz.Direction.R))
+        self.assertEqual(puz.move(self.path, 5, 5, puz.Direction.R), (5, 6, puz.Direction.R))
 
 
 if __name__ == "__main__":
