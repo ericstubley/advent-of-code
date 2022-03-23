b0cebc1f43c5a34e21a0477ca3917f5962ce5d9c
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-23 09:22:00 -0400

    Day 23 solution

--- 2017/23/test_2017_23.py
+++ 2017/23/test_2017_23.py
@@ -5,7 +5,18 @@ import puzzle_2017_23 as puz
 
 
 class Test_2017_23(unittest.TestCase):
-    pass
+    def test_is_prime(self):
+        self.assertEqual(puz.is_prime(2), True)
+        self.assertEqual(puz.is_prime(3), True)
+        self.assertEqual(puz.is_prime(5), True)
+        self.assertEqual(puz.is_prime(7), True)
+        self.assertEqual(puz.is_prime(101), True)
+        self.assertEqual(puz.is_prime(4), False)
+        self.assertEqual(puz.is_prime(57), False)
+
+    def test_count_composite(self):
+        self.assertEqual(puz.count_composite(3, 11, 2), 1)
+        self.assertEqual(puz.count_composite(3, 11, 1), 5)
 
 
 if __name__ == "__main__":
