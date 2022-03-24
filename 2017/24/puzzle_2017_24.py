43e78e5301e745da240482ad5a69df6a415e0d77
Eric Stubley <ericdavidstubley@gmail.com>
2022-03-24 09:50:33 -0400

    Day 24 solution

--- 2017/24/puzzle_2017_24.py
+++ 2017/24/puzzle_2017_24.py
@@ -1,13 +1,85 @@
 #! /usr/bin/env python3
 
-def main_a():
-    pass
+from collections import defaultdict
+from automation.automation import submit_answer
 
 
-def main_b():
-    pass
+def parse_graph(filename):
+    with open(filename) as f:
+        data = f.readlines()
+
+    vertices, edges = set(), defaultdict(list)
+    for line in data:
+        x, y = map(int, line.rstrip('\n').split('/'))
+        vertices.add(x)
+        vertices.add(y)
+        # check to make sure there's no double edges
+        assert y not in edges[x]
+        assert x not in edges[y]
+        edges[x].append(y)
+        edges[y].append(x)
+
+    return frozenset(vertices), edges
+
+
+def maximum_strength(vertices, edges):
+    memo_dict = dict()
+
+    def path_helper(v, used_edges):
+        if (v, used_edges) not in memo_dict:
+            value = 0
+
+            for w in edges[v]:
+                edge = (min(v, w), max(v, w))
+                if edge not in used_edges:
+                    walk = v + w + path_helper(w, used_edges.union({edge}))
+                    value = max(value, walk)
+
+            memo_dict[v, used_edges] = value
+
+        return memo_dict[v, used_edges]
+
+    return path_helper(0, frozenset())
+
+
+def maximum_length(vertices, edges):
+    memo_dict = dict()
+
+    def path_helper(v, used_edges):
+        if (v, used_edges) not in memo_dict:
+            value = (0, 0)
+
+            for w in edges[v]:
+                edge = (min(v, w), max(v, w))
+                if edge not in used_edges:
+                    length, strength = path_helper(w, used_edges.union({edge}))
+                    length += 1
+                    strength += v + w
+                    value = max(value, (length, strength))
+
+            memo_dict[v, used_edges] = value
+
+        return memo_dict[v, used_edges]
+
+    return path_helper(0, frozenset())
+
+
+
+def main_a(vertices, edges):
+    answer = maximum_strength(vertices, edges)
+    print(answer)
+    # result = submit_answer(2017, 24, 1, answer)
+    # print(result)
+
+
+def main_b(vertices, edges):
+    __, answer = maximum_length(vertices, edges)
+    print(answer)
+    # result = submit_answer(2017, 24, 2, answer)
+    # print(result)
 
 
 if __name__ == "__main__":
-    main_a()
-    main_b()
+    vertices, edges = parse_graph("input.txt")
+    main_a(vertices, edges)
+    main_b(vertices, edges)
--- 2017/24/test_2017_24.py
+++ 2017/24/test_2017_24.py
@@ -5,7 +5,15 @@ import puzzle_2017_24 as puz
 
 
 class Test_2017_24(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.vertices, self.edges = puz.parse_graph("test_input.txt")
+
+    def test_maximum_strength(self):
+        self.assertEqual(puz.maximum_strength(self.vertices, self.edges), 31)
+
+
+    def test_maximum_length(self):
+        self.assertEqual(puz.maximum_length(self.vertices, self.edges), (4, 19)) 
 
 
 if __name__ == "__main__":
