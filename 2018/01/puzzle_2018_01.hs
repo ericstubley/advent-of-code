d2adf3b206d91fa3c4720e94064b72cdc74e2b3a
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-01 11:15:17 -0400

    Day 01 solution

--- 2018/01/puzzle_2018_01.hs
+++ 2018/01/puzzle_2018_01.hs
@@ -1,11 +1,37 @@
 module Puzzle_2018_01 where
 
 import System.IO
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Automation (submitAnswer)
+
+
+parseInput :: String -> IO [Int]
+parseInput filename = do
+    raw <- readFile filename
+    let freqs = map toInt $ lines raw where
+        toInt :: String -> Int
+        toInt ('+':s) = toInt s
+        toInt ('-':s) = -1 * (toInt s)
+        toInt s = read s
+    return freqs
+
+
+freqSum :: [Int] -> Int
+freqSum = sum
+
+
+firstRepeat :: [Int] -> Int
+firstRepeat fs = helper 0 Set.empty (cycle fs) where
+    helper freq set (f:fs)
+        | Set.member freq set = freq
+        | otherwise           = helper (freq + f) (Set.insert freq set) fs
+
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    freqs <- parseInput "input.txt"
+    let answer = freqSum freqs
     print answer
     -- result <- submitAnswer 2018 01 1 answer
     -- print result
@@ -14,7 +40,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    freqs <- parseInput "input.txt"
+    let answer = firstRepeat freqs
     print answer
     -- result <- submitAnswer 2018 01 2 answer
     -- print result
--- 2018/01/test_2018_01.hs
+++ 2018/01/test_2018_01.hs
@@ -1,4 +1,4 @@
-import HUnit
+import Test.HUnit
 import System.IO
 import Puzzle_2018_01
 
@@ -7,11 +7,30 @@ import Puzzle_2018_01
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testFreqSum = TestCase (do
+    3 @=? freqSum [1, -2, 3, 1]
+    3 @=? freqSum [1, 1, 1]
+    0 @=? freqSum [1, 1, -2]
+    -6 @=? freqSum [-1, -2, -3])
+
+testParse = TestCase (do
+    freqs <- parseInput "test_input.txt"
+    [1, -2, 3, 1] @=? freqs)
+
+testFirstRepeat = TestCase (do
+    2  @=? firstRepeat [1, -2, 3, 1]
+    0  @=? firstRepeat [1, -1]
+    10 @=? firstRepeat [3, 3, 4, -2, -4]
+    5  @=? firstRepeat [-6, 3, 8, 5, -6]
+    14 @=? firstRepeat [7, 7, -2, -7, -4])
+
+
 testA = TestList
-    []
+    [ TestLabel "Freq Sum" testFreqSum
+    , TestLabel "Parsing" testParse]
 
 testB = TestList
-    []
+    [ TestLabel "FirstRepeat" testFirstRepeat]
 
 testList = TestList
     [ TestLabel "Part A" testA
