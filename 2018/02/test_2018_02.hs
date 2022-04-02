545e7e01cb17a2c02111ecb2c1dbe391093a080f
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-02 11:23:05 -0400

    Day 02 solution

--- 2018/02/test_2018_02.hs
+++ 2018/02/test_2018_02.hs
@@ -7,11 +7,34 @@ import Puzzle_2018_02
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+parsedInput = do
+    raw <- readFile "test_input.txt"
+    return $ lines raw
+
+parsedInputB = do
+    ids <- parseInput "test_input_2.txt"
+    return ids
+
+testTwo = TestCase (do
+    ids <- parsedInput
+    let two_ids = map hasDoubleLetter ids
+    [False, True, True, False, True, True, False] @=? two_ids)
+
+testThree = TestCase (do
+    ids <- parsedInput
+    let three_ids = map hasTripleLetter ids
+    [False, True, False, True, False, False, True] @=? three_ids)
+
+testFindCorrect = TestCase (do
+    ids <- parsedInputB
+    "fgij" @=? findCorrect ids)
+
 testA = TestList
-    []
+    [ TestLabel "Double Letters" testTwo
+    , TestLabel "Triple Letters" testThree]
 
 testB = TestList
-    []
+    [ TestLabel "OffByOne" testFindCorrect]
 
 testList = TestList
     [ TestLabel "Part A" testA
