773003e39a359d5d9a2bbc4b68bb5f44d0774233
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-02 11:55:22 -0400

    Fixed the output formatting for strings (hopefully)

--- automation/automation.hs
+++ automation/automation.hs
@@ -56,16 +56,28 @@ inputToFile filepath year day = do
 
 submitAnswer :: (Show a) => Int -> Int -> Int -> a -> IO String
 submitAnswer year day level answer = do
-    putStrLn ("Are you sure you want to submit " ++ show answer ++ " for " ++ 
+    let fAnswer = formattedAnswer answer
+    putStrLn ("Are you sure you want to submit " ++ fAnswer ++ " for " ++ 
             show year ++ " day " ++ show day ++ " part "  ++ show level ++ "?")
     confirm <- getLine
     if confirm == "y"
         then do
             let submitURL = submitURLFormatter year day
             let value = "Value" :: String
-            let postArgs = ["Name" := value, "level" := show level, "answer" := show answer]
+            let postArgs = ["Name" := value, "level" := show level, "answer" := fAnswer]
             response <- runPostRequest submitURL postArgs
             return $ show (response ^. responseBody)
         else do
             return "Not submitted"
+
+-- this is super janky and there's better Haskell-y typeclass ways to do it
+formattedAnswer :: (Show a) => a -> String
+formattedAnswer x = lStrip . rStrip . show $ x where
+    lStrip s
+        | head s == '"' = tail s
+        | otherwise     = s
+    rStrip s 
+        | last s == '"' = init s
+        | otherwise     = s
+
 
