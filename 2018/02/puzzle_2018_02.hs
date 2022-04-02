2b6967546f030fc0bc9a1f63ab11c528d76843d4
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-02 11:23:05 -0400

    Day 02 solution

--- 2018/02/puzzle_2018_02.hs
+++ 2018/02/puzzle_2018_02.hs
@@ -1,11 +1,59 @@
 module Puzzle_2018_02 where
 
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import System.IO
 import Automation (submitAnswer)
 
+parseInput :: String -> IO [String]
+parseInput filename = do
+    raw <- readFile filename
+    return $ lines raw
+
+
+checksum :: [String] -> Int
+checksum ids = doubles * triples where
+    doubles = length $ filter hasDoubleLetter ids
+    triples = length $ filter hasTripleLetter ids
+
+
+counts :: Ord a => [a] -> Map a Int
+counts = foldl (\acc x -> Map.insertWith (+) x 1 acc) Map.empty
+
+
+hasNRepeat :: Ord a => Int -> [a] -> Bool
+hasNRepeat n ids = Map.empty /= Map.filter (n==) (counts ids)
+
+
+hasDoubleLetter = hasNRepeat 2
+hasTripleLetter = hasNRepeat 3
+
+
+findCorrect :: Eq a => [[a]] -> [a]
+findCorrect (x:xs)
+    | length (matches x xs) > 0 = extractLetters x (head $ matches x xs)
+    | otherwise                 = findCorrect xs
+        where matches x xs = filter (offByOne x) xs
+
+
+offByOne :: Eq a => [a] -> [a] -> Bool
+offByOne [] [] = False
+offByOne (x:xs) (y:ys)
+    | x == y = offByOne xs ys
+    | x /= y = (xs == ys)
+
+
+extractLetters :: Eq a => [a] -> [a] -> [a]
+extractLetters xs ys = foldl extract [] (zip xs ys) where
+    extract acc (x, y)
+        | x == y    = acc ++ [x]
+        | otherwise = acc
+
+
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    ids <- parseInput "input.txt"
+    let answer = checksum ids
     print answer
     -- result <- submitAnswer 2018 02 1 answer
     -- print result
@@ -14,7 +62,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    ids <- parseInput "input.txt"
+    let answer = findCorrect ids
     print answer
     -- result <- submitAnswer 2018 02 2 answer
     -- print result
