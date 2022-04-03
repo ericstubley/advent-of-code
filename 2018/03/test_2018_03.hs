6c8ab5349fccb0536b0bd73b773ed9538fa88acf
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-03 12:37:28 -0400

    Day 03 solution

--- 2018/03/test_2018_03.hs
+++ 2018/03/test_2018_03.hs
@@ -7,11 +7,34 @@ import Puzzle_2018_03
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+parsedInput :: IO [(Int, Rectangle)]
+parsedInput = do
+    rects <- parseInput "test_input.txt"
+    return rects
+
+
+testTotalArea = TestCase (do
+    rects <- parsedInput
+    let justRects = map snd rects
+    let to_all = totalOverlap justRects
+    let to_init = totalOverlap $ init justRects
+    let to_tail = totalOverlap $ tail justRects
+    4 @=? to_all
+    4 @=? to_init
+    0 @=? to_tail)
+
+
+testExtractID = TestCase (do
+    rects <- parsedInput
+    let ident = extractID rects
+    3 @=? ident)
+
+
 testA = TestList
-    []
+    [TestLabel "Overlap" testTotalArea]
 
 testB = TestList
-    []
+    [TestLabel "extraction" testExtractID]
 
 testList = TestList
     [ TestLabel "Part A" testA
