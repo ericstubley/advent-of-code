0b1b5ba0c286a52f8625319b417e915f512546bf
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-04 17:12:43 -0400

    Day 03 solution

--- 2018/04/test_2018_04.hs
+++ 2018/04/test_2018_04.hs
@@ -7,11 +7,43 @@ import Puzzle_2018_04
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+parsedInput :: IO [WallEntry]
+parsedInput = do
+    ws <- parseInput "test_input.txt"
+    return ws
+
+
+testSleepiestGuard = TestCase (do
+    ws <- parsedInput
+    let sg = sleepiestGuard ws
+    Just 10 @=? sg)
+
+testSleepiestMinute = TestCase (do
+    ws <- parsedInput
+    let gs = filter (\x -> ident x == Just 10) ws
+    let (sm, _) = sleepiestMinute gs
+    24 @=? sm)
+
+testTimeAsleep = TestCase (do
+    ws <- parsedInput
+    let gs = filter (\x -> ident x == Just 10) ws
+    let ta = timeAsleep gs
+    50 @=? ta )
+
+testOverallMinute = TestCase (do
+    ws <- parsedInput
+    let (sg, sm) = overallSleepiestGuardMinutePair ws
+    99 @=? sg
+    45 @=? sm)
+
+
 testA = TestList
-    []
+    [ TestLabel "Sleepiest Guard" testSleepiestGuard
+    , TestLabel "Sleepiest Minute" testSleepiestMinute
+    , TestLabel "Time Asleep" testTimeAsleep]
 
 testB = TestList
-    []
+    [ TestLabel "Sleepiest Overall" testOverallMinute]
 
 testList = TestList
     [ TestLabel "Part A" testA
