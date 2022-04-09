9373d7edbdbc21f15fdd13176c588d2e88e330ca
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-09 13:58:55 -0400

    Day 09 solution

--- 2018/09/puzzle_2018_09.hs
+++ 2018/09/puzzle_2018_09.hs
@@ -1,11 +1,56 @@
 module Puzzle_2018_09 where
 
+import Data.CircularList (CList)
+import qualified Data.CircularList as CList
+import Data.Maybe
 import System.IO
 import Automation (submitAnswer)
 
+
+-- functions
+highScore :: Int -> Int -> Int
+highScore players final = maximum $ playGame players final
+
+playGame :: Int -> Int -> [Int]
+playGame players final = summedScores 1 where
+    summedScores :: Int -> [Int]
+    summedScores p
+        | p == players + 1  = []
+        | otherwise         = (sum $ map snd $ filter (\x -> fst x == p) ss) : summedScores (p+1)
+        where
+            ss = filter (\x -> snd x /= 0) $ take (final+1) $ scores players
+
+-- infinite list of who plays and what they score on each turn
+scores :: Int -> [(Int, Int)]
+scores players = zip (cycle [1..players]) boardScores
+
+-- infinite list of scores on each turn
+boardScores :: [Int]
+boardScores = (0 : map nextBoardScore boards)
+
+nextBoardScore :: CList Int -> Int
+nextBoardScore board
+    | mod f 23 == 22   = 1 + f + fromJust (CList.focus $ CList.rotN (-7) board)
+    | otherwise        = 0
+    where
+        f = fromJust $ CList.focus board
+
+
+-- infinite list of board states
+boards :: [CList Int]
+boards = scanl playMarble (CList.fromList [0]) [1..]
+
+
+playMarble :: CList Int -> Int -> CList Int
+playMarble board marble
+    | mod marble 23 == 0    = CList.removeR $ CList.rotN (-7) board 
+    | otherwise             = CList.insertL marble $ CList.rotR board
+
+
+-- mains
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    let answer = highScore 464 71730
     print answer
     -- result <- submitAnswer 2018 09 1 answer
     -- print result
@@ -14,7 +59,7 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    let answer = highScore 464 7173000
     print answer
     -- result <- submitAnswer 2018 09 2 answer
     -- print result
--- 2018/09/test_2018_09.hs
+++ 2018/09/test_2018_09.hs
@@ -1,14 +1,30 @@
 import Test.HUnit
 import System.IO
 import Puzzle_2018_09
+import qualified Data.CircularList as CList
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testHighScore = TestCase (do
+    let games = [(9, 25), (10, 1618), (13, 7999), (17, 1104), (21, 6111), (30, 5807)]
+    let scores = map (\x -> highScore (fst x) (snd x)) games
+    [32, 8317, 146373, 2764, 54718, 37305] @=? scores)
+
+
+testBoards = TestCase (do
+    let firstBoards = take 4 boards
+    let expected =  [ CList.fromList [0]
+                    , CList.fromList [1, 0]
+                    , CList.fromList [2, 1, 0]
+                    , CList.fromList [3, 0, 2, 1]]
+    expected @=? firstBoards)
+
 testA = TestList
-    []
+    [ TestLabel "High Scores" testHighScore
+    , TestLabel "First Boards" testBoards]
 
 testB = TestList
     []
