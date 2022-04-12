8fbc12c1d39df9735cba2940a3c91b2fb41cd91b
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-12 10:53:07 -0400

    Day 12 solution

--- 2018/12/test_2018_12.hs
+++ 2018/12/test_2018_12.hs
@@ -1,3 +1,6 @@
+import qualified Data.Map.Strict as M
+import qualified Data.IntMap.Strict as IM
+
 import Test.HUnit
 import System.IO
 import Puzzle_2018_12
@@ -7,8 +10,34 @@ import Puzzle_2018_12
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+parsedInput :: IO (Rules, Planters)
+parsedInput = do
+    (rules, pots) <- parseInput "test_input.txt"
+    return (rules, pots)
+
+testParse = TestCase (do
+    (rules, pots) <- parsedInput
+    IM.fromList (zip [0..] "#..#.#..##......###...###") @=? pots
+    32 @=? M.size rules
+    '.' @=? rules M.! "....."
+    '#' @=? rules M.! "...##")
+
+
+testEvolve = TestCase (do
+    (rules, pots) <- parsedInput
+    IM.fromList (zip [(-2)..] "..#...#....#.....#..#..#..#..") @=? evolve rules pots)
+
+
+testIndexSum = TestCase (do
+    (rules, pots) <- parsedInput
+    325 @=? (indexSum $ (iterate (evolve rules) pots) !! 20))
+
+
+
 testA = TestList
-    []
+    [ TestLabel "Parsing" testParse
+    , TestLabel "Evolution" testEvolve
+    , TestLabel "Index Sum" testIndexSum]
 
 testB = TestList
     []
