440b12dca6274e4496f3fdc7461182fc3a0213ed
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-14 10:00:35 -0400

    Day 14 solution

--- 2018/14/puzzle_2018_14.hs
+++ 2018/14/puzzle_2018_14.hs
@@ -1,11 +1,66 @@
 module Puzzle_2018_14 where
 
+import Data.Foldable (toList)
+import Data.Sequence (Seq, (|>))
+import qualified Data.Sequence as S
 import System.IO
 import Automation (submitAnswer)
 
+
+-- data types
+data Scoreboard = Scoreboard {elf1 :: Int, elf2 :: Int, board :: Seq Int} deriving (Eq, Show)
+
+
+-- functions
+
+startingBoard :: Scoreboard
+startingBoard = Scoreboard 0 1 (S.fromList [3, 7])
+
+
+tenAfter :: Int -> [Int]
+tenAfter n = helper n startingBoard where
+    helper :: Int -> Scoreboard -> [Int]
+    helper n sb
+        | S.length b < n + 10   = helper n (newRecipe sb)
+        | otherwise             = toList . S.take 10 . S.drop n $ b
+        where b = board sb
+
+
+newRecipe :: Scoreboard -> Scoreboard
+newRecipe (Scoreboard e1 e2 sb) = Scoreboard e1' e2' sb' where
+    s1 = S.index sb e1
+    s2 = S.index sb e2
+    sb' | s1 + s2 < 10  = sb |> (s1+s2)
+        | otherwise     = sb |> 1 |> mod (s1+s2) 10
+    len = S.length sb'
+    e1' = mod (e1 + s1 + 1) len
+    e2' = mod (e2 + s2 + 1) len
+
+
+recipesLeft :: [Int] -> Int
+recipesLeft scores = helper (S.fromList scores) startingBoard where
+    helper :: Seq Int -> Scoreboard -> Int
+    helper ts sb
+        | t1 == ts  = bl - tl
+        | t2 == ts  = bl - tl - 1
+        | otherwise = helper ts (newRecipe sb)
+        where 
+            b = board sb
+            tl = S.length ts
+            bl = S.length b
+            t1 = S.drop (bl - tl) b
+            t2 = S.take tl . S.drop (bl - tl - 1) $ b
+
+
+
+
+
+
+
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    let ls = tenAfter 260321 
+    let answer = concat $ map show ls 
     print answer
     -- result <- submitAnswer 2018 14 1 answer
     -- print result
@@ -14,8 +69,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    let answer = recipesLeft [2,6,0,3,2,1]
     print answer
-    -- result <- submitAnswer 2018 14 2 answer
-    -- print result
+    result <- submitAnswer 2018 14 2 answer
+    print result
     return ()
--- 2018/14/test_2018_14.hs
+++ 2018/14/test_2018_14.hs
@@ -1,3 +1,6 @@
+import Data.Sequence (Seq)
+import qualified Data.Sequence as S
+
 import Test.HUnit
 import System.IO
 import Puzzle_2018_14
@@ -7,11 +10,31 @@ import Puzzle_2018_14
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+
+testCook = TestCase (do
+    let expected = Scoreboard 0 1 (S.fromList [3, 7, 1, 0])
+    let cooked = newRecipe startingBoard
+    expected @=? cooked)
+
+testTenAfter = TestCase (do
+    [5,1,5,8,9,1,6,7,7,9] @=? tenAfter 9
+    [0,1,2,4,5,1,5,8,9,1] @=? tenAfter 5
+    [9,2,5,1,0,7,1,0,8,5] @=? tenAfter 18
+    [5,9,4,1,4,2,9,8,8,2] @=? tenAfter 2018)
+
+testRecipesLeft = TestCase (do
+    9    @=? recipesLeft [5,1,5,8,9,1,6,7,7,9]
+    5    @=? recipesLeft [0,1,2,4,5,1,5,8,9,1]
+    18   @=? recipesLeft [9,2,5,1,0,7,1,0,8,5]
+    2018 @=? recipesLeft [5,9,4,1,4,2,9,8,8,2])
+
+
 testA = TestList
-    []
+    [ TestLabel "New Recipe" testCook
+    , TestLabel "Ten After" testTenAfter]
 
 testB = TestList
-    []
+    [ TestLabel "Recipes Left" testRecipesLeft]
 
 testList = TestList
     [ TestLabel "Part A" testA
