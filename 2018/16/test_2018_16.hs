fca35a2ef3c61b08cf43721246e163be5de028b7
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-16 19:27:34 -0400

    Day 16 solution

--- 2018/16/test_2018_16.hs
+++ 2018/16/test_2018_16.hs
@@ -1,3 +1,5 @@
+
+import qualified Data.Vector.Unboxed as V
 import Test.HUnit
 import System.IO
 import Puzzle_2018_16
@@ -7,8 +9,22 @@ import Puzzle_2018_16
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testParse = TestCase (do
+    (ss, _) <- parseInput "test_input.txt"
+    let s = Sample (V.fromList [3, 2, 1, 1]) (V.fromList [3, 2, 2, 1]) (OP 9 (OPIO 2 1 2))
+    [s] @=? ss)
+
+
+
+testPossible = TestCase (do
+    let s = Sample (V.fromList [3, 2, 1, 1]) (V.fromList [3, 2, 2, 1]) (OP 9 (OPIO 2 1 2))
+    [ADDI, MULR, SETI] @=? possibleOPCodes s)
+
+
+
 testA = TestList
-    []
+    [ TestLabel "Parsing" testParse
+    , TestLabel "Possible OPCodes" testPossible]
 
 testB = TestList
     []
