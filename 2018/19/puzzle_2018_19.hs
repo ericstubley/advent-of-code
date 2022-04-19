1674292bd89bbfc999099ba63a0979b1e3665d6e
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-19 09:18:17 -0400

    Day 19 solution

--- 2018/19/puzzle_2018_19.hs
+++ 2018/19/puzzle_2018_19.hs
@@ -1,11 +1,153 @@
 module Puzzle_2018_19 where
 
+import Data.Bits ((.&.), (.|.))
+import Data.Sequence (Seq)
+import Data.Vector.Unboxed (Vector, (//), (!))
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Data.Vector.Unboxed as V
+import qualified Text.Megaparsec.Char.Lexer as L
+import qualified Data.Sequence as S
+
 import System.IO
 import Automation (submitAnswer)
 
+
+-- data types
+type Parser = Parsec Void String
+
+
+type Program = Seq OP
+data OP = OP {_code :: OPCode, _a :: Int, _b :: Int, _c :: Int} deriving (Eq, Show)
+type Register = Vector Int 
+data OPCode = ADDR | ADDI 
+            | MULR | MULI 
+            | BANR | BANI 
+            | BORR | BORI
+            | SETR | SETI
+            | GTIR | GTRI | GTRR
+            | EQIR | EQRI | EQRR deriving (Enum, Eq, Ord, Show)
+
+
+-- parsing
+parseInput :: String -> IO (Int, Program)
+parseInput filename = do
+    raw <- readFile filename
+    let (Right (ip, program)) = runParser inputP "" raw
+    return (ip, S.fromList program)
+
+
+inputP :: Parser (Int, [OP])
+inputP = do
+    ip <- instructionPointerP
+    char '\n'
+    -- count 2 (char '\n')
+    program <- programP
+    return (ip, program)
+
+
+instructionPointerP :: Parser Int
+instructionPointerP = do
+    string "#ip "
+    ip <- L.decimal
+    return ip
+
+
+programP :: Parser [OP]
+programP = opP `sepBy` char '\n'
+
+
+opcodeP :: Parser OPCode
+opcodeP = do
+    codeString <- choice $ (map (\s -> string s)) codeStrings 
+    return $ strToCode codeString
+
+
+opP :: Parser OP
+opP = do
+    code <- opcodeP
+    char ' '
+    a <- L.decimal
+    char ' '
+    b <- L.decimal
+    char ' '
+    c <- L.decimal
+    return $ OP code a b c
+
+
+
+-- functions
+runOP :: OP -> Register -> Register
+runOP (OP code a b c) reg = case code of
+    ADDR -> reg // [(c, (reg!a) + (reg!b))]
+    ADDI -> reg // [(c, (reg!a) + b)]
+    MULR -> reg // [(c, (reg!a) * (reg!b))]
+    MULI -> reg // [(c, (reg!a) * b)]
+    BANR -> reg // [(c, (reg!a) .&. (reg!b))]
+    BANI -> reg // [(c, (reg!a) .&. b)]
+    BORR -> reg // [(c, (reg!a) .|. (reg!b))]
+    BORI -> reg // [(c, (reg!a) .|. b)]
+    SETR -> reg // [(c, reg!a)]
+    SETI -> reg // [(c, a)]
+    GTIR -> reg // [(c, if a        > (reg!b) then 1 else 0)]
+    GTRI -> reg // [(c, if (reg!a)  > b       then 1 else 0)]
+    GTRR -> reg // [(c, if (reg!a)  > (reg!b) then 1 else 0)]
+    EQIR -> reg // [(c, if a       == (reg!b) then 1 else 0)]
+    EQRI -> reg // [(c, if (reg!a) == b       then 1 else 0)]
+    EQRR -> reg // [(c, if (reg!a) == (reg!b) then 1 else 0)]
+
+
+codes :: [OPCode]
+codes = [ADDR .. EQRR]
+
+
+codeStrings :: [String]
+codeStrings =   [ "addr", "addi", "mulr", "muli"
+                , "banr", "bani", "borr", "bori"
+                , "setr", "seti", "gtir", "gtri", "gtrr"
+                , "eqir", "eqri", "eqrr"]
+
+
+strToCode :: String -> OPCode
+strToCode s = case s of
+    "addr" -> ADDR
+    "addi" -> ADDI
+    "mulr" -> MULR
+    "muli" -> MULI
+    "banr" -> BANR
+    "bani" -> BANI
+    "borr" -> BORR
+    "bori" -> BORI
+    "setr" -> SETR
+    "seti" -> SETI
+    "gtir" -> GTIR
+    "gtri" -> GTRI
+    "gtrr" -> GTRR
+    "eqir" -> EQIR
+    "eqri" -> EQRI
+    "eqrr" -> EQRR
+
+
+advanceIP :: Int -> Register -> Register
+advanceIP ip reg = reg // [(ip, (reg!ip) + 1)]
+
+
+runProgram :: Program -> Int -> Register -> Register
+runProgram program ip reg
+    | pointer >= S.length program || pointer < 0  = reg
+    | otherwise                         = runProgram program ip reg' where
+        pointer = reg!ip
+        reg' = advanceIP ip $ runOP (S.index program pointer) reg
+
+
+
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (ip, program) <- parseInput "input.txt"
+    let reg = V.replicate 6 0
+    let reg' = runProgram program ip reg
+    let answer = reg' ! 0
     print answer
     -- result <- submitAnswer 2018 19 1 answer
     -- print result
@@ -14,8 +156,11 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (ip, program) <- parseInput "input.txt"
+    let reg = V.fromList [1,0,0,0,0,0]
+    let reg' = runProgram program ip reg
+    let answer = reg' ! 0
     print answer
-    -- result <- submitAnswer 2018 19 2 answer
-    -- print result
+    result <- submitAnswer 2018 19 2 answer
+    print result
     return ()
--- 2018/19/test_2018_19.hs
+++ 2018/19/test_2018_19.hs
@@ -1,3 +1,5 @@
+import qualified Data.Sequence as S
+import qualified Data.Vector.Unboxed as V
 import Test.HUnit
 import System.IO
 import Puzzle_2018_19
@@ -7,8 +9,36 @@ import Puzzle_2018_19
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+parsedInput :: IO (Int, Program)
+parsedInput = parseInput "test_input.txt"
+
+
+testParse = TestCase (do
+    (ip, program) <- parsedInput
+    7 @=? S.length program)
+
+
+testProgram = TestCase (do
+    (ip, program) <- parsedInput
+    let p1 = S.take 1 program
+    let p2 = S.take 2 program
+    let p3 = S.take 3 program
+    let p4 = S.take 4 program
+    let p5 = S.take 5 program
+    let p6 = S.take 6 program
+    let p7 = S.take 7 program
+    V.fromList [1, 5, 0, 0, 0, 0] @=? runProgram p1 ip (V.replicate 6 0)
+    V.fromList [2, 5, 6, 0, 0, 0] @=? runProgram p2 ip (V.replicate 6 0)
+    V.fromList [4, 5, 6, 0, 0, 0] @=? runProgram p3 ip (V.replicate 6 0)
+    V.fromList [4, 5, 6, 0, 0, 0] @=? runProgram p4 ip (V.replicate 6 0)
+    V.fromList [6, 5, 6, 0, 0, 0] @=? runProgram p5 ip (V.replicate 6 0)
+    V.fromList [6, 5, 6, 0, 0, 0] @=? runProgram p6 ip (V.replicate 6 0)
+    V.fromList [7, 5, 6, 0, 0, 9] @=? runProgram p7 ip (V.replicate 6 0))
+
+
 testA = TestList
-    []
+    [ TestLabel "Parsing" testParse
+    , TestLabel "Program" testProgram]
 
 testB = TestList
     []
