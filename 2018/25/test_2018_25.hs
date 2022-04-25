9d203d5b5ef96d26f2b01a1ebaac61ab0ee89879
Eric Stubley <ericdavidstubley@gmail.com>
2022-04-24 22:45:36 -0400

    Day 25 solution

--- 2018/25/test_2018_25.hs
+++ 2018/25/test_2018_25.hs
@@ -1,14 +1,27 @@
 import Test.HUnit
 import System.IO
 import Puzzle_2018_25
+import qualified Data.Set as S
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testConstellationCount = TestCase (do
+    starsA <- parseInput "test_input_a.txt"
+    starsB <- parseInput "test_input_b.txt"
+    starsC <- parseInput "test_input_c.txt"
+    starsD <- parseInput "test_input_d.txt"
+    -- print $ constellations starsA
+    2 @=? (S.size $ constellations starsA)
+    4 @=? (S.size $ constellations starsB)
+    3 @=? (S.size $ constellations starsC)
+    8 @=? (S.size $ constellations starsD)
+    )
+
 testA = TestList
-    []
+    [ TestLabel "Constellation Counts" testConstellationCount]
 
 testB = TestList
     []
