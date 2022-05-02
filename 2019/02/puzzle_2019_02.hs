2c677afa61c482c111a4611c6e9ea82378b7c55d
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-02 10:07:28 -0400

    Day 02 solution

--- 2019/02/puzzle_2019_02.hs
+++ 2019/02/puzzle_2019_02.hs
@@ -1,10 +1,31 @@
 module Puzzle_2019_02 where
 
 import Automation (submitAnswer)
+import Intcode
+import Parsing (parseInput)
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+
+
+gravityAssistSearch :: Program -> Int -> (Int, Int)
+gravityAssistSearch prog goal = search [(x, y) | x <- [0..99], y <- [0..99]]
+  where
+    search [] = (-1, -1)
+    search ((n, v):ls)
+        | goal == (V.head . execute . (loadProgram (n, v)) $ prog) = (n, v)
+        | otherwise = search ls
+
+
+loadProgram :: (Int, Int) -> Program -> Program
+loadProgram (noun, verb) prog = prog V.// [(1, noun), (2, verb)]
+
+
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just prog) <- parseInput programP "input.txt"
+    let modifiedProg = loadProgram (12, 2) prog
+    let answer = (execute modifiedProg) V.! 0
     print answer
     -- result <- submitAnswer 2019 02 1 answer
     -- print result
@@ -13,7 +34,9 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just prog) <- parseInput programP "input.txt"
+    let (noun, verb) = gravityAssistSearch prog 19690720
+    let answer = 100*noun + verb
     print answer
     -- result <- submitAnswer 2019 02 2 answer
     -- print result
--- 2019/02/test_2019_02.hs
+++ 2019/02/test_2019_02.hs
@@ -1,13 +1,36 @@
 import Test.HUnit
 import Puzzle_2019_02
+import Intcode
+import Parsing
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+(Just prog1) = programFromString "1,9,10,3,2,3,11,0,99,30,40,50"
+(Just prog2) = programFromString "1,0,0,0,99"
+(Just prog3) = programFromString "2,3,0,3,99"
+(Just prog4) = programFromString "2,4,4,5,99,0"
+(Just prog5) = programFromString "1,1,1,4,99,5,6,0,99"
+(Just prog1After) = programFromString "3500,9,10,70,2,3,11,0,99,30,40,50"
+(Just prog2After) = programFromString "2,0,0,0,99"
+(Just prog3After) = programFromString "2,3,0,6,99"
+(Just prog4After) = programFromString "2,4,4,5,99,9801"
+(Just prog5After) = programFromString "30,1,1,4,2,5,6,0,99"
+
+
+testExecute :: Test
+testExecute = TestCase (do
+    prog1After @=? execute prog1
+    prog2After @=? execute prog2
+    prog3After @=? execute prog3
+    prog4After @=? execute prog4
+    prog5After @=? execute prog5)
+
+
 testA = TestList
-    []
+    [TestLabel "Program Execution" testExecute]
 
 testB = TestList
     []
