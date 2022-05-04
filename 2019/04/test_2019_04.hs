3c29f49521d97262cba398307d553cd9983733c4
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-04 14:16:55 -0400

    Day 04 solution

--- 2019/04/test_2019_04.hs
+++ 2019/04/test_2019_04.hs
@@ -6,11 +6,30 @@ import Puzzle_2019_04
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testDigits :: Test
+testDigits = TestCase $ do
+    [1, 2, 3, 4, 5, 6] @=? digits 123456
+
+testValid :: Test
+testValid = TestCase $ do
+    True @=? validity 111111
+    False @=? validity 223450
+    False @=? validity 123789
+
+
+testValid' :: Test
+testValid' = TestCase $ do
+    True @=? validity' 112233
+    False @=? validity' 123444
+    True @=? validity' 111122
+
+
 testA = TestList
-    []
+    [ TestLabel "Digits" testDigits
+    , TestLabel "Validity" testValid]
 
 testB = TestList
-    []
+    [ TestLabel "Validity'" testValid']
 
 testList = TestList
     [ TestLabel "Part A" testA
