603c21e2d67cbd1d8f7c2c12f51f1626ec92ff47
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-04 14:16:55 -0400

    Day 04 solution

--- 2019/04/puzzle_2019_04.hs
+++ 2019/04/puzzle_2019_04.hs
@@ -2,9 +2,59 @@ module Puzzle_2019_04 where
 
 import Automation (submitAnswer)
 
+
+validity :: Int -> Bool
+validity n = sixDigits ds && nonDecreasingDigits ds && repeatedDigit ds
+    where ds = digits n
+
+validity' :: Int -> Bool
+validity' n = sixDigits ds && nonDecreasingDigits ds && repeatedDigit' ds
+    where ds = digits n
+
+digits :: Int -> [Int]
+digits n = go n []
+  where
+    go :: Int -> [Int] -> [Int]
+    go 0 ls = ls
+    go n ls = go (div n 10) (mod n 10 : ls)
+
+
+sixDigits :: [Int] -> Bool
+sixDigits ds = length ds == 6
+
+
+nonDecreasingDigits :: [Int] -> Bool
+nonDecreasingDigits (x:xs@(y:_)) = (x <= y) && nonDecreasingDigits xs
+nonDecreasingDigits _ = True
+
+
+repeatedDigit :: [Int] -> Bool
+repeatedDigit (x:xs@(y:_)) = (x == y) || repeatedDigit xs
+repeatedDigit _ = False
+
+
+repeatedDigit' :: [Int] -> Bool
+repeatedDigit' (a:b:c:d:e:f:[]) = or [t1, t2, t3, t4, t5]
+  where
+    t1 = a == b && b < c
+    t2 = a < b && b == c && c < d
+    t3 = b < c && c == d && d < e
+    t4 = c < d && d == e && e < f
+    t5 = d < e && e == f
+
+
+validInRange :: Int -> Int -> [Int]
+validInRange lower upper = filter validity [lower..upper]
+
+validInRange' :: Int -> Int -> [Int]
+validInRange' lower upper = filter validity' [lower..upper]
+
+
+
+
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    let answer = length $ validInRange 271973 785961 
     print answer
     -- result <- submitAnswer 2019 04 1 answer
     -- print result
@@ -13,8 +63,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    let answer = length $ validInRange' 271973 785961 
     print answer
-    -- result <- submitAnswer 2019 04 2 answer
-    -- print result
+    result <- submitAnswer 2019 04 2 answer
+    print result
     return ()
