536ab9abc0f7bbeb37c53a68cbfc45e375f71df7
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-04 18:24:43 -0400

    Working version of a mutable vector intcode computer

--- 2019/libraries/intcode.hs
+++ 2019/libraries/intcode.hs
@@ -1,14 +1,8 @@
--- what's the basic architecture?
--- you read a basic list of ints, pass it to execute
--- the main execution loop is to
---      read what op the instruction pointer is telling us to do
---      call the appropriate modification function
---      do that in a (tail-recursive) loop until you hit an exit
-
-
 module Intcode ( Program
                , programP
                , programFromString
+               , prettyFormat
+               , prettyPrint
                , execute) where
 
 
@@ -30,16 +24,15 @@ import qualified Data.Vector.Unboxed.Mutable as VM
 type Program = Vector Int
 type MProgram s = MVector s Int
 
-data Intcode s = Intcode { _iptr :: Int
-                         , _prog :: MProgram s}
+data OPCode = Add
+            | Mul
+            | Exit deriving (Eq, Show)
 
+data OP = OP 
+    { _num :: Int
+    , _code :: OPCode
+    , _width :: Int} deriving (Eq, Show)
 
-data OP = OP { _code :: Int
-             , _width :: Int
-             , _name :: String
-             , _call :: MProgram s -> MProgram s}
-
-makeLenses ''Intcode
 makeLenses ''OP
 
 
@@ -59,59 +52,81 @@ programFromString s = do
 
 
 -- pretty printing programs
+prettyFormat :: Program -> [String]
+prettyFormat prog = helper $ V.toList prog where
+    helper :: [Int] -> [String]
+    helper [] = []
+    helper ls = line : helper rest where
+        (op, _) = opLookup M.! head ls
+        (thisOp, rest) = splitAt (op ^. width) ls
+        thisOpStr = init . tail . show $ thisOp
+        line = show (op ^. code) ++ "\t\t" ++ thisOpStr
+
+
+prettyPrint :: Program -> IO ()
+prettyPrint prog = do 
+    let ls = prettyFormat prog
+    putStrLn $ unlines ls
+
+
 
 
 -- top level executing programs
--- this is the function to call; the job of execute is to construct the
--- MProgram with pointer and pass to the executioner
+-- this is basically a wrapper around runST to pass to the executioner
+-- needs to actually be ST and not State for actual legit mutation
 execute :: Program -> Program
-execute program = evalState go initial where
-    initial = Intcode 0 (VM.thaw program)
-    go 
+execute program = runST $ do
+    mProgram  <- V.thaw program
+    executioner mProgram 0
+    V.freeze mProgram
 
 
 
-
-
-# executioner :: PrimMonad m => Intcode (PrimState m) -> m (Intcode (PrimState m))
-# executioner ic = do
-#     ptrCode <- VM.read (prog ^. ic) (iptr ^. ic)
-#     let op = M.! opLookup ptrCode
-#     if name ^. op == "Exit"
-#         then return ic
-#         else do
-#             ic' <- (call ^. op) ic
-#             executioner ic'
-
-
-
--- op definitions, lookup, call functions
-
-add :: OP
-add = OP 1 4 "Add" opAdd
-
-mul :: OP
-mul = OP 2 4 "Mul" opMul
-
-exit :: OP
-exit = OP 99 1 "Exit" id
-
-
-opAdd :: Program -> Int -> Program
-opAdd p i = p V.// [(z, x+y)]
-  where x = p V.! (p V.! (i+1))
-        y = p V.! (p V.! (i+2))
-        z = p V.! (i+3)
-
-opMul :: Program -> Int -> Program
-opMul p i = p V.// [(z, x*y)]
-  where x = p V.! (p V.! (i+1))
-        y = p V.! (p V.! (i+2))
-        z = p V.! (i+3)
+-- there's an input which is the instruction pointer
+-- the state is the current program
+executioner :: MProgram s -> Int -> ST s ()
+executioner prog iptr = do
+    opNum <- VM.read prog iptr
+    let (op, instruction) = opLookup M.! opNum
+    if op ^. code == Exit
+        then return ()
+        else do
+            instruction prog iptr
+            let iptr' = iptr + (op ^. width)
+            executioner prog iptr'
 
 
 
--- eventually you're gonna want
--- opcode lookup (i.e. for # of fields)
--- mutable programs?
--- pretty printing
+-- op lookup and instruction functions
+
+opLookup :: Map Int (OP, MProgram s -> Int -> ST s())
+opLookup = M.fromList
+    [ ( 1, (OP  1 Add  4, add))
+    , ( 2, (OP  2 Mul  4, mul))
+    , (99, (OP 99 Exit 1, undefined))]
+
+
+add :: MProgram s -> Int -> ST s ()
+add prog iptr = do
+    idx1 <- VM.read prog (iptr+1)
+    idx2 <- VM.read prog (iptr+2)
+    idx3 <- VM.read prog (iptr+3)
+    val1 <- VM.read prog idx1
+    val2 <- VM.read prog idx2
+    VM.write prog idx3 (val1 + val2)
+
+
+mul :: MProgram s -> Int -> ST s ()
+mul prog iptr = do
+    idx1 <- VM.read prog (iptr+1)
+    idx2 <- VM.read prog (iptr+2)
+    idx3 <- VM.read prog (iptr+3)
+    val1 <- VM.read prog idx1
+    val2 <- VM.read prog idx2
+    VM.write prog idx3 (val1 * val2)
+
+
+-- next step is perhaps better use of monads
+-- make the execution types polymorphic instead of restricting to ST
+-- probably eventually you're gonna want IO in the mix
+-- maybe some error handling for when all these lookups start going wrong
