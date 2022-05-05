0edf08e90956fd3eefab5f53f4793ab0e0bafa9a
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-05 09:13:28 -0400

    Day 06 solution

--- 2019/06/puzzle_2019_06.hs
+++ 2019/06/puzzle_2019_06.hs
@@ -1,10 +1,86 @@
 module Puzzle_2019_06 where
 
 import Automation (submitAnswer)
+import Parsing
+import Data.Map.Strict (Map)
+import Data.Set (Set)
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+
+-- data types
+type Orrery = Map String String
+
+
+
+-- parsing
+orbitsP :: Parser Orrery
+orbitsP = do
+    pairs <- sepBy orbitP newline
+    return $ M.fromList pairs
+
+
+orbitP :: Parser (String, String)
+orbitP = do
+    center <- objectP
+    char ')'
+    orbiter <- objectP
+    return (orbiter, center)
+
+
+objectP :: Parser String
+objectP = count' 1 3 (upperChar <|> digitChar)
+
+
+-- functions
+
+
+orbitSum :: Orrery -> Int
+orbitSum orrery = sum . (map (depth orrery)) $ M.keys orrery
+
+
+depth :: Orrery -> String -> Int
+depth orrery object = go object 0 where
+    go :: String -> Int -> Int
+    go object d
+        | object == center = d
+        | otherwise        = go (orrery M.! object) (d+1)
+
+
+decay :: Orrery -> String -> [String]
+decay orrery object
+    | object == center = []
+    | otherwise        = parent : decay orrery parent
+    where parent = orrery M.! object
+
+
+transfers :: Orrery -> String -> String -> Int
+transfers orrery start end = S.size transferPath
+  where
+    sd = S.fromList $ decay orrery start
+    ed = S.fromList $ decay orrery end
+    bothPaths = S.union sd ed
+    stemPath = S.intersection sd ed
+    transferPath = S.difference bothPaths stemPath
+
+
+center :: String
+center = "COM"
+
+
+you :: String
+you = "YOU"
+
+
+santa :: String
+santa = "SAN"
+
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just orrery) <- parseInput orbitsP "06/input.txt"
+    let answer = orbitSum orrery
     print answer
     -- result <- submitAnswer 2019 06 1 answer
     -- print result
@@ -13,8 +89,9 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just orrery) <- parseInput orbitsP "06/input.txt"
+    let answer = transfers orrery you santa
     print answer
-    -- result <- submitAnswer 2019 06 2 answer
-    -- print result
+    result <- submitAnswer 2019 06 2 answer
+    print result
     return ()
--- 2019/06/test_2019_06.hs
+++ 2019/06/test_2019_06.hs
@@ -1,16 +1,27 @@
 import Test.HUnit
 import Puzzle_2019_06
+import Parsing
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+
+testOrbitCount = TestCase $ do
+    (Just orrery) <- parseInput orbitsP "06/test_input.txt"
+    42 @=? orbitSum orrery
+
+
+testTransfers = TestCase $ do
+    (Just orrery) <- parseInput orbitsP "06/test_input_2.txt"
+    4 @=? transfers orrery you santa
+
 testA = TestList
-    []
+    [ TestLabel "Orbit Count" testOrbitCount]
 
 testB = TestList
-    []
+    [ TestLabel "Transfers" testTransfers]
 
 testList = TestList
     [ TestLabel "Part A" testA
