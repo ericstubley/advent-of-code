6b4b77f39dd3d09d3d8fa8657fd6471f618a9371
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-11 10:49:21 -0400

    Day 13 solution

--- 2019/13/puzzle_2019_13.hs
+++ 2019/13/puzzle_2019_13.hs
@@ -1,10 +1,116 @@
 module Puzzle_2019_13 where
 
 import Automation (submitAnswer)
+import Intcode
+import Parsing
+import Control.Monad.IO.Class
+import Data.Conduino
+import Data.Map.Strict (Map)
+import qualified Data.Conduino.Lift as L
+import qualified Data.Map.Strict as M
+
+
+data Tile = Empty | Wall | Block | Paddle | Ball deriving (Eq, Ord, Show, Enum)
+type ScreenMap = Map (Int, Int) Tile
+
+
+
+-- quick hack through part a
+blockCount :: [Int] -> Int
+blockCount = length . (filter (==Block)) . M.elems . tileMap
+
+
+tileMap :: [Int] -> ScreenMap
+tileMap xs = go xs M.empty where
+    go (x:y:t:xs) m = case (x, y) of 
+                        (-1, 0) -> go xs m
+                        (_ , _) -> go xs (M.insert (x, y) (toEnum t) m) 
+    go [] m         = m
+    go (x:y:[]) m   = error "Malformed ouptut"
+    go (x:[])   m   = error "Malformed output"
+
+
+-- the basic loop function will be squeezePipe (program) -> print output -> 
+-- either ask for input and continue or terminate
+
+displayTile :: Tile -> Char
+displayTile t = case t of
+                    Empty  -> ' '
+                    Wall   -> '#'
+                    Block  -> 'X'
+                    Paddle -> 'T'
+                    Ball   -> '@'
+
+
+
+printableScreen :: ScreenMap -> [String]
+printableScreen sm = lls where
+    xs = map fst $ M.keys sm
+    ys = map snd $ M.keys sm
+    (lx, ux) = (minimum xs, maximum xs)
+    (ly, uy) = (minimum ys, maximum ys)
+    ls y = [M.findWithDefault Empty (x, y) sm | x <- [lx..ux]]
+    lls = [map displayTile (ls y) | y <- [ly..uy]]
+
+
+extractScore :: [Int] -> Int
+extractScore (x:y:z:xs)
+    | (-1, 0) == (x, y) = z
+    | otherwise         = extractScore xs
+extractScore _ = 0
+
+
+printScreen :: ScreenMap -> Int -> [Int] -> IO (ScreenMap, Int)
+printScreen sm score output = do 
+    let score' = max score (extractScore output)
+    putStrLn $ "Score: " ++ show score' 
+    let sm' = M.union (tileMap output) sm
+    mapM_  putStrLn $ printableScreen sm'
+    return (sm', score')
+
+
+runGame :: [Int] -> IO ()
+runGame program = gameLoop M.empty 0 (L.execStateP vm execute) where
+    vm = initVM program
+    gameLoop :: (Monad m, MonadIO m) => ScreenMap -> Int -> Pipe Int Int () m a -> m ()
+    gameLoop sm score p = do
+        (output, result) <- squeezePipe p
+        (sm', score') <- liftIO $ printScreen sm score output
+        case result of
+            (Left cont) -> do
+                i <- liftIO $ joystick
+                gameLoop sm' score' (cont i)
+            (Right val) -> return ()
+
+
+joystick :: IO Int
+joystick = do
+    putStrLn "Joystick <_>"
+    j <- getLine
+    case j of
+       "a" -> return (-1)
+       "d" -> return 1
+       _   -> return 0
+
+
+runNoMovement program = hackLoop M.empty 0 (L.execStateP vm execute) where
+    vm = initVM program
+    hackLoop :: (Monad m, MonadIO m) => ScreenMap -> Int -> Pipe Int Int () m a -> m ()
+    hackLoop sm score p = do
+        (output, result) <- squeezePipe p
+        (sm', score') <- liftIO $ printScreen sm score output
+        case result of
+            (Left cont) -> hackLoop sm' score' (cont 0)
+            (Right val) -> return ()
+
+
+
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just breakout) <- parseInput programP "13/input.txt"
+    let (_, output) = runProgram breakout []
+    let answer = blockCount output
     print answer
     -- result <- submitAnswer 2019 13 1 answer
     -- print result
@@ -13,6 +119,9 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
+    (Just breakout) <- parseInput programP "13/sneakier_input.txt"
+    let freeBreakout = 2 : tail breakout
+    runNoMovement freeBreakout
     let answer = 0
     print answer
     -- result <- submitAnswer 2019 13 2 answer
