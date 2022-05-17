9388e2ae0542ed6b0acc679fb67e3f83ddd224bd
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-17 13:28:53 -0400

    Day 21 solution

--- 2019/21/puzzle_2019_21.hs
+++ 2019/21/puzzle_2019_21.hs
@@ -1,10 +1,48 @@
 module Puzzle_2019_21 where
 
 import Automation (submitAnswer)
+import Intcode
+import Parsing
+import Data.Char (ord)
+import Data.Conduino
+import qualified Data.Conduino.Combinators as C
+
+
+programA :: String
+programA = unlines 
+    [ "OR A J"
+    , "AND B J"
+    , "AND C J"
+    , "NOT J J"
+    , "AND D J"
+    , "WALK"]
+
+
+programB :: String
+programB = unlines
+    [ "OR A J"
+    , "AND B J"
+    , "AND C J"
+    , "NOT J J"
+    , "AND D J"
+    , "OR E T"
+    , "OR H T"
+    , "AND T J"
+    , "RUN"]
+
+
+runSpringDroid :: Program -> String -> Int
+runSpringDroid program input = last output
+  where (_, output) = runPipePure
+                         $ C.sourceList input
+                        .| C.map ord
+                        .| intcodePipe program
+                        &| C.sinkList
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just spring) <- parseInput programP "21/input.txt"
+    let answer = runSpringDroid spring programA
     print answer
     -- result <- submitAnswer 2019 21 1 answer
     -- print result
@@ -13,7 +51,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just spring) <- parseInput programP "21/input.txt"
+    let answer = runSpringDroid spring programB
     print answer
     -- result <- submitAnswer 2019 21 2 answer
     -- print result
