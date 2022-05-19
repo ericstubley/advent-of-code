881da10d5fe9e8805fc3fa39e7a1c798570ae393
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-19 09:45:01 -0400

    Day 23 solution

--- 2019/23/puzzle_2019_23.hs
+++ 2019/23/puzzle_2019_23.hs
@@ -5,7 +5,7 @@ import Intcode
 import Parsing
 import Control.Monad.State
 import Data.Conduino
-import Data.Sequence (Seq(..))
+import Data.Sequence (Seq(..), (|>))
 import Data.Void (Void)
 import qualified Data.Conduino.Combinators as C
 import qualified Data.Conduino.Lift as L
@@ -14,36 +14,27 @@ import qualified Data.Sequence as Sq
 
 -- data types
 
-data Packet = EmptyPacket | Packet Int Int Int deriving (Eq, Ord, Show)
+data Packet = EmptyPacket 
+            | Packet Int Int Int deriving (Eq, Ord, Show)
 
 type Queue = Seq Packet
 
-data Resources = Resources {queue :: Queue, nat :: Packet, ycount :: Count}
+
+data Resources = Resources
+    { queue :: Queue
+    , nat :: Packet
+    , prev :: Maybe Int}
     deriving (Eq, Ord, Show)
-
-
-data Count = None | Once | Twice deriving (Eq, Ord, Show, Enum, Bounded)
 
 -- functions
 
 controller :: MonadState Resources m => Pipe () Packet Void m ()
-controller = do
+controller = C.repeatM $ do
     q <- gets queue
     n <- gets nat
-    c <- gets ycount
-    case (q, n) of
-        (Empty, Packet _ _ _) -> do
-            modify (\r -> r {ycount = succ c})
-            yield n
-            controller
-        (Empty, EmptyPacket) -> do
-            modify (\r -> r {ycount = None})
-            yield n
-            controller
-        ((Packet i x y) :<| q', _) -> do
-            modify (\r -> r {queue = q'})
-            yield (Packet i x y)
-            controller
+    case q of
+        Empty      -> return n
+        (p :<| q') -> (modify $ \r -> r {queue = q'}) >> return p
 
 
 preprocessor :: Int -> Pipe Packet Int u m u
@@ -54,91 +45,76 @@ preprocessor ident = do
 
 packetPicker :: Int -> Packet -> Pipe Packet Int u m ()
 packetPicker ident packet = do
-    case packet of
-        EmptyPacket -> yield (-1)
-        (Packet i x y) -> if ident == i
-                            then yield x >> yield y
-                            else yield (-1)
+    case (packet, ident) of
+        (EmptyPacket, _)    -> yield (-1)
+        (Packet 255 x y, 0) -> yield x >> yield y
+        (Packet i x y, _)   -> if i == ident
+            then yield x >> yield y
+            else return ()
 
 
-postprocessorA :: MonadState Resources m => Pipe Int Void u m Int
-postprocessorA = do
+postprocessor :: MonadState Resources m => Pipe Int Void u m a
+postprocessor = do
     ident <- await
-    x <- await
-    y <- await 
-    case (ident, x, y) of
-        (Just 255, Just x', Just y') -> return y'
-        (Just i, Just x', Just y')   -> do 
+    a <- await
+    b <- await 
+    case (ident, a, b) of
+        (Just 255, Just x, Just y) -> do
+            modify $ \r -> r {nat = Packet 255 x y}
+            postprocessor
+        (Just i,   Just x, Just y) -> do 
             q <- gets queue
-            modify $ \r -> r {queue = (q Sq.|> (Packet i x' y'))}
-            postprocessorA
-        (_, _, _)                    -> error "post processor failure"
+            modify $ \r -> r {queue = q |> Packet i x y}
+            postprocessor
+        (_, _, _)              -> error "post processor failure"
 
 
-postprocessorB :: MonadState Resources m => Pipe Int Void u m Int
-postprocessorB = do
-    c <- gets ycount
-    packet <- gets nat
-    if c == Twice 
-        then case packet of 
-                EmptyPacket -> return (-1)
-                (Packet _ _ y) -> return y
-        else do
-            mident <- await
-            mx <- await
-            my <- await 
-            case (packet, mident, mx, my) of
-                (Packet _ _ y', Just 255, Just x, Just y) -> if y == y'
-                    then do
-                        modify $ \r -> r {nat = (Packet 0 x y)}
-                        postprocessorB
-                    else do
-                        modify $ \r -> r {nat = (Packet 0 x y), ycount = None}
-                        postprocessorB
-                (EmptyPacket, Just 255, Just x, Just y) -> do
-                    modify $ \r -> r {nat = (Packet 0 x y), ycount = None}
-                    postprocessorB
-                (_, Just i, Just x, Just y) -> do 
-                    q <- gets queue
-                    modify $ \r -> r {queue = q Sq.|> (Packet i x y)}
-                    postprocessorB
-                (_, _, _, _) -> error "post processor failure"
+node :: MonadState Resources m => Program -> Int -> Pipe Packet Void u m Int
+node program ident = preprocessor ident
+                  .| intcodePipe program
+                  .| postprocessor
 
 
-nodeA :: MonadState Resources m => Program -> Int -> Pipe Packet Void u m Int
-nodeA program ident = preprocessor ident
-                   .| intcodePipe program
-                   .| postprocessorA
+cluster :: MonadState Resources m => Program -> Pipe Packet Void u m Int
+cluster program = foldl1 altSink $ map (node program) [0..49]
 
 
-clusterA :: MonadState Resources m => Program -> Pipe Packet Void u m Int
-clusterA program = foldl1 altSink $ map (nodeA program) [0..49]
+snooperA :: MonadState Resources m => Pipe Packet Void u m Int
+snooperA = do
+    p <- await
+    case p of
+        Just (Packet 255 _ y) -> return y
+        _ -> snooperA
 
 
 networkA :: Program -> Int
-networkA program = runPipePure $ L.evalStateP initRes pipeline 
-  where pipeline = controller .| clusterA program
-        initRes = Resources Empty EmptyPacket None
+networkA program = runPipePure $ L.evalStateP initRes pipeline where
+    initRes = Resources Empty EmptyPacket Nothing
+    pipeline = controller 
+            .| altSink snooperA (cluster program)
 
 
-
-nodeB :: MonadState Resources m => Program -> Int -> Pipe Packet Void u m Int
-nodeB program ident = preprocessor ident
-                   .| intcodePipe program
-                   .| postprocessorB
-
-
-clusterB :: MonadState Resources m => Program -> Pipe Packet Void u m Int
-clusterB program = foldl1 altSink $ map (nodeB program) [0..49]
+snooperB :: MonadState Resources m => Pipe Packet Void u m Int
+snooperB = do
+    p <- await
+    py <- gets prev
+    case (p, py) of
+        (Just (Packet 255 _ y), Just y') -> if y == y' 
+            then return y
+            else do
+                modify $ \r -> r {prev = Just y}
+                snooperB
+        (Just (Packet 255 _ y), _) -> do 
+                modify $ \r -> r {prev = Just y}
+                snooperB
+        (_, _) -> snooperB
 
 
 networkB :: Program -> Int
-networkB program = runPipePure $ L.evalStateP initRes pipeline 
-  where pipeline = controller .| clusterB program
-        initRes = Resources Empty EmptyPacket None
-
-
-
+networkB program = runPipePure $ L.evalStateP initRes pipeline where
+    initRes = Resources Empty EmptyPacket Nothing
+    pipeline = controller 
+            .| altSink snooperB (cluster program)
 
 
 -- mains
@@ -158,7 +134,6 @@ mainB :: IO ()
 mainB = do
     (Just program) <- parseInput programP "23/input.txt"
     let answer = networkB program
-    -- let answer = 0
     print answer
     -- result <- submitAnswer 2019 23 2 answer
     -- print result
