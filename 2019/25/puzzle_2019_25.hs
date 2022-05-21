d7b85d18aa1fc21b23970d1d2ecfd2f2629a5542
Eric Stubley <ericdavidstubley@gmail.com>
2022-05-21 10:02:25 -0400

    Day 25 solution

--- 2019/25/puzzle_2019_25.hs
+++ 2019/25/puzzle_2019_25.hs
@@ -1,10 +1,38 @@
 module Puzzle_2019_25 where
 
 import Automation (submitAnswer)
+import Intcode
+import Parsing
+
+
+weightWatcher :: String
+weightWatcher = unlines 
+    [ "west"
+    , "west"
+    , "take bowl of rice"
+    , "east"
+    , "north"
+    , "east"
+    , "south"
+    , "take dark matter"
+    , "north"
+    , "west"
+    , "north"
+    , "take candy cane"
+    , "west"
+    , "west"
+    , "north"
+    , "take dehydrated water"
+    , "west"
+    , "south"]
+
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just rpg) <- parseInput programP "25/input.txt"
+    -- runInteractiveAscii rpg
+    let (_, output) = runProgramAscii rpg weightWatcher 
+    let answer = last $ lines output
     print answer
     -- result <- submitAnswer 2019 25 1 answer
     -- print result
