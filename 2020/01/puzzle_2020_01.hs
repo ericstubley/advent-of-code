5cdf479beab3b152567ff93391fbd866fe3561bb
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-01 08:26:32 -0400

    Day 01 solution

--- 2020/01/puzzle_2020_01.hs
+++ 2020/01/puzzle_2020_01.hs
@@ -1,11 +1,40 @@
 module Puzzle_2020_01 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.Set (Set)
+import qualified Data.Set as S
+
+-- parsing
+expensesP :: Parser (Set Int)
+expensesP = do
+    es <- sepBy integer newline
+    return $ S.fromList es
+
+
+-- actual functions
+findPair :: Int -> Set Int -> Maybe Int
+findPair goal s = S.lookupMax filtered >>= (\x -> Just (x * (goal - x)))
+  where filtered = S.filter (\x -> S.member (goal - x) s) s
+
+
+findMatch :: Set Int -> Int
+findMatch s = prod
+  where (Just prod) = findPair 2020 s
+
+
+findThreeMatch :: Set Int -> Int
+findThreeMatch s = m * prod
+  where m = S.findMax $ S.filter cond s
+        cond a = findPair (2020 - a) (S.delete a s) /= Nothing
+        (Just prod) = findPair (2020-m) (S.delete m s)
+
+
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just expenses) <- parseInput expensesP "01/input.txt"
+    let answer = findMatch expenses
     print answer
     -- result <- submitAnswer 2020 01 1 answer
     -- print result
@@ -14,7 +43,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just expenses) <- parseInput expensesP "01/input.txt"
+    let answer = findThreeMatch expenses
     print answer
     -- result <- submitAnswer 2020 01 2 answer
     -- print result
--- 2020/01/test_2020_01.hs
+++ 2020/01/test_2020_01.hs
@@ -1,16 +1,28 @@
 import Test.HUnit
 import Puzzle_2020_01
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testExpenses :: Test
+testExpenses = TestCase $ do
+    (Just expenses) <- parseInput expensesP "01/test_input.txt"
+    514579 @=? findMatch expenses 
+
+
+testThreeExpenses :: Test
+testThreeExpenses = TestCase $ do
+    (Just expenses) <- parseInput expensesP "01/test_input.txt"
+    241861950 @=? findThreeMatch expenses 
+
 
 testA = TestList
-    []
+    [ TestLabel "Expenses" testExpenses]
 
 testB = TestList
-    []
+    [ TestLabel "Three Expenses" testThreeExpenses]
 
 testList = TestList
     [ TestLabel "Part A" testA
