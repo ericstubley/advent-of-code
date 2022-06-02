ee36668954d759cf2b45ccf60d6ccb8f289b6966
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-02 13:33:12 -0400

    Day 03 solution

--- 2020/03/test_2020_03.hs
+++ 2020/03/test_2020_03.hs
@@ -1,16 +1,30 @@
 import Test.HUnit
 import Puzzle_2020_03
+import Parsing (parseInput)
+import Data.Massiv.Array (Ix2(..))
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testTreesHit :: Test
+testTreesHit = TestCase $ do
+    (Just trees) <- parseInput treesP "03/test_input.txt"
+    7 @=? treesHit (1:.3) trees 
+
+
+testAverageHits :: Test
+testAverageHits = TestCase $ do
+    (Just trees) <- parseInput treesP "03/test_input.txt"
+    336 @=? averageHits trees
+
+
 testA = TestList
-    []
+    [ TestLabel "Small test track" testTreesHit]
 
 testB = TestList
-    []
+    [ TestLabel "Average Hits" testAverageHits]
 
 testList = TestList
     [ TestLabel "Part A" testA
