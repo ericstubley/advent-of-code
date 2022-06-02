87c4691a212480742ef4a332800951f37f2709a1
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-02 09:46:35 -0400

    Day 02 solution

--- 2020/02/puzzle_2020_02.hs
+++ 2020/02/puzzle_2020_02.hs
@@ -1,11 +1,60 @@
 module Puzzle_2020_02 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+
+-- data types
+data Rule = Rule
+    { lower :: Int
+    , upper :: Int
+    , letter :: Char}
+    deriving (Eq, Ord, Show)
+
+type Password = String
+
+-- parsing
+databaseP :: Parser [(Rule, Password)]
+databaseP = sepBy entryP newline
+
+entryP :: Parser (Rule, Password)
+entryP = do
+    m <- integer
+    char '-'
+    n <- integer
+    char ' '
+    c <- lowerChar
+    string ": "
+    pwd <- some lowerChar
+
+    return (Rule m n c, pwd)
+
+-- functions
+isValid :: Rule -> Password -> Bool
+isValid r pwd = lower r <= occurences && occurences <= upper r
+  where occurences = length $ filter (== letter r) pwd 
+
+
+isPositional :: Rule -> Password -> Bool
+isPositional (Rule l u c) pwd
+    | length pwd < u = False 
+    | otherwise      = xor lCond rCond
+      where lCond = pwd !! (l-1) == c
+            rCond = pwd !! (u-1) == c
+
+
+xor :: Bool -> Bool -> Bool
+xor a b = a /= b
+
+
+countValid :: (Rule -> Password -> Bool) -> [(Rule, Password)] -> Int
+countValid validator db = length $ filter (\e -> validator (fst e) (snd e)) db
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just database) <- parseInput databaseP "02/input.txt"
+    let answer = countValid isValid database
     print answer
     -- result <- submitAnswer 2020 02 1 answer
     -- print result
@@ -14,7 +63,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just database) <- parseInput databaseP "02/input.txt"
+    let answer = countValid isPositional database
     print answer
     -- result <- submitAnswer 2020 02 2 answer
     -- print result
--- 2020/02/test_2020_02.hs
+++ 2020/02/test_2020_02.hs
@@ -1,16 +1,38 @@
 import Test.HUnit
 import Puzzle_2020_02
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testValidity :: Test
+testValidity = TestCase $ do
+    (Just database) <- parseInput databaseP "02/test_input.txt"
+    let (r0, p0) = database !! 0 
+    let (r1, p1) = database !! 1 
+    let (r2, p2) = database !! 2 
+    True  @=? isValid r0 p0
+    False @=? isValid r1 p1
+    True  @=? isValid r2 p2
+
+
+testPositional :: Test
+testPositional = TestCase $ do
+    (Just database) <- parseInput databaseP "02/test_input.txt"
+    let (r0, p0) = database !! 0 
+    let (r1, p1) = database !! 1 
+    let (r2, p2) = database !! 2 
+    True  @=? isPositional r0 p0
+    False @=? isPositional r1 p1
+    False @=? isPositional r2 p2
+
 
 testA = TestList
-    []
+    [ TestLabel "Validity" testValidity]
 
 testB = TestList
-    []
+    [ TestLabel "Positional Validity" testPositional]
 
 testList = TestList
     [ TestLabel "Part A" testA
