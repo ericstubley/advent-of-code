028772ff189a8ff2a36e77500fd2f8d1ef604897
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-02 13:33:12 -0400

    Day 03 solution

--- 2020/03/puzzle_2020_03.hs
+++ 2020/03/puzzle_2020_03.hs
@@ -1,11 +1,40 @@
 module Puzzle_2020_03 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Grid
+import qualified Data.Massiv.Array as A
+
+-- parsing
+treesP :: Parser (Array2 Char)
+treesP = do
+    trees <- sepBy rowP newline
+    return $ A.fromLists' A.Seq trees
+
+
+rowP :: Parser [Char]
+rowP = some (char '.' <|> char '#')
+
+
+-- functions
+treesHit :: Ix2 -> Array2 Char -> Int
+treesHit (si:.sj) trees = length $ filter (== '#') path
+  where (A.Sz (li:.lj)) = A.size trees
+        inBounds (i:.j) = 0 <= i && i < li
+        ixs = takeWhile inBounds [n*si :. (mod (n*sj) lj) | n <- [0..]]
+        path = map (\ix -> A.index' trees ix) ixs
+
+
+averageHits :: Array2 Char -> Int
+averageHits trees = product $ map (\s -> treesHit s trees) slopes
+  where slopes = [1:.1, 1:.3, 1:.5, 1:.7, 2:.1]
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just trees) <- parseInput treesP "03/input.txt"
+    let answer = treesHit (1:.3) trees
     print answer
     -- result <- submitAnswer 2020 03 1 answer
     -- print result
@@ -14,7 +43,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just trees) <- parseInput treesP "03/input.txt"
+    let answer = averageHits trees
     print answer
     -- result <- submitAnswer 2020 03 2 answer
     -- print result
