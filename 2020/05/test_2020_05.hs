91d5a26f4b26ebc437412c73c879ffeb381495f2
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-03 21:21:08 -0400

    Day 05 solution

--- 2020/05/test_2020_05.hs
+++ 2020/05/test_2020_05.hs
@@ -1,13 +1,19 @@
 import Test.HUnit
 import Puzzle_2020_05
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testSeatID :: Test
+testSeatID = TestCase $ do
+    (Just seats) <- parseInput seatsP "05/test_input.txt"
+    [567, 119, 820] @=? seats 
+
 testA = TestList
-    []
+    [ TestLabel "Seat IDs" testSeatID]
 
 testB = TestList
     []
