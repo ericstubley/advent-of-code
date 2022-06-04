f722c8a081ae0d98722d61494ad6154fe5ea7438
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-03 21:21:18 -0400

    Day 06 solution

--- 2020/06/puzzle_2020_06.hs
+++ 2020/06/puzzle_2020_06.hs
@@ -1,11 +1,35 @@
 module Puzzle_2020_06 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.Set (Set)
+import qualified Data.Set as S
+
+-- parsing
+groupsP :: Parser [[String]]
+groupsP = sepBy groupP newline
+
+groupP :: Parser [String]
+groupP = some personP
+
+personP :: Parser String
+personP = some lowerChar <* optional newline
+
+
+-- functions
+score :: [String] -> Int
+score group = S.size $ S.fromList (concat group)
+
+
+score' :: [String] -> Int
+score' group = S.size $ foldl1 S.intersection (map S.fromList group)
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just groups) <- parseInput groupsP "06/input.txt"
+    let answer = sum $ map score groups
     print answer
     -- result <- submitAnswer 2020 06 1 answer
     -- print result
@@ -14,7 +38,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just groups) <- parseInput groupsP "06/input.txt"
+    let answer = sum $ map score' groups
     print answer
     -- result <- submitAnswer 2020 06 2 answer
     -- print result
--- 2020/06/test_2020_06.hs
+++ 2020/06/test_2020_06.hs
@@ -1,16 +1,28 @@
 import Test.HUnit
 import Puzzle_2020_06
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testScores :: Test
+testScores = TestCase $ do
+    (Just groups) <- parseInput groupsP "06/test_input.txt"
+    [3, 3, 3, 1, 1] @=? map score groups
+
+
+testScores' :: Test
+testScores' = TestCase $ do
+    (Just groups) <- parseInput groupsP "06/test_input.txt"
+    [3, 0, 1, 1, 1] @=? map score' groups
+
 testA = TestList
-    []
+    [TestLabel "Scores" testScores]
 
 testB = TestList
-    []
+    [TestLabel "Scores'" testScores']
 
 testList = TestList
     [ TestLabel "Part A" testA
