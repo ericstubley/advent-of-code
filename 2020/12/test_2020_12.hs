c419be30dd81d23ffafc916f0ed9bb7476a534c2
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-07 12:07:14 -0400

    Day 12 solution

--- 2020/12/test_2020_12.hs
+++ 2020/12/test_2020_12.hs
@@ -1,16 +1,28 @@
 import Test.HUnit
 import Puzzle_2020_12
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testNavigation :: Test
+testNavigation = TestCase $ do
+    (Just commands) <- parseInput commandsP "12/test_input.txt"
+    (17, -8) @=? finalPosition commands 
+
+
+testRelative :: Test
+testRelative = TestCase $ do
+    (Just commands) <- parseInput commandsP "12/test_input.txt"
+    (214, -72) @=? finalRelative commands
+
 testA = TestList
-    []
+    [ TestLabel "Navigation" testNavigation]
 
 testB = TestList
-    []
+    [ TestLabel "Relative" testRelative]
 
 testList = TestList
     [ TestLabel "Part A" testA
