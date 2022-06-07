078190979d4ff9c6e65c3396abbeae404ccab297
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-07 09:23:30 -0400

    Day 11 solution

--- 2020/11/test_2020_11.hs
+++ 2020/11/test_2020_11.hs
@@ -1,16 +1,32 @@
 import Test.HUnit
 import Puzzle_2020_11
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testStable :: Test
+testStable = TestCase $ do
+    (Just initial) <- parseInput seatsP "11/test_input.txt"
+    (Just stable) <- parseInput seatsP "11/test_input_stable.txt"
+    stable @=? stableState initial
+    37 @=? countOccupied stable
+
+
+testStable' :: Test
+testStable' = TestCase $ do
+    (Just initial) <- parseInput seatsP "11/test_input.txt"
+    (Just stable) <- parseInput seatsP "11/test_input_stable_2.txt"
+    stable @=? stableState' initial
+    26 @=? countOccupied stable
+
 testA = TestList
-    []
+    [ TestLabel "Stable Seating" testStable]
 
 testB = TestList
-    []
+    [ TestLabel "Stable Seating 2" testStable']
 
 testList = TestList
     [ TestLabel "Part A" testA
