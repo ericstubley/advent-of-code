6cb95107c2bb657b128c55d587fa9c1aa2c4e881
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-08 09:26:07 -0400

    Day 13 solution

--- 2020/13/puzzle_2020_13.hs
+++ 2020/13/puzzle_2020_13.hs
@@ -1,11 +1,77 @@
 module Puzzle_2020_13 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Utilities (minimumWith)
+
+-- parsing
+
+scheduleP :: Parser (Int, [Int])
+scheduleP = do
+    time <- integer
+    newline
+    routes <- sepBy routeP (char ',')
+    return (time, filter (/= (-1)) routes)
+
+routeP :: Parser Int
+routeP = (char 'x' >> return (-1)) <|> integer
+
+
+contestP :: Parser [(Int, Int)]
+contestP = do
+    routes <- sepBy routeP (char ',')
+    let pairs = zip routes ([0..])
+    return $ filter (\x -> fst x /= (-1)) pairs
+
+
+testConP :: Parser [[(Int, Int)]]
+testConP = sepBy contestP newline
+
+actualConP :: Parser [(Int, Int)]
+actualConP = integer >> newline >> contestP
+
+
+-- functions
+
+waitingTime :: Int -> Int -> Int
+waitingTime time bus = mod (-time) bus
+
+
+earliestBus :: Int -> [Int] -> Int
+earliestBus time buses = minimumWith (waitingTime time) buses
+
+
+solutionA :: Int -> [Int] -> Int
+solutionA time buses = eb * wt
+  where eb = earliestBus time buses
+        wt = waitingTime time eb
+
+
+winner :: [(Int, Int)] -> Int
+winner contest = soln 
+  where reformat (n, a) = (n, mod (-a) n)
+        contest' = map reformat contest
+        (_, soln) = foldl1 crt contest'
+
+
+
+crt :: (Int, Int) -> (Int, Int) -> (Int, Int)
+crt (m, a) (n, b) = (lcm m n, soln)
+  where opts = [a + k*m | k <- [0..]]
+        soln = head $ dropWhile (\x -> mod x n /= b) opts
+
+
+verify :: [(Int, Int)] -> Int -> Bool
+verify contest soln = all checker contest
+  where checker (m, a) = mod soln m == mod (-a) m
+
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just (time, buses)) <- parseInput scheduleP "13/input.txt"
+    let answer = solutionA time buses
     print answer
     -- result <- submitAnswer 2020 13 1 answer
     -- print result
@@ -14,7 +80,9 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just contest) <- parseInput actualConP "13/input.txt"
+    let answer = winner contest
+    print $ verify contest answer
     print answer
     -- result <- submitAnswer 2020 13 2 answer
     -- print result
--- 2020/13/test_2020_13.hs
+++ 2020/13/test_2020_13.hs
@@ -1,16 +1,38 @@
 import Test.HUnit
 import Puzzle_2020_13
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testBus :: Test
+testBus = TestCase $ do
+    (Just (time, buses)) <- parseInput scheduleP "13/test_input.txt"
+    59 @=? earliestBus time buses
+    5 @=? waitingTime time 59
+
+
+testCrt :: Test
+testCrt = TestCase $ do
+    (221, 102) @=? crt (17, 0) (13, 11)
+    (4199, 3417) @=? crt (221, 102) (19, 16)
+    (4199, 3417) @=? foldl1 crt [(17, 0), (13, 11), (19, 16)]
+
+
+testContests :: Test
+testContests = TestCase $ do
+    (Just contests) <- parseInput testConP "13/test_contests.txt"
+    [1068781, 3417, 754018, 779210, 1261476, 1202161486] @=? map winner contests
+
+
 testA = TestList
-    []
+    [ TestLabel "Bus bus bus" testBus]
 
 testB = TestList
-    []
+    [ TestLabel "CRT" testCrt
+    , TestLabel "Contests" testContests]
 
 testList = TestList
     [ TestLabel "Part A" testA
