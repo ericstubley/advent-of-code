949ef5dce1b1988a703c358076fa21f4235f6e59
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-08 15:04:10 -0400

    Day 15 solution

--- 2020/15/puzzle_2020_15.hs
+++ 2020/15/puzzle_2020_15.hs
@@ -1,11 +1,37 @@
 module Puzzle_2020_15 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as IM
+
+-- functions
+-- want to generate an infinite list of the game
+-- to generate the next all you need is 
+--      a) the record of the last time each number was said
+--      b) the last number said
+--      c) the current turn number
+next :: IntMap Int -> Int -> Int -> Int
+next record said turn
+    | IM.member said record = turn - (record IM.! said)
+    | otherwise             = 0
+
+
+game :: [Int] -> Int -> Int
+game initial desired = go record (last initial) (length initial)
+  where record = IM.fromList $ zip (init initial) [1..]
+        go :: IntMap Int -> Int -> Int -> Int
+        go record said turn 
+            | turn == desired = said
+            | otherwise       = go record' said' (turn+1)
+              where said' = next record said turn
+                    record' = IM.insert said turn record
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    let initial = [7,14,0,17,11,1,2] 
+    let answer = game initial 2020
     print answer
     -- result <- submitAnswer 2020 15 1 answer
     -- print result
@@ -14,7 +40,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    let initial = [7,14,0,17,11,1,2] 
+    let answer = game initial 30000000
     print answer
     -- result <- submitAnswer 2020 15 2 answer
     -- print result
--- 2020/15/test_2020_15.hs
+++ 2020/15/test_2020_15.hs
@@ -5,9 +5,28 @@ import Puzzle_2020_15
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testDepth :: Test
+testDepth = TestCase $ do
+    [0,3,3,1,0,4,0] @=? (map (game [0,3,6]) [4..10])
+
+
+
+testGame :: Test
+testGame = TestCase $ do
+    436  @=? game [0,3,6] 2020
+    1    @=? game [1,3,2] 2020
+    10   @=? game [2,1,3] 2020
+    27   @=? game [1,2,3] 2020
+    78   @=? game [2,3,1] 2020
+    438  @=? game [3,2,1] 2020
+    1836 @=? game [3,1,2] 2020
+
+
+
 
 testA = TestList
-    []
+    [ TestLabel "Test in depth" testDepth
+    , TestLabel "Test Game" testGame]
 
 testB = TestList
     []
