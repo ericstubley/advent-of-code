caae746fc77b72890a4de6da55bc1b001118b887
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-09 10:26:14 -0400

    Day 17 solution

--- 2020/17/test_2020_17.hs
+++ 2020/17/test_2020_17.hs
@@ -1,13 +1,22 @@
 import Test.HUnit
 import Puzzle_2020_17
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testSimulate :: Test
+testSimulate = TestCase $ do
+    (Just grid) <- parseInput gridP "17/test_input.txt"
+    5 @=? (numberActive grid)
+    11 @=? (numberActive $ step grid )
+    112 @=? (numberActive $ (iterate step grid) !! 6)
+
+
 testA = TestList
-    []
+    [ TestLabel "Simulation" testSimulate]
 
 testB = TestList
     []
