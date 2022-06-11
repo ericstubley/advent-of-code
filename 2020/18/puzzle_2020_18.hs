6d9b04cf081cb16f25ce4738766974e8182d8410
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-11 09:27:00 -0400

    Day 18 solution

--- 2020/18/puzzle_2020_18.hs
+++ 2020/18/puzzle_2020_18.hs
@@ -1,11 +1,101 @@
 module Puzzle_2020_18 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+
+-- data types
+data Expr = Val Int | Add Expr Expr | Mul Expr Expr
+    deriving (Eq, Ord, Show)
+
+-- parsing
+exprsP :: Parser [Expr]
+exprsP = sepBy exprP newline
+
+exprP :: Parser Expr
+exprP = try (opP) <|> termP
+
+
+termP :: Parser Expr
+termP = (Val <$> integer) <|>
+        ((Val . evaluate) <$> (between (char '(') (char ')') exprP))
+
+
+advancedsP :: Parser [Expr]
+advancedsP = sepBy advancedP newline
+
+advancedP :: Parser Expr
+advancedP = try (aOpP) <|> aTermP
+
+
+aTermP :: Parser Expr
+aTermP = (Val <$> integer) <|>
+         ((Val . advanced) <$> (between (char '(') (char ')') advancedP))
+
+
+opP :: Parser Expr
+opP = do
+    e1 <- termP
+    space
+    cons <- addP <|> mulP
+    space
+    e2 <- exprP
+    return $ cons e1 e2
+
+
+aOpP :: Parser Expr
+aOpP = do
+    e1 <- aTermP
+    space
+    cons <- addP <|> mulP
+    space
+    e2 <- advancedP
+    return $ cons e1 e2
+
+
+addP :: Parser (Expr -> Expr -> Expr)
+addP = char '+' >> return Add
+
+
+mulP :: Parser (Expr -> Expr -> Expr)
+mulP = char '*' >> return Mul
+
+
+-- functions
+evaluate :: Expr -> Int
+evaluate (Val n)     = n
+evaluate (Add e1 e2) = 
+    case e2 of 
+        (Val n)     -> (evaluate e1) + n
+        (Add f1 f2) -> evaluate $ Add intermediate f2
+          where intermediate = Val $ (evaluate e1) + (evaluate f1)
+        (Mul f1 f2) -> evaluate $ Mul intermediate f2
+          where intermediate = Val $ (evaluate e1) + (evaluate f1)
+evaluate (Mul e1 e2) = case e2 of
+        (Val n)     -> (evaluate e1) * n
+        (Add f1 f2) -> evaluate $ Add intermediate f2
+          where intermediate = Val $ (evaluate e1) * (evaluate f1)
+        (Mul f1 f2) -> evaluate $ Mul intermediate f2
+          where intermediate = Val $ (evaluate e1) * (evaluate f1)
+
+
+advanced :: Expr -> Int
+advanced (Val n)     = n
+advanced (Add e1 e2) = 
+    case e2 of 
+        (Val n)     -> (advanced e1) + n
+        (Add f1 f2) -> advanced $ Add intermediate f2
+          where intermediate = Val $ (advanced e1) + (advanced f1)
+        (Mul f1 f2) -> advanced $ Mul intermediate f2
+          where intermediate = Val $ (advanced e1) + (advanced f1)
+advanced (Mul e1 e2) = (advanced e1) * (advanced e2)
+
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just exprs) <- parseInput exprsP "18/input.txt"
+    let answer = sum $ map evaluate exprs
     print answer
     -- result <- submitAnswer 2020 18 1 answer
     -- print result
@@ -14,7 +104,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just exprs) <- parseInput advancedsP "18/input.txt"
+    let answer = sum $ map advanced exprs
     print answer
     -- result <- submitAnswer 2020 18 2 answer
     -- print result
--- 2020/18/test_2020_18.hs
+++ 2020/18/test_2020_18.hs
@@ -1,16 +1,28 @@
 import Test.HUnit
 import Puzzle_2020_18
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testEvaluate :: Test
+testEvaluate = TestCase $ do
+    (Just exprs) <- parseInput exprsP "18/test_input.txt"
+    [71, 51, 26, 437, 12240, 13632] @=? map evaluate exprs 
+
+
+testAdvanced :: Test
+testAdvanced = TestCase $ do
+    (Just exprs) <- parseInput advancedsP "18/test_input.txt"
+    [231, 51, 46, 1445, 669060, 23340] @=? map advanced exprs
+
 
 testA = TestList
-    []
+    [ TestLabel "Evaluate" testEvaluate]
 
 testB = TestList
-    []
+    [ TestLabel "Advanced" testAdvanced]
 
 testList = TestList
     [ TestLabel "Part A" testA
