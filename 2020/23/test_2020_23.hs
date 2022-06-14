4c4128a758d39ac498f53a257a0b45e008152141
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-14 11:09:57 -0400

    Day 23 solution

--- 2020/23/test_2020_23.hs
+++ 2020/23/test_2020_23.hs
@@ -6,11 +6,22 @@ import Puzzle_2020_23
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testCups :: Test
+testCups = TestCase $ do
+    92658374 @=? (after1 $ play 389125467 10)
+    67384529 @=? (after1 $ play 389125467 100)
+
+
+testBig :: Test
+testBig = TestCase $ do
+    149245887792 @=? (nbhd $ playBig 389125467 10000000) 
+
+
 testA = TestList
-    []
+    [ TestLabel "Cup Shuffle" testCups]
 
 testB = TestList
-    []
+    [ TestLabel "Big Test" testBig]
 
 testList = TestList
     [ TestLabel "Part A" testA
