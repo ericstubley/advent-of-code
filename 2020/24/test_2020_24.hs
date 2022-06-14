f4cb6bf22a61f621d026696dc1c49ce72e770bc0
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-14 13:47:54 -0400

    Day 24 solution

--- 2020/24/test_2020_24.hs
+++ 2020/24/test_2020_24.hs
@@ -1,16 +1,30 @@
 import Test.HUnit
 import Puzzle_2020_24
+import Parsing (parseInput)
+import qualified Data.Set as S
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testTiles :: Test
+testTiles = TestCase $ do
+    (Just paths) <- parseInput pathsP "24/test_input.txt"
+    10 @=? countTiles paths 
+
+
+testExhibit :: Test
+testExhibit = TestCase $ do
+    (Just paths) <- parseInput pathsP "24/test_input.txt"
+    let lobby = tileSet paths
+    [10,15,12,25,14,23,28,41,37,49,37] @=? (map S.size $ take 11 (iterate update lobby))
+
 
 testA = TestList
-    []
+    [ TestLabel "Tiles" testTiles]
 
 testB = TestList
-    []
+    [ TestLabel "Exhibit" testExhibit]
 
 testList = TestList
     [ TestLabel "Part A" testA
