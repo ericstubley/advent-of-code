8d7f12771136ddeea72b2c4b457feff4c0f320f7
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-14 14:24:27 -0400

    Day 25 solution

--- 2020/25/puzzle_2020_25.hs
+++ 2020/25/puzzle_2020_25.hs
@@ -1,14 +1,36 @@
 module Puzzle_2020_25 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Data.Bits
+
+
+-- mod 20201227
+base :: Int
+base = 20201227
+-- generator 7
+discreteLog :: Int -> Int
+discreteLog n = go 0 1
+  where go :: Int -> Int -> Int
+        go e k
+            | k == n    = e
+            | otherwise = go (e+1) (mod (7*k) base)
+
+
+encryptionKey :: Int -> Int -> Int
+encryptionKey p1 p2 = modExp p2 (discreteLog p1) base
+
+
+modExp :: Int -> Int -> Int -> Int
+modExp b 0 m = 1
+modExp b e m = t * modExp ((b * b) `mod` m) (shiftR e 1) m `mod` m
+           where t = if testBit e 0 then b `mod` m else 1
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    let answer = encryptionKey 19241437 17346587
     print answer
-    -- result <- submitAnswer 2020 25 1 answer
-    -- print result
+    result <- submitAnswer 2020 25 1 answer
+    print result
     return ()
 
 
--- 2020/25/test_2020_25.hs
+++ 2020/25/test_2020_25.hs
@@ -5,9 +5,21 @@ import Puzzle_2020_25
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testDiscreteLog :: Test
+testDiscreteLog = TestCase $ do
+    8 @=? discreteLog 5764801
+    11 @=? discreteLog 17807724 
+
+
+testEncryptionKey :: Test
+testEncryptionKey = TestCase $ do
+   14897079 @=? encryptionKey 5764801 17807724
+
+
 
 testA = TestList
-    []
+    [ TestLabel "Discrete Log" testDiscreteLog
+    , TestLabel "Encryption Key" testEncryptionKey]
 
 testB = TestList
     []
