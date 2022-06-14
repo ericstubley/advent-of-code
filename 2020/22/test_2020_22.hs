217536cdf241185877e0c2f485de64b96e86f6ff
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-14 08:37:52 -0400

    Day 22 solution

--- 2020/22/test_2020_22.hs
+++ 2020/22/test_2020_22.hs
@@ -1,16 +1,31 @@
 import Test.HUnit
 import Puzzle_2020_22
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testCombat :: Test
+testCombat = TestCase $ do
+    (Just (deck1, deck2)) <- parseInput decksP "22/test_input.txt"
+    let final = combat deck1 deck2
+    306 @=? score final 
+
+
+testRecursive :: Test
+testRecursive = TestCase $ do
+    (Just (deck1, deck2)) <- parseInput decksP "22/test_input.txt"
+    let final = recursiveCombat deck1 deck2
+    291 @=? score final
+
+
 
 testA = TestList
-    []
+    [ TestLabel "Combat" testCombat]
 
 testB = TestList
-    []
+    [ TestLabel "Recursive Combat" testRecursive]
 
 testList = TestList
     [ TestLabel "Part A" testA
