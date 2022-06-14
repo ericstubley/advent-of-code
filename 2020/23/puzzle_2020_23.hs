e99f6c5a36e5a6b9bd32031b892030abafd7d360
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-14 10:30:07 -0400

    Part a solution, commiting early so I can change it all

--- 2020/23/puzzle_2020_23.hs
+++ 2020/23/puzzle_2020_23.hs
@@ -1,11 +1,65 @@
 module Puzzle_2020_23 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Data.Foldable (toList)
+import Data.Sequence (Seq(..), (|>), (<|), (><))
+import qualified Data.Sequence as S
+
+-- data types
+type Cups = Seq Int
+
+-- functions
+layout :: Int -> Cups
+layout 0 = Empty
+layout n = layout (div n 10) |> (mod n 10)
+
+
+after1 :: Cups -> Int
+after1 (1:<|cs) = go 0 cs
+  where go :: Int -> Cups -> Int
+        go n Empty = n
+        go n (c:<|cs) = go (10*n + c) cs
+after1 cs = after1 $ rotate cs
+
+
+rotate :: Seq a -> Seq a
+rotate Empty = Empty
+rotate (a:<|s) = s |> a
+
+
+-- take 3
+-- compute next
+-- find next
+-- insert 3
+-- rotate
+shuffle :: Cups -> Cups
+shuffle cups@(curr:<|rest) = rest'' |> curr
+  where (temp, rest') = S.splitAt 3 rest
+        dest = destination curr temp
+        (Just ix) = S.elemIndexL dest rest'
+        (before, after) = S.splitAt (ix+1) rest'
+        rest'' = before >< temp >< after
+
+
+destination :: Int -> Cups -> Int
+destination curr temp
+    | search == Nothing = next
+    | otherwise         = destination next temp
+      where next = center (curr-1)
+            search = S.elemIndexL next temp
+
+
+center :: Int -> Int
+center n
+    | n < 1     = n + 9
+    | n > 9     = n - 9
+    | otherwise = n
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    let answer = after1 $ iterate shuffle (layout 137826495) !! 100
     print answer
     -- result <- submitAnswer 2020 23 1 answer
     -- print result
