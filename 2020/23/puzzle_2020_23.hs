33de59af7d338df61078ad008a13835690be4021
Eric Stubley <ericdavidstubley@gmail.com>
2022-06-14 11:09:57 -0400

    Day 23 solution

--- 2020/23/puzzle_2020_23.hs
+++ 2020/23/puzzle_2020_23.hs
@@ -1,65 +1,98 @@
 module Puzzle_2020_23 where
 
 import Automation (submitAnswer)
-import Data.Foldable (toList)
-import Data.Sequence (Seq(..), (|>), (<|), (><))
-import qualified Data.Sequence as S
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
 
 -- data types
-type Cups = Seq Int
+type Cups = IntMap Int
+
 
 -- functions
+digits :: Int -> [Int]
+digits 0 = []
+digits n = digits (div n 10) ++ [mod n 10]
+
+
 layout :: Int -> Cups
-layout 0 = Empty
-layout n = layout (div n 10) |> (mod n 10)
+layout n = M.fromList $ zip ds (drop 1 $ cycle ds)
+  where ds = digits n
+
+
+layoutBig :: Int -> Cups
+layoutBig n = M.fromList $ zip is (drop 1 $ cycle is)
+  where is = digits n ++ [10..1000000]
+
+
+play :: Int -> Int -> Cups
+play start rounds = simulate rounds first cups
+  where first = head $ digits start
+        cups = layout start
+
+
+playBig :: Int -> Int -> Cups
+playBig start rounds = simulate rounds first cups
+  where first = head $ digits start
+        cups = layoutBig start
+
+
+simulate :: Int -> Int -> Cups -> Cups
+simulate 0 curr cups = cups
+simulate rounds curr cups = simulate (rounds-1) curr' cups'
+  where cups' = shuffle curr cups
+        curr' = cups' M.! curr
 
 
 after1 :: Cups -> Int
-after1 (1:<|cs) = go 0 cs
-  where go :: Int -> Cups -> Int
-        go n Empty = n
-        go n (c:<|cs) = go (10*n + c) cs
-after1 cs = after1 $ rotate cs
+after1 cups = go 0 (cups M.! 1)
+  where go :: Int -> Int -> Int
+        go acc 1 = acc
+        go acc curr = go (10*acc + curr) (cups M.! curr)
 
 
-rotate :: Seq a -> Seq a
-rotate Empty = Empty
-rotate (a:<|s) = s |> a
+nbhd :: Cups -> Int
+nbhd cups = first * second
+  where first = cups M.! 1
+        second = cups M.! first
 
 
--- take 3
--- compute next
--- find next
--- insert 3
--- rotate
-shuffle :: Cups -> Cups
-shuffle cups@(curr:<|rest) = rest'' |> curr
-  where (temp, rest') = S.splitAt 3 rest
-        dest = destination curr temp
-        (Just ix) = S.elemIndexL dest rest'
-        (before, after) = S.splitAt (ix+1) rest'
-        rest'' = before >< temp >< after
+-- updates to make
+-- curr -> (whatever third maps to)
+-- dest -> first
+-- third -> (whatever dest maps to)
+shuffle :: Int -> Cups -> Cups
+shuffle curr cups = cups'''
+  where first = cups M.! curr
+        second = cups M.! first
+        third = cups M.! second
+        afterThird = cups M.! third
+        dest = destination curr cups
+        afterDest = cups M.! dest
+        cups' = M.insert curr afterThird cups
+        cups'' = M.insert dest first cups'
+        cups''' = M.insert third afterDest cups''
 
 
 destination :: Int -> Cups -> Int
-destination curr temp
-    | search == Nothing = next
-    | otherwise         = destination next temp
-      where next = center (curr-1)
-            search = S.elemIndexL next temp
+destination curr cups = head $ dropWhile (\x -> elem x [first, second, third]) options
+  where options = map (center cups) [curr-1, curr-2, curr-3, curr-4]
+        first = cups M.! curr
+        second = cups M.! first
+        third = cups M.! second
 
 
-center :: Int -> Int
-center n
-    | n < 1     = n + 9
-    | n > 9     = n - 9
-    | otherwise = n
+center :: Cups -> Int -> Int
+center cups n
+    | n > 0     = n
+    | otherwise = n + (M.size cups)
+
+
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = after1 $ iterate shuffle (layout 137826495) !! 100
+    let answer = after1 $ play 137826495 100
     print answer
     -- result <- submitAnswer 2020 23 1 answer
     -- print result
@@ -68,8 +101,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    let answer = nbhd $ playBig 137826495 10000000
     print answer
-    -- result <- submitAnswer 2020 23 2 answer
-    -- print result
+    result <- submitAnswer 2020 23 2 answer
+    print result
     return ()
