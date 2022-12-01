1b956ce0064211f035dc9a6d9c7faffc8e4eac63
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-01 09:18:27 -0500

    Day 01 solution

--- 2022/01/puzzle_2022_01.hs
+++ 2022/01/puzzle_2022_01.hs
@@ -1,11 +1,47 @@
 module Puzzle_2022_01 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+
+
+elvesP :: Parser [[Int]]
+elvesP = sepBy elfP newline
+
+
+elfP :: Parser [Int]
+elfP = some foodP
+
+
+foodP :: Parser Int
+foodP = do
+    food <- natural
+    newline
+    return food
+
+
+findMax :: [[Int]] -> Int
+findMax elves = maximum $ map sum elves
+
+
+findTopThree :: [[Int]] -> Int
+findTopThree elves = a + b + c where
+    (a, b, c) = topThree $ map sum elves
+
+
+topThree :: [Int] -> (Int, Int, Int)
+topThree calories = foldl finder (0, 0, 0) calories where
+    finder :: (Int, Int, Int) -> Int -> (Int, Int, Int)
+    finder (a, b, c) x
+        | x > a     = (x, a, b)
+        | x > b     = (a, x, b)
+        | x > c     = (a, b, x)
+        | otherwise = (a, b, c)
+
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just elves) <- parseInput elvesP "01/input.txt"
+    let answer = findMax elves
     print answer
     -- result <- submitAnswer 2022 01 1 answer
     -- print result
@@ -14,7 +50,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just elves) <- parseInput elvesP "01/input.txt"
+    let answer = findTopThree elves
     print answer
     -- result <- submitAnswer 2022 01 2 answer
     -- print result
--- 2022/01/test_2022_01.hs
+++ 2022/01/test_2022_01.hs
@@ -1,16 +1,35 @@
 import Test.HUnit
 import Puzzle_2022_01
+import Parsing (parseInput)
+
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testParse = TestCase $ do
+    (Just elves) <- parseInput elvesP "01/test_input.txt"
+    elves @=? [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+
+
+testMax = TestCase $ do
+    (Just elves) <- parseInput elvesP "01/test_input.txt"
+    let m = findMax elves
+    24000 @=? m
+
+
+testTopThree = TestCase $ do
+    (Just elves) <- parseInput elvesP "01/test_input.txt"
+    let t = topThree $ map sum elves
+    t @=? (24000, 11000, 10000)
+
 
 testA = TestList
-    []
+    [ TestLabel "Parsing" testParse
+    , TestLabel "Max" testMax]
 
 testB = TestList
-    []
+    [ TestLabel "Top Three" testTopThree]
 
 testList = TestList
     [ TestLabel "Part A" testA
