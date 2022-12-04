bd7ecc7acfdb51b4d2fcb039a15d204efb77f650
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-04 09:41:47 -0500

    Day 04 solution

--- 2022/04/puzzle_2022_04.hs
+++ 2022/04/puzzle_2022_04.hs
@@ -1,11 +1,58 @@
 module Puzzle_2022_04 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing hiding (count)
+
+-- data types
+data Assignment = Assignment 
+    { _l1 :: Int
+    , _u1 :: Int
+    , _l2 :: Int
+    , _u2 :: Int } deriving (Eq, Ord, Show)
+
+-- parsing
+assignmentsP :: Parser [Assignment]
+assignmentsP = sepBy assignmentP newline
+
+assignmentP :: Parser Assignment
+assignmentP = do
+    l1 <- natural
+    char '-'
+    u1 <- natural
+    char ','
+    l2 <- natural
+    char '-'
+    u2 <- natural
+    return $ Assignment l1 u1 l2 u2
+
+
+-- functions
+-- test if one pair contains the other
+containment :: Assignment -> Bool
+containment a
+    | (_l1 a <= _l2 a) && (_u1 a >= _u2 a) = True
+    | (_l1 a >= _l2 a) && (_u1 a <= _u2 a) = True
+    | otherwise                            = False
+
+
+count :: (a -> Bool) -> [a] -> Int
+count f xs = length $ filter f xs
+
+
+-- test if there's any overlap at all
+overlap :: Assignment -> Bool
+overlap a
+    | (_l1 a <= _u2 a) && (_l2 a <= _u1 a) = True
+    | otherwise                            = False
+
+
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just assignments) <- parseInput assignmentsP "04/input.txt"
+    let answer = count containment assignments
     print answer
     -- result <- submitAnswer 2022 04 1 answer
     -- print result
@@ -14,8 +61,9 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just assignments) <- parseInput assignmentsP "04/input.txt"
+    let answer = count overlap assignments
     print answer
-    -- result <- submitAnswer 2022 04 2 answer
-    -- print result
+    result <- submitAnswer 2022 04 2 answer
+    print result
     return ()
