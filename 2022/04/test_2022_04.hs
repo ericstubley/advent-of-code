ac4f747d964adbf4976f188c84b42fe754e76845
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-04 09:41:47 -0500

    Day 04 solution

--- 2022/04/test_2022_04.hs
+++ 2022/04/test_2022_04.hs
@@ -1,16 +1,29 @@
 import Test.HUnit
 import Puzzle_2022_04
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testContainment = TestCase $ do
+    (Just assignments) <- parseInput assignmentsP "04/test_input.txt"
+    let cs = map containment assignments
+    [False, False, False, True, True, False] @=? cs 
+
+
+testOverlap = TestCase $ do
+    (Just assignments) <- parseInput assignmentsP "04/test_input.txt"
+    let os = map overlap assignments
+    [False, False, True, True, True, True] @=? os 
+
+
 
 testA = TestList
-    []
+    [ TestLabel "Containment" testContainment]
 
 testB = TestList
-    []
+    [ TestLabel "Overlap" testOverlap]
 
 testList = TestList
     [ TestLabel "Part A" testA
