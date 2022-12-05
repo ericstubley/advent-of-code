fffe7398c1d1630c7c422c45adb9e3bcab79f3cc
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-05 10:59:52 -0500

    Day 05 solution

--- 2022/05/test_2022_05.hs
+++ 2022/05/test_2022_05.hs
@@ -1,16 +1,40 @@
 import Test.HUnit
 import Puzzle_2022_05
+import Parsing (parseInput)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testParse = TestCase $ do
+    (Just (crates, _)) <- parseInput craneP "05/test_input.txt" 
+    let correct = V.fromList ["NZ", "DCM", "P"]
+    crates @=? correct
+
+
+testRearrange = TestCase $ do
+    (Just (crates, instructions)) <- parseInput craneP "05/test_input.txt" 
+    let rearranged = V.fromList ["C", "M", "ZNDP"] 
+    rearranged @=? (rearrange crates instructions)
+
+
+
+testRearrange9001 = TestCase $ do
+    (Just (crates, instructions)) <- parseInput craneP "05/test_input.txt" 
+    let rearranged = V.fromList ["M", "C", "DNZP"] 
+    rearranged @=? (rearrange9001 crates instructions)
+
+
+
 testA = TestList
-    []
+    [ TestLabel "Parsing" testParse
+    , TestLabel "Rearrangement" testRearrange]
 
 testB = TestList
-    []
+    [ TestLabel "Rearrangement 9001" testRearrange9001]
 
 testList = TestList
     [ TestLabel "Part A" testA
