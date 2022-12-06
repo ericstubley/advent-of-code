520cdbc7a491d31ff89cc8ba43018f5d11d75500
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-06 10:50:57 -0500

    Day 06 solution

--- 2022/06/puzzle_2022_06.hs
+++ 2022/06/puzzle_2022_06.hs
@@ -1,11 +1,49 @@
 module Puzzle_2022_06 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.Sequence (Seq(..), ViewL(..))
+import Data.Set (Set)
+import qualified Data.Sequence as Seq
+import qualified Data.Set as Set
+
+-- data types
+
+-- parsing
+
+datastreamP :: Parser String
+datastreamP = some lowerChar
+
+-- functions
+-- doesn't do any input validation! if you don't have a length l distinct run
+-- no guarantees that this will behave sensibly
+-- returns the index after the distinct run
+firstDistinctRun :: (Eq a, Ord a) => [a] -> Int -> Int
+firstDistinctRun xs l = fdr (Seq.empty) (Set.empty) xs 0 where
+    fdr :: (Eq a, Ord a) => Seq a -> Set a -> [a] -> Int -> Int
+    fdr run count xs idx
+        | Seq.length run == l               = idx
+        | not (Set.member (head xs) count)  = fdr (run Seq.|> x) (Set.insert x count) (tail xs) (idx+1)
+        | otherwise                         = fdr left (Set.delete first count) xs idx
+          where 
+            x = head xs
+            (first :< left) = Seq.viewl run
+
+
+
+packetStart :: String -> Int
+packetStart s = firstDistinctRun s 4
+
+
+messageStart :: String -> Int
+messageStart s = firstDistinctRun s 14
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just datastream) <- parseInput datastreamP "06/input.txt"
+    let answer = packetStart datastream
     print answer
     -- result <- submitAnswer 2022 06 1 answer
     -- print result
@@ -14,7 +52,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just datastream) <- parseInput datastreamP "06/input.txt"
+    let answer = messageStart datastream
     print answer
     -- result <- submitAnswer 2022 06 2 answer
     -- print result
--- 2022/06/test_2022_06.hs
+++ 2022/06/test_2022_06.hs
@@ -6,11 +6,25 @@ import Puzzle_2022_06
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testPacketStart = TestCase $ do
+    7 @=? packetStart "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    5 @=? packetStart "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    6 @=? packetStart "nppdvjthqldpwncqszvftbrmjlhg"
+    10 @=? packetStart "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    11 @=? packetStart "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+
+testMessageStart = TestCase $ do
+    19 @=? messageStart "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    23 @=? messageStart "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    23 @=? messageStart "nppdvjthqldpwncqszvftbrmjlhg"
+    29 @=? messageStart "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    26 @=? messageStart "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+
 testA = TestList
-    []
+    [ TestLabel "Packet Start" testPacketStart]
 
 testB = TestList
-    []
+    [ TestLabel "Message Start" testMessageStart]
 
 testList = TestList
     [ TestLabel "Part A" testA
