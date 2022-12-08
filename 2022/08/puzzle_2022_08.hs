affb0647fb2f8d02d5abd2701636cd9297fe45e2
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-08 12:59:25 -0500

    Day 08 solution

--- 2022/08/puzzle_2022_08.hs
+++ 2022/08/puzzle_2022_08.hs
@@ -1,11 +1,85 @@
 module Puzzle_2022_08 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.Char (digitToInt)
+import Data.List (transpose)
+
+-- data types
+
+-- parsing
+forestP :: Parser [[Int]]
+forestP = sepBy (some (digitToInt <$> digitChar)) newline
+
+
+-- functions
+mark :: [Int] -> [Bool]
+mark = mark' (-1)
+
+
+mark' :: Int -> [Int] -> [Bool]
+mark' _ [] = []
+mark' height (t:trees)
+    | t > height = True : mark' t trees
+    | otherwise  = False : mark' height trees
+
+
+left :: [[a]] -> [[a]]
+left = id
+
+right :: [[a]] -> [[a]]
+right = map reverse
+
+above :: [[a]] -> [[a]]
+above = transpose
+
+below :: [[a]] -> [[a]]
+below = reverse . transpose . reverse
+
+
+markVisible :: [[Int]] -> [[Bool]]
+markVisible trees = foldl1 (zipWith (zipWith (||))) [markL, markR, markA, markB]
+  where
+    markL = map mark trees
+    markR = right . (map mark) . right $ trees
+    markA = above . (map mark) . above $ trees
+    markB = below . (map mark) . below $ trees
+
+
+countVisible :: [[Int]] -> Int
+countVisible = length . (filter id) . concat . markVisible
+
+
+views :: [Int] -> [Int]
+views = views' 0 []
+
+views' :: Int -> [(Int, Int)] -> [Int] -> [Int]
+views' _ _ [] = []
+views' distance [] (t:trees) = distance : views' 1 [(t, distance)] trees
+views' distance (s:stack) (t:trees)
+    | fst s < t  = views' (distance + snd s) stack (t:trees)
+    | fst s > t  = distance : views' 1 ((t, distance) : s : stack) trees
+    | fst s == t = distance : views' 1 ((t, distance + snd s) : stack) trees
+
+
+scenicScores :: [[Int]] -> [[Int]]
+scenicScores trees = foldl1 (zipWith (zipWith (*))) [viewL, viewR, viewA, viewB]
+  where
+    viewL = map views trees
+    viewR = right . (map views) . right $ trees
+    viewA = above . (map views) . above $ trees
+    viewB = below . (map views) . below $ trees
+
+
+mostScenic :: [[Int]] -> Int
+mostScenic = maximum . concat . scenicScores
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just forest) <- parseInput forestP "08/input.txt"
+    let answer = countVisible forest
     print answer
     -- result <- submitAnswer 2022 08 1 answer
     -- print result
@@ -14,7 +88,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just forest) <- parseInput forestP "08/input.txt"
+    let answer = mostScenic forest
     print answer
     -- result <- submitAnswer 2022 08 2 answer
     -- print result
--- 2022/08/test_2022_08.hs
+++ 2022/08/test_2022_08.hs
@@ -1,16 +1,61 @@
 import Test.HUnit
 import Puzzle_2022_08
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testMarkVisible = TestCase $ do
+    (Just forest) <- parseInput forestP "08/test_input.txt"
+    let correct = [ [True, True, True, True, True]
+                  , [True, True, True, False, True]
+                  , [True, True, False, True, True]
+                  , [True, False, True, False, True]
+                  , [True, True, True, True, True]]
+    correct @=? markVisible forest
+
+
+testCountVisible = TestCase $ do
+    (Just forest) <- parseInput forestP "08/test_input.txt"
+    21 @=? countVisible forest
+
+
+testViewingDistances = TestCase $ do
+    [0, 1, 2, 3, 1] @=? views [3, 0, 3, 7, 3]
+    [0, 1, 1, 1, 2] @=? views [2, 5, 5, 1, 2] 
+    [0, 1, 1, 1, 1] @=? views [6, 5, 3, 3, 2]
+    [0, 1, 2, 1, 4] @=? views [3, 3, 5, 4, 9]
+    [0, 1, 1, 3, 1] @=? views [3, 5, 3, 9, 0]
+
+    [0, 1, 2, 1, 1] @=? views [3, 2, 6, 3, 3]
+    [0, 1, 1, 1, 2] @=? views [0, 5, 5, 3, 5]
+    [0, 1, 1, 2, 1] @=? views [3, 5, 3, 5, 3]
+    [0, 1, 2, 3, 4] @=? views [7, 1, 3, 4, 9]
+    [0, 1, 1, 3, 1] @=? views [3, 2, 2, 9, 0]
+
+
+testScenicScores = TestCase $ do
+    (Just forest) <- parseInput forestP "08/test_input.txt"
+    let ss = scenicScores forest
+    4 @=? (ss !! 1) !! 2
+    8 @=? (ss !! 3) !! 2
+
+
+testMostScenic = TestCase $ do
+    (Just forest) <- parseInput forestP "08/test_input.txt"
+    8 @=? mostScenic forest
+
+
 testA = TestList
-    []
+    [ TestLabel "Mark Visible" testMarkVisible
+    , TestLabel "Count Visible" testCountVisible]
 
 testB = TestList
-    []
+    [ TestLabel "Viewing Distances" testViewingDistances
+    , TestLabel "Scenic Scores" testScenicScores
+    , TestLabel "Most Scenic" testMostScenic]
 
 testList = TestList
     [ TestLabel "Part A" testA
