4a4a3bb17b2808d903054e8adf13302c2da2b95b
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-10 09:45:06 -0500

    Day 10 solution

--- 2022/10/puzzle_2022_10.hs
+++ 2022/10/puzzle_2022_10.hs
@@ -1,11 +1,90 @@
 module Puzzle_2022_10 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.List.Split (chunksOf)
+
+-- data types
+
+data Instruction = NOOP | ADDX Int deriving (Eq, Show, Ord)
+
+
+-- parsing
+
+programP :: Parser [Instruction]
+programP = sepBy instructionP newline
+
+
+instructionP :: Parser Instruction
+instructionP = noopP <|> addxP
+
+
+noopP :: Parser Instruction
+noopP = string "noop" >> return NOOP
+
+
+addxP :: Parser Instruction
+addxP = do
+    string "addx "
+    n <- integer
+    return $ ADDX n
+
+
+-- functions
+register :: [Instruction] -> [Int]
+register = register' 1
+
+
+register' :: Int -> [Instruction] -> [Int]
+register' _ [] = []
+register' x (i:is) = case i of
+                        NOOP   -> x : register' x is
+                        ADDX n -> x : x : register' (x+n) is
+
+
+signalStrengths :: [Instruction] -> [Int]
+signalStrengths = zipWith (*) [1..] . register
+
+
+takeEvery :: Int -> [a] -> [a]
+takeEvery _ [] = []
+takeEvery n xs = case drop (n-1) xs of
+                        []     -> []
+                        (y:ys) -> y : takeEvery n ys
+
+
+-- every a mod m-th element of the list
+-- should only use with a < m
+arithmeticProgression :: Int -> Int -> [a] -> [a]
+arithmeticProgression _ _ [] = []
+arithmeticProgression a m xs = case drop (a-1) xs of
+                                    []     -> []
+                                    (y:ys) -> y : takeEvery m ys
+
+
+sumInterestingSignalStrengths :: [Instruction] -> Int
+sumInterestingSignalStrengths = sum . arithmeticProgression 20 40 . signalStrengths
+
+
+lit :: Bool -> Char
+lit True = '#'
+lit False = '.'
+
+
+isLit :: Int -> Int -> Bool
+isLit electron sprite = abs ((mod electron 40) - sprite) <= 1
+
+
+screen :: [Instruction] -> [String]
+screen = chunksOf 40 . map lit . zipWith isLit [0..239] . register
+
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just program) <- parseInput programP "10/input.txt"
+    let answer = sumInterestingSignalStrengths program
     print answer
     -- result <- submitAnswer 2022 10 1 answer
     -- print result
@@ -14,8 +93,10 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
-    print answer
+    (Just program) <- parseInput programP "10/input.txt"
+    mapM_ putStrLn . screen $ program
+    -- let answer = 0
+    -- print answer
     -- result <- submitAnswer 2022 10 2 answer
     -- print result
     return ()
--- 2022/10/test_2022_10.hs
+++ 2022/10/test_2022_10.hs
@@ -1,16 +1,53 @@
 import Test.HUnit
 import Puzzle_2022_10
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testRegister = TestCase $ do
+    (Just program) <- parseInput programP "10/test_input.txt"
+    [1, 1, 1, 4, 4] @=? register program 
+
+
+testSignalStrength = TestCase $ do
+    (Just program) <- parseInput programP "10/test_input_2.txt"
+    let correct = zipWith (*) [1..] [1, 1, 16, 16, 5, 5, 11, 11]
+    correct @=? (take 8 $ signalStrengths program)
+
+
+testInterestingSignals = TestCase $ do
+    (Just program) <- parseInput programP "10/test_input_2.txt"
+    [420, 1140, 1800, 2940, 2880, 3960] @=? (arithmeticProgression 20 40 . signalStrengths $ program) 
+    13140 @=? sumInterestingSignalStrengths program 
+
+
+testDraw = TestCase $ do
+    (Just program) <- parseInput programP "10/test_input_2.txt"
+    putStrLn "correct one"
+    mapM_ putStrLn [ "##..##..##..##..##..##..##..##..##..##.."
+                   , "###...###...###...###...###...###...###."
+                   , "####....####....####....####....####...."
+                   , "#####.....#####.....#####.....#####....."
+                   , "######......######......######......####"
+                   , "#######.......#######.......#######....."]
+    putStrLn "program one"
+    mapM_ putStrLn . screen $ program 
+    putStrLn "are they the same? y/n"
+    result <- getChar
+    'y' @=? result
+
+
+
 testA = TestList
-    []
+    [ testRegister
+    , testSignalStrength
+    , testInterestingSignals]
 
 testB = TestList
-    []
+    [ testDraw]
 
 testList = TestList
     [ TestLabel "Part A" testA
