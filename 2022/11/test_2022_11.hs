850624ef391c1531f8ca43baa463f9059bd648bb
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-11 17:07:52 -0500

    Day 11 solution

--- 2022/11/test_2022_11.hs
+++ 2022/11/test_2022_11.hs
@@ -1,16 +1,41 @@
 import Test.HUnit
 import Puzzle_2022_11
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testKeepAwayRound = TestCase $ do
+    (Just (monkeys, items)) <- parseInput monkeysP "11/test_input.txt"
+    let items' = keepAwayRound monkeys items
+    mapM_ (putStrLn . show) items'
+
+
+testManyRounds = TestCase $ do
+    (Just (monkeys, items)) <- parseInput monkeysP "11/test_input.txt"
+    let items' = (iterate (keepAwayRound monkeys) items) !! 20
+    mapM_ (putStrLn . show) items'
+
+
+testCounts = TestCase $ do
+    (Just (monkeys, items)) <- parseInput monkeysP "11/test_input.txt"
+    [101, 95, 7, 105] @=? totalHeld 20 monkeys items 
+    
+
+testLongCounts = TestCase $ do
+    print "Make sure you remembered to change the modulus in inspect'"
+    (Just (monkeys, items)) <- parseInput monkeysP "11/test_input.txt"
+    [52166, 47830, 1938, 52013] @=? longRun 10000 monkeys items
+
 testA = TestList
-    []
+    [ testKeepAwayRound
+    , testManyRounds
+    , testCounts]
 
 testB = TestList
-    []
+    [ testLongCounts]
 
 testList = TestList
     [ TestLabel "Part A" testA
