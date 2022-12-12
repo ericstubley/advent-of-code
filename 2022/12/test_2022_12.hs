22560044806309b59a02ca66984634d73c0549fc
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-12 13:49:27 -0500

    Day 12 solution

--- 2022/12/test_2022_12.hs
+++ 2022/12/test_2022_12.hs
@@ -1,16 +1,27 @@
 import Test.HUnit
 import Puzzle_2022_12
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testShortestPath = TestCase $ do
+    (Just area) <- parseInput areaP "12/test_input.txt"
+    31 @=? shortestPath area
+
+
+testShortestPath' = TestCase $ do
+    (Just area) <- parseInput areaP "12/test_input.txt"
+    29 @=? shortestPath' area
+
+
 testA = TestList
-    []
+    [ testShortestPath]
 
 testB = TestList
-    []
+    [ testShortestPath']
 
 testList = TestList
     [ TestLabel "Part A" testA
