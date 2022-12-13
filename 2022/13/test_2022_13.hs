3693c0d918a2e429a670d746860c40b454330559
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-13 09:11:47 -0800

    Day 13 solution

--- 2022/13/test_2022_13.hs
+++ 2022/13/test_2022_13.hs
@@ -1,16 +1,33 @@
 import Test.HUnit
 import Puzzle_2022_13
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testComparisons = TestCase $ do
+    (Just packetPairs) <- parseInput packetPairsP "13/test_input.txt"
+    [LT, LT, GT, LT, GT, LT, GT, GT] @=? comparisons packetPairs 
+
+
+testCorrectIndices = TestCase $ do
+    (Just packetPairs) <- parseInput packetPairsP "13/test_input.txt"
+    [1, 2, 4, 6] @=? correctIndices packetPairs
+
+
+testDecoderKey = TestCase $ do
+    (Just packets) <- parseInput packetsP "13/test_input.txt"
+    140 @=? decoderKey packets
+
+
 testA = TestList
-    []
+    [ testComparisons
+    , testCorrectIndices]
 
 testB = TestList
-    []
+    [ testDecoderKey]
 
 testList = TestList
     [ TestLabel "Part A" testA
