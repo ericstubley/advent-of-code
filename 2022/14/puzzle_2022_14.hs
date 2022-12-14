ee2525e4309254eb56e84f5662780914d92469d3
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-14 11:46:57 -0800

    Day 14 solution

--- 2022/14/puzzle_2022_14.hs
+++ 2022/14/puzzle_2022_14.hs
@@ -1,11 +1,95 @@
 module Puzzle_2022_14 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Massiv.Array (Ix2(..))
+
+-- data types
+data Tile = Air | Sand | Rock deriving (Eq, Ord, Show)
+
+data FillMode = Abyss | Floor deriving (Eq, Ord, Show)
+
+type Cavern = Map Ix2 Tile
+
+-- parsing
+cavernP :: Parser Cavern
+cavernP = M.unions <$> (sepBy lineP newline)
+
+
+lineP :: Parser Cavern
+lineP = (M.fromList . (map (\c -> (c, Rock))) . makePath) 
+        <$> (sepBy coordP (string " -> "))
+
+
+coordP :: Parser Ix2
+coordP = do
+    j <- natural
+    char ','
+    i <- natural
+    return $ j :. i
+
+
+makePath :: [Ix2] -> [Ix2]
+makePath [] = []
+makePath (x:[]) = []
+makePath (x:y:xs) = (path x y) ++ (makePath (y:xs))
+
+
+path :: Ix2 -> Ix2 -> [Ix2]
+path (j1:.i1) (j2:.i2) = [(j:.i) | j <- [minJ..maxJ], i <- [minI..maxI]]
+  where minJ = min j1 j2
+        maxJ = max j1 j2
+        minI = min i1 i2
+        maxI = max i1 i2
+
+-- functions
+source :: Ix2
+source = 500:.0
+
+
+abyss :: Cavern -> Int
+abyss cavern = maximum . map depth . M.keys . M.filter (==Rock) $ cavern
+  where depth (j:.i) = i
+
+
+fill :: FillMode -> Cavern -> Cavern
+fill mode cavern = fill' cavern
+  where level = abyss cavern
+        fill' :: Cavern -> Cavern
+        fill' cavern
+            | mode == Floor && M.member source cavern = cavern
+            | mode == Abyss && cavern' == cavern      = cavern
+            | otherwise                               = fill' cavern'
+              where cavern' = grain mode level cavern
+
+
+grain :: FillMode -> Int -> Cavern -> Cavern
+grain mode level cavern = go source
+  where go :: Ix2 -> Cavern
+        go (j:.i)
+            | mode == Floor && i > level          = M.insert (j:.i) Sand cavern
+            | mode == Abyss && i > level          = cavern
+            | M.notMember (j     :. (i+1)) cavern = go (j:.(i+1))
+            | M.notMember ((j-1) :. (i+1)) cavern = go ((j-1):.(i+1))
+            | M.notMember ((j+1) :. (i+1)) cavern = go ((j+1):.(i+1))
+            | otherwise                           = M.insert (j:.i) Sand cavern
+
+
+abyssVolume :: Cavern -> Int
+abyssVolume = M.size . M.filter (== Sand) . fill Abyss
+
+
+floorVolume :: Cavern -> Int
+floorVolume = M.size . M.filter (== Sand) . fill Floor
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just cavern) <- parseInput cavernP "14/input.txt"
+    let answer = abyssVolume cavern
     print answer
     -- result <- submitAnswer 2022 14 1 answer
     -- print result
@@ -14,7 +98,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just cavern) <- parseInput cavernP "14/input.txt"
+    let answer = floorVolume cavern
     print answer
     -- result <- submitAnswer 2022 14 2 answer
     -- print result
--- 2022/14/test_2022_14.hs
+++ 2022/14/test_2022_14.hs
@@ -1,16 +1,34 @@
 import Test.HUnit
 import Puzzle_2022_14
+import Parsing (parseInput)
+import qualified Data.Map as M
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testParse = TestCase $ do
+    (Just cavern) <- parseInput cavernP "14/test_input.txt"
+    20 @=? M.size cavern 
+
+
+testAbyssVolume = TestCase $ do
+    (Just cavern) <- parseInput cavernP "14/test_input.txt"
+    24 @=? abyssVolume cavern
+
+
+testFloorVolume = TestCase $ do
+    (Just cavern) <- parseInput cavernP "14/test_input.txt"
+    93 @=? floorVolume cavern
+
+
 testA = TestList
-    []
+    [ testParse
+    , testAbyssVolume]
 
 testB = TestList
-    []
+    [ testFloorVolume]
 
 testList = TestList
     [ TestLabel "Part A" testA
