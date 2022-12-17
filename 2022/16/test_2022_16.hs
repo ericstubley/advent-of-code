3a6bc80439e79b5fe30393a731512f8aa120c9ed
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-16 16:59:12 -0800

    Day 16 in progress

--- 2022/16/test_2022_16.hs
+++ 2022/16/test_2022_16.hs
@@ -1,16 +1,27 @@
 import Test.HUnit
 import Puzzle_2022_16
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testMaximumRelease = TestCase $ do
+    (Just (adjacencies, rates)) <- parseInput graphP "16/test_input.txt"
+    1651 @=? maximumRelease adjacencies rates
+
+
+testCollaborativeRelease = TestCase $ do
+    (Just (adjacencies, rates)) <- parseInput graphP "16/test_input.txt"
+    1707 @=? collaborativeRelease adjacencies rates
+
+
 testA = TestList
-    []
+    [ testMaximumRelease]
 
 testB = TestList
-    []
+    [ testCollaborativeRelease]
 
 testList = TestList
     [ TestLabel "Part A" testA
