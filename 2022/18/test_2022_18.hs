8587ef53b2defde72421a21701bbe4d75c5ec4b5
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-18 09:33:10 -0800

    Day 18 solution

--- 2022/18/test_2022_18.hs
+++ 2022/18/test_2022_18.hs
@@ -1,16 +1,27 @@
 import Test.HUnit
 import Puzzle_2022_18
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testSurfaceArea = TestCase $ do
+    (Just cubes) <- parseInput cubesP "18/test_input.txt"
+    64 @=? surfaceArea cubes 
+
+
+testExteriorSurfaceArea = TestCase $ do
+    (Just cubes) <- parseInput cubesP "18/test_input.txt"
+    58 @=? exteriorSurfaceArea cubes
+
+
 testA = TestList
-    []
+    [ testSurfaceArea]
 
 testB = TestList
-    []
+    [ testExteriorSurfaceArea]
 
 testList = TestList
     [ TestLabel "Part A" testA
