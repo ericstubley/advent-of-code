9cf3cd4334b428b73da3f8cf6d7f8a680acdd6f6
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-20 13:17:28 -0800

    Day 20 in progress

--- 2022/20/puzzle_2022_20.hs
+++ 2022/20/puzzle_2022_20.hs
@@ -1,11 +1,110 @@
 module Puzzle_2022_20 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.Sort (sortOn)
+
+-- data types
+-- map original index -> (value, currentIndex)
+type File = Vector (Int, Int)
+
+-- parsing
+fileP :: Parser [Int]
+fileP = sepBy integer newline
+
+-- functions
+prepare :: [Int] -> File
+prepare is = V.fromList $ zip is [0..]
+
+
+mix :: File -> File
+mix file = go file 0 (V.length file)
+
+
+go :: File -> Int -> Int -> File
+go f n limit
+    | n == limit = f
+    | otherwise  = go f' (n+1) limit
+      where 
+        m = V.length f
+        (jump, old) = f V.! n
+        new = mod (old + jump) (m-1)
+        mapper (v, i) = (v, mod (shuffle old new i) m)
+        f' = V.map mapper f
+
+
+
+shuffle :: Int -> Int -> Int -> Int
+shuffle old new current
+    | old == current                  = new
+    | old < current && current < new  = current - 1
+    | new < current && current < old  = current + 1
+    | old < current && current == new = current - 1
+    | current < old && current == new = current + 1
+    | otherwise                       = current
+
+
+reindex :: File -> File
+reindex file = V.map adjust file where
+    m = V.length file
+    zeroIndex = snd . head . dropWhile (\x -> fst x /= 0) $ V.toList file
+    adjust = \x -> (fst x, mod (snd x - zeroIndex) m)
+
+
+find :: File -> Int -> Int
+find file i = fst . head . dropWhile (\x -> snd x /= i) $ V.toList file
+
+
+finalList :: [Int] -> [Int]
+finalList is = map fst . sortOn snd . V.toList . reindex . mix . prepare $ is
+
+
+intermediateList :: [Int] -> Int -> [Int]
+intermediateList is n = map fst . sortOn snd . V.toList . (\f -> go f 0 n) . prepare $ is
+
+
+groveCoordinateSum :: [Int] -> Int
+groveCoordinateSum is = (find f i1) + (find f i2) + (find f i3) where
+    m = length is
+    f = reindex . mix . prepare $ is
+    i1 = mod 1000 m
+    i2 = mod 2000 m
+    i3 = mod 3000 m
+
+
+decryptionKey :: Int
+decryptionKey = 811589153
+
+
+prePrepare :: [Int] -> [Int]
+prePrepare = map ((*) decryptionKey)
+
+
+finalList' :: [Int] -> Int -> [Int]
+finalList' is n = map fst . sortOn snd . V.toList . reindex . mixN n . prepare . prePrepare $ is
+
+
+mixN :: Int -> File -> File
+mixN n f = head . drop n . iterate mix $ f
+
+
+groveCoordinateSum' :: [Int] -> Int
+groveCoordinateSum' is = (find f i1) + (find f i2) + (find f i3) where
+    m = length is
+    f = reindex . mixN 10 . prepare . prePrepare $ is
+    i1 = mod 1000 m
+    i2 = mod 2000 m
+    i3 = mod 3000 m
+
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just file) <- parseInput fileP "20/input.txt"
+    let answer = groveCoordinateSum file
     print answer
     -- result <- submitAnswer 2022 20 1 answer
     -- print result
@@ -14,7 +113,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just file) <- parseInput fileP "20/input.txt"
+    let answer = groveCoordinateSum' (prePrepare file)
     print answer
     -- result <- submitAnswer 2022 20 2 answer
     -- print result
--- 2022/20/test_2022_20.hs
+++ 2022/20/test_2022_20.hs
@@ -1,16 +1,63 @@
 import Test.HUnit
 import Puzzle_2022_20
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testIntermediateLists = TestCase $ do
+    (Just file) <- parseInput fileP "20/test_input.txt"
+    [2, 1, -3, 3, -2, 0, 4] @=? intermediateList file 1
+    [1, -3, 2, 3, -2, 0, 4] @=? intermediateList file 2
+    [1, 2, 3, -2, -3, 0, 4] @=? intermediateList file 3
+    [1, 2, -2, -3, 0, 3, 4] @=? intermediateList file 4
+    [-2, 1, 2, -3, 0, 3, 4] @=? intermediateList file 5
+    [-2, 1, 2, -3, 0, 3, 4] @=? intermediateList file 6
+    [-2, 1, 2, -3, 4, 0, 3] @=? intermediateList file 7
+
+
+testFinalList = TestCase $ do
+    (Just file) <- parseInput fileP "20/test_input.txt"
+    [0, 3, -2, 1, 2, -3, 4] @=? finalList file
+
+
+testGroveCoordinateSum = TestCase $ do
+    (Just file) <- parseInput fileP "20/test_input.txt"
+    3 @=? groveCoordinateSum file
+
+
+testMixes = TestCase $ do
+    (Just file) <- parseInput fileP "20/test_input.txt"
+    [0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153] @=? finalList' file 1
+    [0, 2434767459, 1623178306, 3246356612, -2434767459, -1623178306, 811589153] @=? finalList' file 2
+    [0, 811589153, 2434767459, 3246356612, 1623178306, -1623178306, -2434767459] @=? finalList' file 3
+    [0, 1623178306, -2434767459, 811589153, 2434767459, 3246356612, -1623178306] @=? finalList' file 4
+    [0, 811589153, -1623178306, 1623178306, -2434767459, 3246356612, 2434767459] @=? finalList' file 5
+    [0, 811589153, -1623178306, 3246356612, -2434767459, 1623178306, 2434767459] @=? finalList' file 6
+    [0, -2434767459, 2434767459, 1623178306, -1623178306, 811589153, 3246356612] @=? finalList' file 7
+    [0, 1623178306, 3246356612, 811589153, -2434767459, 2434767459, -1623178306] @=? finalList' file 8
+    [0, 811589153, 1623178306, -2434767459, 3246356612, 2434767459, -1623178306] @=? finalList' file 9
+    [0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153] @=? finalList' file 10
+
+
+testGroveCoordinateSum' = TestCase $ do
+    (Just file) <- parseInput fileP "20/test_input.txt"
+    1623178306 @=? groveCoordinateSum' file
+
+
+
+
+
 testA = TestList
-    []
+    [ testIntermediateLists
+    , testFinalList
+    , testGroveCoordinateSum]
 
 testB = TestList
-    []
+    [ testMixes
+    , testGroveCoordinateSum']
 
 testList = TestList
     [ TestLabel "Part A" testA
