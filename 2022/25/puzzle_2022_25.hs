1ff1150cb9bfbcf4f27bc119a336397a6616076e
Eric Stubley <ericdavidstubley@gmail.com>
2022-12-25 10:19:05 -0500

    Day 25 solution

--- 2022/25/puzzle_2022_25.hs
+++ 2022/25/puzzle_2022_25.hs
@@ -1,11 +1,102 @@
 module Puzzle_2022_25 where
 
 import Automation (submitAnswer)
-import Parsing (parseInput)
+import Parsing
+
+-- data types
+-- Mew for "Minus two", etc...
+data Figit = Mew | Mun | Nun | One | Two deriving (Eq, Ord)
+
+instance Show Figit where
+    show Mew = "="
+    show Mun = "-"
+    show Nun = "0"
+    show One = "1"
+    show Two = "2"
+
+type Fun = [Figit]
+
+instance Num Fun where
+    (+) f1 f2 = tinSum f1 f2
+    fromInteger n = []
+     
+-- parsing
+snafusP :: Parser [Fun]
+snafusP = sepBy snafuP newline
+
+
+snafuP :: Parser Fun
+snafuP = some figitP
+
+
+figitP :: Parser Figit
+figitP = (char '=' >> return Mew)
+     <|> (char '-' >> return Mun)
+     <|> (char '0' >> return Nun)
+     <|> (char '1' >> return One)
+     <|> (char '2' >> return Two)
+
+
+-- functions
+-- tin is Sn on the periodic table, and this function is snSum
+tinSum :: Fun -> Fun -> Fun
+tinSum [] fs = fs
+tinSum fs [] = fs
+tinSum fs1 fs2
+    | length fs1 < length fs2 = tinSum (pad ++ fs1) fs2
+    | length fs1 > length fs2 = tinSum fs1 (pad ++ fs2)
+    | otherwise               = dropWhile (==Nun) $ tinHelper Nun fs1 fs2
+      where pad = take (abs (length fs1 - length fs2)) $ repeat Nun
+
+
+tinHelper :: Figit -> Fun -> Fun -> Fun
+tinHelper car [] [] = [car]
+tinHelper car fs1 fs2 = tinHelper (carry x) (init fs1) (init fs2) ++ [remain x]
+  where x = (fint car) + (fint $ last fs1) + (fint $ last fs2)
+
+
+carry :: Int -> Figit
+carry x
+    | -5 <= x && x <= -3 = Mun
+    | -2 <= x && x <=  2 = Nun
+    |  3 <= x && x <=  5 = One
+    | otherwise          = error "Unexpected digit sum"
+
+
+remain :: Int -> Figit
+remain x
+    | x == -5   = Nun
+    | x == -4   = One
+    | x == -3   = Two
+    | x == -2   = Mew
+    | x == -1   = Mun
+    | x ==  0   = Nun
+    | x ==  1   = One
+    | x ==  2   = Two
+    | x ==  3   = Mew
+    | x ==  4   = Mun
+    | x ==  5   = Nun
+    | otherwise = error "Unexpected digit sum"
+
+
+fint :: Figit -> Int
+fint f = case f of
+            Mew -> -2
+            Mun -> -1
+            Nun ->  0
+            One ->  1
+            Two ->  2
+
+
+printer :: Fun -> String
+printer = concat . map show
+
+-- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just snafus) <- parseInput snafusP "25/input.txt"
+    let answer = printer $ sum snafus
     print answer
     -- result <- submitAnswer 2022 25 1 answer
     -- print result
--- 2022/25/test_2022_25.hs
+++ 2022/25/test_2022_25.hs
@@ -1,13 +1,42 @@
 import Test.HUnit
 import Puzzle_2022_25
+import Parsing (parseInput)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+-- decimalExamples :: [Int]
+-- decimalExamples = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 2022, 12345
+--                   , 314159265, 1747, 906, 198, 11, 201, 31, 1257, 32, 353
+--                   , 107, 7, 3, 37]
+-- 
+-- 
+-- snafuExamples :: [String]
+-- snafuExamples = [ "1", "2", "1=", "1-", "10", "11", "12", "2=", "2-", "20"
+--                 , "1=0", "1-0", "1=11-2", "1-0---0", "1121-1110-1=0", "1=-0-2"
+--                 , "12111", "2=0=", "21", "2=01", "111", "20012", "112", "1=-1="
+--                 , "1-12", "12", "1=", "122"]
+-- 
+-- 
+-- testSnafu = TestCase $ do
+--     snafuExamples @=? map snafu decimalExamples
+-- 
+-- 
+-- testUnfuck = TestCase $ do
+--     decimalExamples @=? map unfuck snafuExamples
+
+
+testSum = TestCase $ do
+    (Just snafus) <- parseInput snafusP "25/test_input.txt"
+    "2=-1=0" @=? (printer $ sum snafus)
+
+
 testA = TestList
-    []
+    -- [ TestLabel "Snafu" testSnafu
+    -- , TestLabel "Unfuck" testUnfuck
+    [ TestLabel "Sum" testSum]
 
 testB = TestList
     []
