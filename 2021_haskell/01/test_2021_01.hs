a4c92634de5f398c06b1dcd12e39acd3c0d133b1
Eric Stubley <ericdavidstubley@gmail.com>
2023-01-16 09:50:44 -0800

    Day 01 solution

--- 2021_haskell/01/test_2021_01.hs
+++ 2021_haskell/01/test_2021_01.hs
@@ -7,11 +7,21 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testIncreases = TestCase $ do
+    (Just depths) <- parseInput depthsP "01/test_input.txt"
+    7 @=? increases depths 
+
+
+testWindow3 = TestCase $ do
+    (Just depths) <- parseInput depthsP "01/test_input.txt"
+    [607, 618, 618, 617, 647, 716, 769, 792] @=? window3 depths
+
+
 testA = TestList
-    []
+    [ TestLabel "Increases" testIncreases]
 
 testB = TestList
-    []
+    [ TestLabel "Window3" testWindow3]
 
 testList = TestList
     [ TestLabel "Part A" testA
