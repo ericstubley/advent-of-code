ace76ac2e751745d0b1e630e643bd8d984b3e005
Eric Stubley <ericdavidstubley@gmail.com>
2023-01-16 09:50:44 -0800

    Day 01 solution

--- 2021_haskell/01/puzzle_2021_01.hs
+++ 2021_haskell/01/puzzle_2021_01.hs
@@ -6,14 +6,27 @@ import Parsing
 -- data types
 
 -- parsing
+depthsP :: Parser [Int]
+depthsP = sepBy integer newline
 
 -- functions
+increases :: [Int] -> Int
+increases ds = length . filter (\t -> fst t < snd t) $ zip ds (tail ds)
+
+
+window3 :: [Int] -> [Int]
+window3 ds = zipWith3 (\a b c -> a + b + c) ds (tail ds) (drop 2 ds)
+
+
+increases' :: [Int] -> Int
+increases' = increases . window3
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just depths) <- parseInput depthsP "01/input.txt"
+    let answer = increases depths
     print answer
     -- result <- submitAnswer 2021 01 1 answer
     -- print result
@@ -22,7 +35,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just depths) <- parseInput depthsP "01/input.txt"
+    let answer = increases' depths
     print answer
     -- result <- submitAnswer 2021 01 2 answer
     -- print result
