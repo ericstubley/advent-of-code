55f69df81af330d1bba01b4b65ad4f7bc014e720
Eric Stubley <ericdavidstubley@gmail.com>
2023-01-16 11:06:36 -0800

    Day 02 solution

--- 2021_haskell/02/test_2021_02.hs
+++ 2021_haskell/02/test_2021_02.hs
@@ -7,11 +7,21 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testNavigate = TestCase $ do
+    (Just course) <- parseInput courseP "02/test_input.txt"
+    (15, 10) @=? navigate course
+
+
+testNavigate' = TestCase $ do
+    (Just course) <- parseInput courseP "02/test_input.txt"
+    (15, 60) @=? navigate' course
+
+
 testA = TestList
-    []
+    [ TestLabel "Navigate" testNavigate]
 
 testB = TestList
-    []
+    [ TestLabel "Navigate'" testNavigate']
 
 testList = TestList
     [ TestLabel "Part A" testA
