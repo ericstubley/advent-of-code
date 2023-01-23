54c3d617740fb0ef19931c960cbd18f93507d397
Eric Stubley <ericdavidstubley@gmail.com>
2023-01-22 17:35:37 -0800

    Day 03 solution

--- 2021_haskell/03/puzzle_2021_03.hs
+++ 2021_haskell/03/puzzle_2021_03.hs
@@ -2,18 +2,74 @@ module Puzzle_2021_03 where
 
 import Automation (submitAnswer)
 import Parsing
+import Data.Bits
+import Data.Vector (Vector)
+import qualified Data.Vector as V
 
 -- data types
+type BitString = Vector Int
 
 -- parsing
+reportP :: Parser [BitString]
+reportP = sepBy bitStringP newline
+
+
+bitStringP :: Parser BitString
+bitStringP = V.fromList <$> many bitP
+
+
+bitP :: Parser Int
+bitP = (char '1' >> return 1) 
+   <|> (char '0' >> return 0)
+
 
 -- functions
+interpret :: BitString -> Int
+interpret bs = V.foldl (\acc b -> if b == 0 then 2*acc else (2*acc + 1)) 0 bs
+
+
+gammaRate :: [BitString] -> BitString
+gammaRate xs = V.map (\x -> div (2*x) l) $ foldl1 (V.zipWith (+)) xs
+  where l = length xs
+
+
+epsilonRate :: [BitString] -> BitString
+epsilonRate xs = V.map (1 - ) (gammaRate xs)
+
+
+powerConsumption :: [BitString] -> Int
+powerConsumption xs = (interpret $ gammaRate xs) * (interpret $ epsilonRate xs)
+
+
+-- part b
+rating :: ([BitString] -> BitString) -> [BitString] -> BitString
+rating f xs = go xs 0 where
+    go :: [BitString] -> Int -> BitString
+    go [bs] _ = bs
+    go bss  n = go bss' (n+1) where
+        bss' = filter (\b -> b V.! n == cond V.! n) bss
+        cond = f bss
+
+
+oxyRating :: [BitString] -> BitString
+oxyRating = rating gammaRate
+
+
+co2Rating :: [BitString] -> BitString
+co2Rating = rating epsilonRate
+
+
+lifeSupportRating :: [BitString] -> Int
+lifeSupportRating xs = (interpret $ oxyRating xs) * (interpret $ co2Rating xs)
+
+
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just report) <- parseInput reportP "03/input.txt"
+    let answer = powerConsumption report
     print answer
     -- result <- submitAnswer 2021 03 1 answer
     -- print result
@@ -22,7 +78,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just report) <- parseInput reportP "03/input.txt"
+    let answer = lifeSupportRating report
     print answer
     -- result <- submitAnswer 2021 03 2 answer
     -- print result
--- 2021_haskell/03/test_2021_03.hs
+++ 2021_haskell/03/test_2021_03.hs
@@ -7,11 +7,34 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+
+testGamma = TestCase $ do
+    (Just report) <- parseInput reportP "03/test_input.txt"
+    22 @=? (interpret $ gammaRate report)
+
+
+testEpsilon = TestCase $ do
+    (Just report) <- parseInput reportP "03/test_input.txt"
+    9 @=? (interpret $ epsilonRate report)
+
+
+testOxy = TestCase $ do
+    (Just report) <- parseInput reportP "03/test_input.txt"
+    23 @=? (interpret $ oxyRating report)
+
+
+testCo2 = TestCase $ do
+    (Just report) <- parseInput reportP "03/test_input.txt"
+    10 @=? (interpret $ co2Rating report)
+
+
 testA = TestList
-    []
+    [ TestLabel "Gamma Rate" testGamma
+    , TestLabel "Epsilon Rate" testEpsilon]
 
 testB = TestList
-    []
+    [ TestLabel "Oxygen Rating" testOxy
+    , TestLabel "Co2 Rating" testCo2]
 
 testList = TestList
     [ TestLabel "Part A" testA
