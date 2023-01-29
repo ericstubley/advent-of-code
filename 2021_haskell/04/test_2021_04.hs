cc5e086e191148faa21d960a0fb4127d097474b7
Eric Stubley <ericdavidstubley@gmail.com>
2023-01-28 17:45:17 -0800

    Day 04 solution

--- 2021_haskell/04/test_2021_04.hs
+++ 2021_haskell/04/test_2021_04.hs
@@ -7,11 +7,20 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testFirstScore = TestCase $ do
+    (Just (calls, boards)) <- parseInput bingoP "04/test_input.txt"
+    4512 @=? firstScore calls boards 
+
+testLastScore = TestCase $ do
+    (Just (calls, boards)) <- parseInput bingoP "04/test_input.txt"
+    1924 @=? lastScore calls boards 
+
+
 testA = TestList
-    []
+    [ TestLabel "First Score" testFirstScore]
 
 testB = TestList
-    []
+    [ TestLabel "Last Score" testLastScore]
 
 testList = TestList
     [ TestLabel "Part A" testA
