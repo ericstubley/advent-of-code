e6ec6dd3c9195516f92649685fe512ec1f406e4e
Eric Stubley <ericdavidstubley@gmail.com>
2023-01-28 19:11:11 -0800

    Day 05 solution

--- 2021_haskell/05/test_2021_05.hs
+++ 2021_haskell/05/test_2021_05.hs
@@ -7,11 +7,27 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testSegment = TestCase $ do
+    let segment = ((1, 1), (3, 3))
+    [(1, 1), (2, 2), (3, 3)] @=? points segment 
+
+
+testOverlapsA = TestCase $ do
+    (Just segments) <- parseInput segmentsP "05/test_input.txt"
+    5 @=? overlapsA segments 
+
+
+testOverlapsB = TestCase $ do
+    (Just segments) <- parseInput segmentsP "05/test_input.txt"
+    12 @=? overlapsB segments 
+
+
 testA = TestList
-    []
+    [ TestLabel "Segment" testSegment
+    , TestLabel "Overlaps A" testOverlapsA]
 
 testB = TestList
-    []
+    [ TestLabel "Overlaps B" testOverlapsB]
 
 testList = TestList
     [ TestLabel "Part A" testA
