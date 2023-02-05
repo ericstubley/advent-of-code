0857cded0daa5486e7ec8f743f51f6cc3673ef42
Eric Stubley <ericdavidstubley@gmail.com>
2023-02-04 19:07:22 -0800

    Day 06 solution
    There's a real issue with haskell: you don't really understand how to optimize things.
    The boxed vs unboxed, strict vs thunks, ... all seem very ambiguous to me.

--- 2021_haskell/06/puzzle_2021_06.hs
+++ 2021_haskell/06/puzzle_2021_06.hs
@@ -2,18 +2,53 @@ module Puzzle_2021_06 where
 
 import Automation (submitAnswer)
 import Parsing
+import Data.Vector (Vector)
+import Linear.Vector
+import qualified Data.Vector as V
 
 -- data types
+type Fish = Vector Int
 
 -- parsing
+lanternfishP :: Parser Fish
+lanternfishP = foldl1 (^+^) <$> sepBy fishP (char ',')
+
+
+fishP :: Parser Fish
+fishP = bucket <$> integer 
 
 -- functions
+
+days :: Int -> Fish -> Fish
+days 0 (!fish) = fish
+days n (!fish) = days (n-1) (day fish)
+
+
+day :: Fish -> Fish
+day (!fish) = shifted ^+^ spawned
+  where shifted = (V.tail fish) V.++ (V.singleton 0)
+        spawned = spawn (V.head fish)
+
+
+spawn :: Int -> Fish
+spawn n = n *^ respawn
+
+
+bucket :: Int -> Fish
+bucket i = (V.replicate size 0) V.// [(i, 1)]
+  where size = 9
+
+
+respawn :: Fish  
+respawn = (bucket 6) ^+^ (bucket 8)
+
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just lanternfish) <- parseInput lanternfishP "06/input.txt"
+    let answer = V.sum $ days 80 lanternfish
     print answer
     -- result <- submitAnswer 2021 06 1 answer
     -- print result
@@ -22,7 +57,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just lanternfish) <- parseInput lanternfishP "06/input.txt"
+    let answer = V.sum $ days 256 lanternfish
     print answer
     -- result <- submitAnswer 2021 06 2 answer
     -- print result
--- 2021_haskell/06/test_2021_06.hs
+++ 2021_haskell/06/test_2021_06.hs
@@ -1,14 +1,29 @@
 import Test.HUnit
 import Puzzle_2021_06
 import Parsing (parseInput)
+import Data.Vector (toList)
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testDays = TestCase $ do
+    (Just lanternfish) <- parseInput lanternfishP "06/test_input.txt"
+    [0, 1, 1, 2, 1, 0, 0, 0, 0] @=? (toList $ days 0 lanternfish)
+    [1, 1, 2, 1, 0, 0, 0, 0, 0] @=? (toList $ days 1 lanternfish)
+    [1, 2, 1, 0, 0, 0, 1, 0, 1] @=? (toList $ days 2 lanternfish)
+    [2, 1, 0, 0, 0, 1, 1, 1, 1] @=? (toList $ days 3 lanternfish)
+    [1, 0, 0, 0, 1, 1, 3, 1, 2] @=? (toList $ days 4 lanternfish)
+    [0, 0, 0, 1, 1, 3, 2, 2, 1] @=? (toList $ days 5 lanternfish)
+    [0, 0, 1, 1, 3, 2, 2, 1, 0] @=? (toList $ days 6 lanternfish)
+    [0, 1, 1, 3, 2, 2, 1, 0, 0] @=? (toList $ days 7 lanternfish)
+    [1, 1, 3, 2, 2, 1, 0, 0, 0] @=? (toList $ days 8 lanternfish)
+    [1, 3, 2, 2, 1, 0, 1, 0, 1] @=? (toList $ days 9 lanternfish)
+
+
 testA = TestList
-    []
+    [ TestLabel "Days" testDays]
 
 testB = TestList
     []
