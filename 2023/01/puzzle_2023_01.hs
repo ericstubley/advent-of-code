e7db7cf1f476dcc3d7a2161de3a20b1f965f92dc
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-01 23:18:37 -0800

    Day 01 solution

--- 2023/01/puzzle_2023_01.hs
+++ 2023/01/puzzle_2023_01.hs
@@ -1,11 +1,63 @@
 module Puzzle_2023_01 where
 
 import Automation (submitAnswer)
+import Data.Char (digitToInt, isDigit)
 import Parsing
 
 -- data types
 
 -- parsing
+calibrationsP :: Parser [Int]
+calibrationsP = sepBy calibrationP newline
+
+calibrationP :: Parser Int
+calibrationP = do
+    digits <- digitsP
+    let a = head digits
+    let b = last digits
+    return $ 10*a + b
+
+digitsP :: Parser [Int]
+digitsP = amendmentsP *> many (digitP <* amendmentsP)
+
+digitP :: Parser Int
+digitP = fmap (digitToInt) digitChar
+
+amendmentsP :: Parser ()
+amendmentsP = skipMany lowerChar
+
+actualCalibrationsP :: Parser [Int]
+actualCalibrationsP = sepBy actualCalibrationP newline
+
+actualCalibrationP :: Parser Int
+actualCalibrationP = do
+    digits <- actualDigitsP
+    let a = head digits
+    let b = last digits
+    return $ 10*a + b
+
+actualDigitsP :: Parser [Int]
+actualDigitsP = fmap (map digitToInt . filter isDigit) actualCharsP
+
+actualCharsP :: Parser [Char]
+actualCharsP = many (digitChar <|> spelledAndAdvanceP <|> lowerChar)
+
+spelledAndAdvanceP :: Parser Char
+spelledAndAdvanceP = lookAhead spelledP <* lowerChar
+
+spelledP :: Parser Char
+spelledP = (string "zero" >> return '0')
+    <|> (string "one"     >> return '1')
+    <|> (string "two"     >> return '2')
+    <|> (string "three"   >> return '3')
+    <|> (string "four"    >> return '4')
+    <|> (string "five"    >> return '5')
+    <|> (string "six"     >> return '6')
+    <|> (string "seven"   >> return '7')
+    <|> (string "eight"   >> return '8')
+    <|> (string "nine"    >> return '9')
+
+-- efnostz
 
 -- functions
 
@@ -13,7 +65,8 @@ import Parsing
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just calibrationValues) <- parseInput calibrationsP "01/input.txt"
+    let answer = sum calibrationValues
     print answer
     -- result <- submitAnswer 2023 01 1 answer
     -- print result
@@ -22,7 +75,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just calibrationValues) <- parseInput actualCalibrationsP "01/input.txt"
+    let answer = sum calibrationValues
     print answer
     -- result <- submitAnswer 2023 01 2 answer
     -- print result
--- 2023/01/test_2023_01.hs
+++ 2023/01/test_2023_01.hs
@@ -1,17 +1,33 @@
 import Test.HUnit
 import Puzzle_2023_01
-import Parsing (parseInput)
+import Parsing
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testParseA = TestCase $ do
+    (Just calibrationValues) <- parseInput calibrationsP "01/test_input.txt" 
+    [12, 38, 15, 77] @=? calibrationValues
+
+
+testOverlap = TestCase $ do
+    (Just results) <- parseInput actualCalibrationsP "01/overlap_test.txt"
+    [98, 82, 18, 12] @=? results
+
+
+testParseB = TestCase $ do
+    (Just calibrationValues) <- parseInput actualCalibrationsP "01/test_input_2.txt" 
+    [29, 83, 13, 24, 42, 14, 76] @=? calibrationValues
+
+
 testA = TestList
-    []
+    [ TestLabel "Parsing A" testParseA]
 
 testB = TestList
-    []
+    [ TestLabel "Overlap edge case" testOverlap
+    , TestLabel "Parsing B" testParseB]
 
 testList = TestList
     [ TestLabel "Part A" testA
