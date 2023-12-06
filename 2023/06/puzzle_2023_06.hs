6e2643013ac84c9e522b79b6e4ce3ab4f25fb3e4
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-05 22:10:52 -0800

    Day 06 solution

--- 2023/06/puzzle_2023_06.hs
+++ 2023/06/puzzle_2023_06.hs
@@ -4,16 +4,35 @@ import Automation (submitAnswer)
 import Parsing
 
 -- data types
+data Race = Race { _time :: Int 
+                 , _distance :: Int } deriving (Eq, Ord, Show)
 
 -- parsing
+racesP :: Parser [Race]
+racesP = do
+    string "Time:" 
+    times <- many (hspace >> natural)
+    newline
+    string "Distance:"
+    distances <- many (hspace >> natural)
+    return $ zipWith Race times distances
 
 -- functions
+strategies :: Race -> [Int]
+strategies (Race t d) = filter (d<) . map (\n -> n*(t-n)) $ [0..t]
+
+stratCountProd :: [Race] -> Int
+stratCountProd = product . map (length . strategies)
+
+compress :: [Race] -> Race
+compress races = Race (read . concat . map (show . _time) $ races) (read . concat . map (show . _distance) $ races)
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just races) <- parseInput racesP "06/input.txt"
+    let answer = stratCountProd races
     print answer
     -- result <- submitAnswer 2023 06 1 answer
     -- print result
@@ -22,7 +41,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just races) <- parseInput racesP "06/input.txt"
+    let answer = length . strategies . compress $ races
     print answer
     -- result <- submitAnswer 2023 06 2 answer
     -- print result
