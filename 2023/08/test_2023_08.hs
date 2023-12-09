d43755d4fff203c4604d765c8a855160ce550a1f
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-08 19:03:01 -0800

    Day 08 solution

--- 2023/08/test_2023_08.hs
+++ 2023/08/test_2023_08.hs
@@ -6,12 +6,24 @@ import Parsing (parseInput)
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testStepCount = TestCase $ do
+    (Just environment) <- parseInput inputP "08/test_input.txt"
+    2 @=? stepCount environment
+    (Just environment) <- parseInput inputP "08/test_input_2.txt"
+    6 @=? stepCount environment
+
+testParallelStepCount = TestCase $ do
+    (Just environment) <- parseInput inputP "08/test_input_3.txt"
+    6 @=? ghostCount environment
+    return ()
+
+
 
 testA = TestList
-    []
+    [ TestLabel "Step count" testStepCount]
 
 testB = TestList
-    []
+    [ TestLabel "Parallel step count" testParallelStepCount]
 
 testList = TestList
     [ TestLabel "Part A" testA
