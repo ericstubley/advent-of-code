353671b3b08d0890234e45ea32e430240d27145d
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-08 21:51:57 -0800

    Day 09 solution

--- 2023/09/puzzle_2023_09.hs
+++ 2023/09/puzzle_2023_09.hs
@@ -6,14 +6,40 @@ import Parsing
 -- data types
 
 -- parsing
+reportP :: Parser [[Int]]
+reportP = sepBy historyP newline
+
+historyP :: Parser [Int]
+historyP = sepBy integer (char ' ')
 
 -- functions
+-- prediction :: [Int] -> Int
+-- prediction xs
+--     | all (==0) xs = 0
+--     | otherwise    = last xs + (prediction $ diffs xs)
+
+-- extrapolation :: [Int] -> Int
+-- extrapolation = prediction . reverse
+
+-- bc Haskell lists, use the version that uses head rather than last
+extrapolation :: [Int] -> Int
+extrapolation xs
+    | all (==0) xs = 0
+    | otherwise    = head xs - (extrapolation $ diffs xs)
+
+prediction :: [Int] -> Int
+prediction = extrapolation . reverse
+
+diffs :: [Int] -> [Int]
+diffs (x:xs@(y:ys)) = (y-x) : (diffs xs)
+diffs _ = []
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just report) <- parseInput reportP "09/input.txt"
+    let answer = sum . map prediction $ report
     print answer
     -- result <- submitAnswer 2023 09 1 answer
     -- print result
@@ -22,7 +48,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just report) <- parseInput reportP "09/input.txt"
+    let answer = sum . map extrapolation $ report
     print answer
     -- result <- submitAnswer 2023 09 2 answer
     -- print result
--- 2023/09/test_2023_09.hs
+++ 2023/09/test_2023_09.hs
@@ -7,11 +7,21 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testPredictions = TestCase $ do
+    (Just report) <- parseInput reportP "09/test_input.txt"
+    [18, 28, 68] @=? map prediction report
+
+
+testExtrapolations = TestCase $ do
+    (Just report) <- parseInput reportP "09/test_input.txt"
+    [-3, 0, 5] @=? map extrapolation report
+
+
 testA = TestList
-    []
+    [ TestLabel "Predictions" testPredictions]
 
 testB = TestList
-    []
+    [ TestLabel "Extrapolations" testExtrapolations]
 
 testList = TestList
     [ TestLabel "Part A" testA
