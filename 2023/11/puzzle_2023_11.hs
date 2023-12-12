ce3ed6982cde28f1e9275b6d7aa34d3c5022103b
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-11 22:04:58 -0800

    Day 11 solution

--- 2023/11/puzzle_2023_11.hs
+++ 2023/11/puzzle_2023_11.hs
@@ -2,18 +2,72 @@ module Puzzle_2023_11 where
 
 import Automation (submitAnswer)
 import Parsing
+import Data.Massiv.Array (Array, Ix2(..), Sz(..))
+import qualified Data.Massiv.Array as A
+import Control.Monad.Reader
 
 -- data types
+type Array2 a = A.Array A.B A.Ix2 a
+type Universe = Array2 Char
 
 -- parsing
+universeP :: Parser Universe
+universeP = A.fromLists' A.Seq <$> sepBy (many astroP) newline
+
+astroP :: Parser Char
+astroP = char '.' <|> char '#'
 
 -- functions
+analyze :: Int -> Universe -> Int
+analyze expansion universe = div (sum . map (inflateDistance expansion rows cols) $ pairs) 2
+  where gs = runReader galaxies universe
+        pairs = [(a, b) | a <- gs, b <- gs]
+        rows = runReader weft universe
+        cols = runReader warp universe
+
+analyzeNow :: Universe -> Int
+analyzeNow = analyze 2
+
+analyzeLater :: Universe -> Int
+analyzeLater = analyze 1000000
+
+galaxies :: MonadReader Universe m => m [Ix2]
+galaxies = do
+    u <- reader id
+    s <- reader $ A.size
+    let (Sz (r:.c)) = s
+    let ixs = [(x :. y) | x <- [0..(r-1)], y <- [0..(c-1)]]
+    return $ filter (\ix -> (u A.! ix) == '#') ixs
+
+-- rows but more numinous
+weft :: MonadReader Universe m => m [Int]
+weft = do
+    u <- reader id
+    s <- reader $ A.size
+    let (Sz (r:.c)) = s
+    return $ filter (\i -> A.all (=='.') (u A.!> i)) [0..(r-1)]
+
+-- cols but more numinous
+warp :: MonadReader Universe m => m [Int]
+warp = do
+    u <- reader id
+    s <- reader $ A.size
+    let (Sz (r:.c)) = s
+    return $ filter (\j -> A.all (=='.') (u A.<! j)) [0..(c-1)]
+
+inflateDistance :: Int -> [Int] -> [Int] -> (Ix2, Ix2) -> Int
+inflateDistance expansion rows cols ((x1:.y1), (x2:.y2)) = dx + dy + (expansion - 1)*(ix + iy)
+  where dx = abs (x1 - x2)
+        dy = abs (y1 - y2)
+        ix = length . takeWhile (< max x1 x2) . dropWhile (< min x1 x2) $ rows
+        iy = length . takeWhile (< max y1 y2) . dropWhile (< min y1 y2) $ cols
 
 -- mains
 
 mainA :: IO ()
 mainA = do
-    let answer = 0
+    (Just universe) <- parseInput universeP "11/input.txt"
+    let answer = analyzeNow universe
     print answer
     -- result <- submitAnswer 2023 11 1 answer
     -- print result
@@ -22,7 +76,8 @@ mainA = do
 
 mainB :: IO ()
 mainB = do
-    let answer = 0
+    (Just universe) <- parseInput universeP "11/input.txt"
+    let answer = analyzeLater universe
     print answer
     -- result <- submitAnswer 2023 11 2 answer
     -- print result
--- 2023/11/test_2023_11.hs
+++ 2023/11/test_2023_11.hs
@@ -1,17 +1,36 @@
 import Test.HUnit
 import Puzzle_2023_11
 import Parsing (parseInput)
+import Data.Massiv.Array (Ix2(..))
+import Control.Monad.Reader
 
 -- testGamma = TestCase (do
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testAnalyze = TestCase $ do
+    (Just universe) <- parseInput universeP "11/test_input.txt"
+    let rows = runReader weft universe
+    let cols = runReader warp universe
+    [3, 7] @=? rows
+    [2, 5, 8] @=? cols
+    9 @=? inflateDistance 2 rows cols ((5:.1), (9:. 4))
+    15 @=? inflateDistance 2 rows cols ((0:.3), (8:. 7))
+    17 @=? inflateDistance 2 rows cols ((2:.0), (6:. 9))
+    5 @=? inflateDistance 2 rows cols ((9:.0), (9:. 4))
+    374 @=? analyzeNow universe
+
+
+testAnalyzeLater = TestCase $ do
+    (Just universe) <- parseInput universeP "11/test_input.txt"
+    1030 @=? analyze 10 universe
+    8410 @=? analyze 100 universe
 
 testA = TestList
-    []
+    [ TestLabel "Analyze" testAnalyze]
 
 testB = TestList
-    []
+    [ TestLabel "Analyze later" testAnalyzeLater]
 
 testList = TestList
     [ TestLabel "Part A" testA
