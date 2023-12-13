a24f1cd075d0699d1bf4f9035eede362f461ab50
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-12 22:02:24 -0800

    Day 12 solution

--- 2023/12/test_2023_12.hs
+++ 2023/12/test_2023_12.hs
@@ -6,12 +6,45 @@ import Parsing (parseInput)
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testPossibilities = TestCase $ do
+    [[Operational]] @=? possibilities [Operational]
+    [[Damaged]] @=? possibilities [Damaged]
+    [[Operational], [Damaged]] @=? possibilities [Unknown]
+    [[Operational, Operational], [Operational, Damaged], [Damaged, Operational], [Damaged, Damaged]] @=? possibilities [Unknown, Unknown]
+
+
+testArrangements = TestCase $ do
+    (Just reports) <- parseInput reportsP "12/test_input.txt"
+    1 @=? countArrangements (Report [Damaged] [1])
+    1 @=? countArrangements (Report [Operational] [])
+    [1, 4, 1, 1, 4, 10] @=? map countArrangements reports
+    -- mapM_ (print . printer) (options . last $ reports)
+
+testArrangements' = TestCase $ do
+    (Just reports) <- parseInput reportsP "12/test_input.txt"
+    1 @=? countArrangements' (Report [Damaged] [1])
+    1 @=? countArrangements' (Report [Operational] [])
+    2 @=? countArrangements' (Report [Unknown, Unknown] [1])
+    [1, 4, 1, 1, 4, 10] @=? map countArrangements' reports
+
+testUnfold = TestCase $ do
+    let r  = Report [Operational, Damaged] [1]
+    -- .#?.#?.#?.#?.# 1,1,1,1,1
+    let r' = Report [Operational, Damaged, Unknown, Operational, Damaged, Unknown, Operational, Damaged, Unknown, Operational, Damaged, Unknown, Operational, Damaged] [1, 1, 1, 1, 1]
+    r' @=? unfold r
+
+testUnfolded = TestCase $ do
+    (Just reports) <- parseInput reportsP "12/test_input.txt"
+    [1, 16384, 1, 16, 2500, 506250] @=? map (countArrangements' . unfold) reports
 
 testA = TestList
-    []
+    [ TestLabel "Possibilities" testPossibilities
+    , TestLabel "Count arrangements" testArrangements]
 
 testB = TestList
-    []
+    [ TestLabel "Arrangements'" testArrangements'
+    , TestLabel "Unfold" testUnfold
+    , TestLabel "Unfolded arrangments" testUnfolded]
 
 testList = TestList
     [ TestLabel "Part A" testA
