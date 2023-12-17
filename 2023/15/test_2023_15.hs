b94f5790aab1089493585b81ea5df985182fa5ea
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-17 00:05:27 -0800

    Day 15 solution

--- 2023/15/test_2023_15.hs
+++ 2023/15/test_2023_15.hs
@@ -7,11 +7,23 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testHash = TestCase $ do
+   52 @=? hash "HASH" 
+
+testHashLonger = TestCase $ do
+    (Just input) <- parseInput inputP "15/test_input.txt"
+    [30, 253, 97, 47, 14, 180, 9, 197, 48, 214, 231] @=? map hash input
+
+testFocusingPower = TestCase $ do
+    (Just steps) <- parseInput stepsP "15/test_input.txt"
+    145 @=? (configPower . fillBoxes $ steps)
+
 testA = TestList
-    []
+    [ TestLabel "Hash" testHash
+    , TestLabel "Longer hash" testHashLonger]
 
 testB = TestList
-    []
+    [ TestLabel "Focusing Power" testFocusingPower]
 
 testList = TestList
     [ TestLabel "Part A" testA
