d2dec65c87b916c0b392f93dd5011de5d165b13a
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-22 21:42:53 -0500

    Day 17 solution

--- 2023/17/test_2023_17.hs
+++ 2023/17/test_2023_17.hs
@@ -7,11 +7,23 @@ import Parsing (parseInput)
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
 
+testHeatLossA = TestCase $ do
+    (Just env) <- parseInput envP "17/test_input.txt"
+    102 @=? heatLossA env
+
+testHeatLossB = TestCase $ do
+    (Just env1) <- parseInput envP "17/test_input.txt"
+    -- print $ heatLossB env1
+    94 @=? heatLossB env1
+    (Just env2) <- parseInput envP "17/test_input_2.txt"
+    -- print $ heatLossB env2
+    71 @=? heatLossB env2
+
 testA = TestList
-    []
+    [ TestLabel "Heat loss A" testHeatLossA]
 
 testB = TestList
-    []
+    [ TestLabel "Heat loss B" testHeatLossB]
 
 testList = TestList
     [ TestLabel "Part A" testA
