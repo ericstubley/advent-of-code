086cf84d59b70375a5184fb3c601db74eef55586
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-23 21:32:18 -0500

    Day 21 solution

--- 2023/21/test_2023_21.hs
+++ 2023/21/test_2023_21.hs
@@ -6,12 +6,28 @@ import Parsing (parseInput)
 --     report <- parsedInput
 --     let gr = binaryToInt $ gammaRate report
 --     22 @=? gr)
+testStepLocations = TestCase $ do
+    (Just garden) <- parseInput gardenP "21/test_input.txt"
+    2 @=? steps garden 1
+    4 @=? steps garden 2
+    6 @=? steps garden 3
+    16 @=? steps garden 6
+
+testWrapSteps = TestCase $ do
+    (Just garden) <- parseInput gardenP "21/test_input.txt"
+    (Just garden) <- parseInput gardenP "21/test_input.txt"
+    16 @=? wrapSteps garden 6
+    50 @=? wrapSteps garden 10
+    1594 @=? wrapSteps garden 50
+    6536 @=? wrapSteps garden 100
+
+
 
 testA = TestList
-    []
+    [ TestLabel "Number of locations" testStepLocations]
 
 testB = TestList
-    []
+    [ TestLabel "Wrap steps" testWrapSteps]
 
 testList = TestList
     [ TestLabel "Part A" testA
