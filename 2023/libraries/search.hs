fbac84fe7b38add81c40e6f6d512be8985e58dcf
Eric Stubley <ericdavidstubley@gmail.com>
2023-12-24 12:19:01 -0500

    Make search functions work

--- 2023/libraries/search.hs
+++ 2023/libraries/search.hs
@@ -2,10 +2,11 @@ module Search ( search
               , binarySearch
               , linearSearchForward
               , linearSearchBackward
-              , bfs
+              -- , bfs
               , dfs ) where
+              -- , floydWarshall ) where
 
-
+import Data.Foldable (foldlM)
 import Data.Map.Strict (Map)
 import Data.Maybe (Maybe(..))
 import Data.Sequence (Seq(..), (><))
@@ -33,11 +34,10 @@ import qualified Data.Set as S
 -- use at your own risk! if your update function is bad in some way
 search :: (Int -> Int -> Int) -> (a -> Bool) -> [a] -> (a, a)
 search update predicate xs = go 0 ((length xs) - 1) where
-    go :: Int -> Int -> (a, a)
     go lo hi
-        | hi == lo + 1           = (xs !! lo, xs !! hi)
-        | predicate mid == False = go mid hi
-        | predicate mid == True  = go lo mid where
+        | hi == lo + 1                   = (xs !! lo, xs !! hi)
+        | predicate (xs !! mid) == False = go mid hi
+        | predicate (xs !! mid) == True  = go lo mid where
             mid = update lo hi
 
 binarySearch :: (a -> Bool) -> [a] -> (a, a)
@@ -54,20 +54,19 @@ linearSearchBackward = search (\lo hi -> hi - 1)
 -- function which detects termination state
 -- function which generates next states from current one
 
-bfs :: Monad m , Ord a =>
-       a ->
-       (a -> m Bool) ->
-       (a -> m [a]) ->
-       (a -> )
-       m a
-bfs initial terminator generator = go S.empty (Sq.singleton initial) where
-    go :: Monad m => Set a -> Seq a -> m a
-    go seen (state :<| queue) = do
-        if S.member state seen then go seen queue else do
-            finished <- terminator state
-            if finished then return state else do
-                options <- generator state
-                go (S.insert state seen) (queue >< $ Sq.fromList options)
+-- bfs :: (Monad m , Ord a)
+--     => a
+--     -> (a -> m Bool)
+--     -> (a -> m [a])
+--     -> m a
+-- bfs initial terminator generator = go S.empty (Sq.singleton initial) where
+--     go :: Monad m => Set a -> Seq a -> m a
+--     go seen (state :<| queue) = do
+--         if S.member state seen then go seen queue else do
+--             finished <- terminator state
+--             if finished then return state else do
+--                 options <- generator state
+--                 go (S.insert state seen) (queue >< $ Sq.fromList options)
 
 
 -- potential problems
@@ -81,50 +80,53 @@ bfs initial terminator generator = go S.empty (Sq.singleton initial) where
 --      Nothing for not a termination state
 --      Just b for some function a -> b of state -> termination value
 -- function which generates next states from current one
--- function which selects the best state based on the dfs values
 -- function which computes the new value based off of the current state,
 --      along with the values of the substates
 
-dfs :: Monad m , Ord a =>
-       a ->
-       (a -> m (Maybe b)) ->
-       (a -> m [a]) ->
-       (a -> [b] -> m b) ->
-       m b
-dfs initial terminator generator computer = go initial where
-    memo = M.empty
-    go :: Monad m, Ord a=> a -> m b
-    go state = do
-        if M.member state memo then return (memo M.! state) else do
-            case terminator state of
+dfs :: (Monad m , Ord a)
+    => a
+    -> (a -> m (Maybe b))
+    -> (a -> m [a]) 
+    -> (a -> [b] -> m b)
+    -> m b
+dfs initial terminator generator computer = fst <$> go initial blank where
+    blank = M.empty
+    go state mem = do
+        if M.member state mem then return (mem M.! state, mem) else do
+            result <- terminator state
+            case result of
                 (Just x) -> do
-                    M.insert state x memo
-                    return x
+                    let mem' = M.insert state x mem
+                    return (x, mem)
                 Nothing  -> do
                     options <- generator state
-                    values <- mapM go options
+                    (values, mem') <- foldlM folder ([], mem) options
                     value <- computer state values
-                    M.insert state value memo
-                    return value
+                    let mem'' = M.insert state value mem'
+                    return (value, mem'')
+    folder (vs, mem) x = do
+        (v, mem') <- go x mem
+        return (v:vs, mem')
+
 
 -- potential problems
 -- too annoying to write 4 functions to pass through?
 
 
 -- 
-floydWarshall :: Ord a => 
-                 (a -> a -> Maybe Int) -> 
-                 [a] -> 
-                 Map (a, a) (Maybe Int)
-floydWarshall weight nodes = foldl go start tries where
-    start = M.fromList $ map (\t -> (t, weight (fst t) (snd t))) (pairs nodes)
-    tries = [(k, i, j) | k <- nodes, i <- nodes, j <- nodes]
-    go :: Ord a => Map (a, a) (Maybe Int) -> (a, a, a) -> Map (a, a) (Maybe Int)
-    go state (k, i, j) = do
-        curr <- M.find (i, j) state
-        leg1 <- M.find (i, k) state
-        leg2 <- M.find (j, k) state
-        return $ M.insert (i, j) 
+-- floydWarshall :: Ord a 
+--               => (a -> a -> Maybe Int)
+--               -> [a]
+--               -> Map (a, a) (Maybe Int)
+-- floydWarshall weight nodes = foldl go start tries where
+--     start = M.fromList $ map (\t -> (t, weight (fst t) (snd t))) (pairs nodes)
+--     tries = [(k, i, j) | k <- nodes, i <- nodes, j <- nodes]
+--     go :: Ord a => Map (a, a) (Maybe Int) -> (a, a, a) -> Map (a, a) (Maybe Int)
+--     go state (k, i, j) = do
+--         curr <- M.find (i, j) state
+--         leg1 <- M.find (i, k) state
+--         leg2 <- M.find (j, k) state
+--         return $ M.insert (i, j) 
 
 -- how to correctly handle the maybes here?
 -- the logic you want: replace the value at (i, j) with
